6ed6588d8cd61e2c29c59005f6bcd185d14e723a
Jente Rosseel <rosseeljente@gmail.com>
2023-08-01 16:36:46 +0300

    :wrench: Wire correct build paths

--- apps/elewa-website/server.ts
+++ apps/elewa-website/server.ts
@@ -10,7 +10,7 @@ import { AppServerModule } from './src/main.server';
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
-  const distFolder = join(process.cwd(), 'dist/elewa-website/browser');
+  const distFolder = join(process.cwd(), 'dist/apps/elewa-website/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
