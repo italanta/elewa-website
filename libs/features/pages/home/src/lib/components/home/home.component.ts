49ff10628badf1eff8b5b807e9c658b352d2a83c
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-03 16:05:27 +0300

    :recycle: fix: change css file to scss

--- libs/features/pages/home/src/lib/components/home/home.component.ts
+++ libs/features/pages/home/src/lib/components/home/home.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'elewa-website-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css'],
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {}
