cfec4f9c9e6bfa1e32fd9ccb05b0540585f5afa3
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-03 15:42:52 +0300

    :sparkles: feat: import home module into main module

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { FeaturesPagesHomeModule } from '@elewa-website/features/pages/home';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { TranslocoRootModule } from './transloco-root.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FeaturesPagesHomeModule],
   providers: [
     HttpClientModule,
     TranslocoRootModule
