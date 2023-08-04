397cc05b407abf17cfa150ee60e55055e9ad4dbf
danielsind <danielsindet@gmail.com>
2023-08-04 13:28:42 +0300

    Renamed the import path of importing NormalButtonComponent from - NormalButton/ to NormalButton/noramal-button.component

--- libs/elements/layout/buttons/src/lib/elements-layout-buttons.module.ts
+++ libs/elements/layout/buttons/src/lib/elements-layout-buttons.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NormalButtonComponent } from './NormalButton/normal-button.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [NormalButtonComponent],
+  exports: [NormalButtonComponent],
 })
 export class ElementsLayoutButtonsModule {}
