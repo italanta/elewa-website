3fc1e844cde49ef134fdbc0e1284668593399362
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-08-04 22:10:15 +0300

    exports and imports header component appropriately so it can be rendered on screen

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { TranslocoRootModule } from './transloco-root.module';
 
 import { AppRoutingModule } from './app.routing';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, AppHeaderModule],
   providers: [HttpClientModule, TranslocoRootModule],
   bootstrap: [AppComponent],
 })
--- libs/elements/layout/header/src/lib/app-header.module.ts
+++ libs/elements/layout/header/src/lib/app-header.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './elements/layout/header/components/header/header.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [HeaderComponent],
+  exports: [HeaderComponent]
 })
 export class AppHeaderModule {}
