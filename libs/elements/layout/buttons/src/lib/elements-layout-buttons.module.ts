fc9ffa9e21f337436306088c9ffd8b5a714f5707
Alexotieno1717 <alexotieno900@gmail.com>
2023-08-04 17:17:04 +0300

    created a component normal button

--- libs/elements/layout/buttons/src/lib/elements-layout-buttons.module.ts
+++ libs/elements/layout/buttons/src/lib/elements-layout-buttons.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NormalButtonComponent } from './normal-button/normal-button.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [
+    NormalButtonComponent,
+  ],
 })
 export class ElementsLayoutButtonsModule {}
---
+++ libs/elements/layout/buttons/src/lib/normal-button/normal-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-normal-button',
+  templateUrl: './normal-button.component.html',
+  styleUrls: ['./normal-button.component.scss'],
+})
+export class NormalButtonComponent {}
