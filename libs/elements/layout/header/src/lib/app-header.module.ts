eafb8e29d847edf2857b32f62a19c325bb697c23
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-08-04 20:41:30 +0300

    feature: adds the app's header component

--- libs/elements/layout/header/src/lib/app-header.module.ts
+++ libs/elements/layout/header/src/lib/app-header.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './elements/layout/header/components/header/header.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [HeaderComponent],
 })
 export class AppHeaderModule {}
