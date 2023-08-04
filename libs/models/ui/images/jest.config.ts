a2a986c56714003966cc3c155969efe36b61b333
Jente Rosseel <rosseeljente@gmail.com>
2023-08-04 10:26:04 +0300

    :package: Create models/ui/images

---
+++ libs/models/ui/images/jest.config.ts
@@ -0,0 +1,11 @@
+/* eslint-disable */
+export default {
+  displayName: 'models-ui-images',
+  preset: '../../../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../../../coverage/libs/models/ui/images',
+};
