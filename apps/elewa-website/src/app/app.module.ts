68cd75ca10102c3a0cfd84cc3d17f16a26c1d8a0
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-04 11:28:29 +0300

    :wastebasket: fix: delete default nx template component

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { TranslocoRootModule } from './transloco-root.module';
 
 import { AppRoutingModule } from './app.routing';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [HttpClientModule, TranslocoRootModule],
   bootstrap: [AppComponent],
