e0982aadb422d8db1b4451e57021353a86b43919
kevinkkimutai <kiruikevinkim@gmail.com>
2023-08-04 11:08:37 +0300

    ✨feat: homepage library generated

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { TranslocoRootModule } from './transloco-root.module';
+import { FeaturesPagesHomeModule } from '@elewa-website/features/pages/home';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FeaturesPagesHomeModule],
   providers: [
     HttpClientModule,
     TranslocoRootModule
---
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    HomeComponent
+  ],
+  exports:[HomeComponent]
+})
+export class FeaturesPagesHomeModule {}
