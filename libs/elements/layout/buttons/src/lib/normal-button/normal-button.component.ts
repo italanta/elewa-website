3e68a8d6c4e9ba5351074a0754d00fed46dea9f9
danielsind <danielsindet@gmail.com>
2023-08-04 13:34:34 +0300

    renamed the folder NormalButton to normal-button, to observe the caps and use of the hyphens well

---
+++ libs/elements/layout/buttons/src/lib/normal-button/normal-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-normal-button',
+  templateUrl: './normal-button.component.html',
+  styleUrls: ['./normal-button.component.scss'],
+})
+export class NormalButtonComponent {}
