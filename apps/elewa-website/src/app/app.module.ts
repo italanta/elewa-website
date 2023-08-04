5d317eeb61f387d9c7714be5f2283d0a4dbc6193
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-04 10:41:47 +0300

    :sparkles: feat: add routing module to the app

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -2,18 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
-import { FeaturesPagesHomeModule } from '@elewa-website/features/pages/home';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { TranslocoRootModule } from './transloco-root.module';
 
+import { AppRoutingModule } from './app.routing';
+
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, FeaturesPagesHomeModule],
-  providers: [
-    HttpClientModule,
-    TranslocoRootModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [HttpClientModule, TranslocoRootModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
