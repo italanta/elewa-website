ec49b022f4d327e09e68082c7bcb14954db46882
jbdura <bidurajohnson@gmail.com>
2023-08-04 12:17:59 +0300

    Created the following folders: element/layout/header. Created a module AppHeader and imported it in app.module.ts

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { FeaturesPagesHomeModule } from '@elewa-website/features/pages/home';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { TranslocoRootModule } from './transloco-root.module';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, FeaturesPagesHomeModule],
+  imports: [ BrowserModule, FeaturesPagesHomeModule, AppHeaderModule],
   providers: [
     HttpClientModule,
     TranslocoRootModule
