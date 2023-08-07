47f57a72904856043f84c8c6be2b39a3d00ae679
Leestan360 <stanleymayore2@gmail.com>
2023-08-07 19:56:16 +0300

    :memo: models: Add documentation to image interface

---
+++ libs/models/schema/ui/images/src/lib/image-config.interface.ts
@@ -0,0 +1,15 @@
+// Image interface type
+export interface ImageConfig {
+  title: string
+  imageSrc: string
+  visualisation: ImageVisualisation
+  maxWidth?: string
+}
+
+// Image interface enum
+export enum ImageVisualisation {
+  Pill = 0,
+  stacked = 1,
+  Window = 2,
+  Card = 3
+}
