99c2615a2588d184b7ee70476a305025a10bc734
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-07 10:41:45 +0300

    :sparkles:create header component and layout library

---
+++ libs/elements/layout/header/app-header-module/README.md
@@ -0,0 +1,7 @@
+# elements-layout-header-app-header-module
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test elements-layout-header-app-header-module` to execute the unit tests.
---
+++ libs/elements/layout/header/app-header-module/src/lib/elements-layout-header-app-header-module.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [
+    HeaderComponent
+  ],
+})
+export class ElementsLayoutHeaderAppHeaderModuleModule {}
