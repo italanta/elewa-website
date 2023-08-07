686c7f2c37c78c9679637cf1a4dca0a7074c0082
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-07 13:51:42 +0300

    :sparkles:imports app header module

--- libs/elements/layout/header/app-header-module/src/lib/header/header.component.ts
+++ libs/elements/layout/header/app-header-module/src/lib/header/header.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'elewa-website-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
