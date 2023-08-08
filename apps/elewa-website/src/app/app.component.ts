d7fd0c09284325a8ebbc28febe2841a8d30fb756
Preston <prestonosoro56@gmail.com>
2023-08-09 02:58:03 +0300

    feat: add text contents

--- apps/elewa-website/src/app/app.component.ts
+++ apps/elewa-website/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ContentText } from 'libs/models/schema/ui/texts/content-text.interface';
 
 @Component({
   selector: 'elewa-website-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'elewa-website';
+
+  textContents: ContentText[] = [
+    {
+      title: 'From here to the world',
+      descriptions: [
+        'Getting thousands of people up to speed can be hard and expensive. But it doesn\'t have to be. \n Elewa is an education partner which develops the kind of training that work at scale. This without  losing track of what matters most, people!\nPersonalised, enjoyable and effective training solutons at scale. Technology enabled; Human powered.'
+      ],
+    },
+    {
+      title: 'People at the centre',
+      descriptions: [
+        ' People are the secret to successful education programs. THrough in-depth research, we work with your organisation at different levels to understand ("elewa") how people live and learn.\n Based on decades of research, this understanding allows us to co-create strong learning networks within your organisation. Networks that can be re-used every time a training need arises; At a fraction of the cost of current solutions.',
+      ],
+    },
+  ];
 }
