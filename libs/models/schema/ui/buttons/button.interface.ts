8f66b8acbfb235d1e62eae8c260c4d282b7a12fb
danielsind <danielsindet@gmail.com>
2023-08-08 10:02:59 +0300

    Created the folder models/schema/ui/buttons/ with file button.interface.ts and added all the interface features to be exported

---
+++ libs/models/schema/ui/buttons/button.interface.ts
@@ -0,0 +1,10 @@
+// button interface to be exported by interface module
+
+export interface ButtonData {
+    text: string;
+    bgColor?: string;
+    color?: string;
+    hoverBgColor?: string;
+    hoverColor?: string;
+  }
+  
