c2fa70d403b910574f6d7ac4216320e32034f970
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-08-08 21:13:48 +0300

    chore: add hambuger to toggle nav-links for small screens

--- libs/elements/layout/header/src/lib/components/header/header.component.ts
+++ libs/elements/layout/header/src/lib/components/header/header.component.ts
@@ -1,8 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'elewa-website-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements OnInit {
+  ngOnInit(): void {
+    const hambuger = document.querySelector('.hambuger');
+    const navMenu = document.querySelector('.nav-menu');
+
+    hambuger?.addEventListener('click', () => {
+      hambuger.classList.toggle('active');
+      navMenu?.classList.toggle('active');
+    });
+
+    navMenu?.querySelectorAll('.nav-links').forEach((link) => {
+      console.log(link);
+      link.addEventListener('click', () => {
+        hambuger?.classList.remove('active');
+        navMenu.classList.remove('active');
+      });
+    });
+  }
+}
