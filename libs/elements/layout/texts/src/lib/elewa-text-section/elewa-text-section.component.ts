7ea8098c21593f749a5e631782e00eb44792f469
paul <paulnyoiken@gmail.com>
2023-08-08 21:32:24 +0300

    create text-section component in texts module

---
+++ libs/elements/layout/texts/src/lib/elewa-text-section/elewa-text-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-text-section',
+  templateUrl: './elewa-text-section.component.html',
+  styleUrls: ['./elewa-text-section.component.scss'],
+})
+export class ElewaTextSectionComponent {}
