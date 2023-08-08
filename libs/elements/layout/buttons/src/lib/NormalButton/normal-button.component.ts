f6fd6807d5fc84ed46e0ccd11de075cb998e7c8d
danielsind <danielsindet@gmail.com>
2023-08-08 09:30:52 +0300

    Deleted the NormalButtonComponent folder and all the related files, scss, html, and ts and spec.ts

--- libs/elements/layout/buttons/src/lib/NormalButton/normal-button.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-normal-button',
-  templateUrl: './normal-button.component.html',
-  styleUrls: ['./normal-button.component.scss'],
-})
-export class NormalButtonComponent {}
