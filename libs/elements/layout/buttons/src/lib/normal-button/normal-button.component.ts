37ec1d0f7b627cb47ae8c26ac0029526f81820ab
danielsind <danielsindet@gmail.com>
2023-08-08 10:06:14 +0300

    Added to imports - EventEmiter and Output

--- libs/elements/layout/buttons/src/lib/normal-button/normal-button.component.ts
+++ libs/elements/layout/buttons/src/lib/normal-button/normal-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 // imported the input
 @Component({
   selector: 'elewa-normal-button',
