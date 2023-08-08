ea34f772618d5f6c13d12be064f6a14f457ef379
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-08-08 16:28:21 +0300

    ✨ Feat: Create Reusable Card Component 1. Create elements/cards library to hold the card component. 2. Create the component within the created library.

---
+++ libs/elements/cards/src/lib/elements-cards.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElewaProjectItemCardComponent } from './elewa-project-item-card/elewa-project-item-card.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ElewaProjectItemCardComponent],
+})
+export class ElementsCardsModule {}
