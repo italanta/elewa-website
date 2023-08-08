720e7f58979a1d8664da5132a226191a00f6d55a
kiplangat01 <meshnobie@gmail.com>
2023-08-09 01:04:33 +0300

    ft:initial commit

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './main/home/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
+import { InfoCardComponent } from './info-card/info-card.component';
 
 @NgModule({
   imports: [CommonModule, HomeRoutingModule],
-  declarations: [HomePageComponent],
+  declarations: [HomePageComponent, InfoCardComponent],
   exports: [HomePageComponent],
 })
 export class FeaturesPagesHomeModule {}
---
+++ libs/features/pages/home/src/lib/info-card/info-card.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InfoCardComponent } from './info-card.component';
+
+describe('InfoCardComponent', () => {
+  let component: InfoCardComponent;
+  let fixture: ComponentFixture<InfoCardComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [InfoCardComponent]
+    });
+    fixture = TestBed.createComponent(InfoCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
