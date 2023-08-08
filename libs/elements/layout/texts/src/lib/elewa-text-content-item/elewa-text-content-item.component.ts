a9abba460620e691fef02e923631c2eaffa67a66
Preston <prestonosoro56@gmail.com>
2023-08-09 00:14:35 +0300

    ci: initial the constructor

--- libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component.ts
+++ libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ContentText } from '@libs/models/schema/ui/texts/content-text.interface';
+
 
 @Component({
   selector: 'elewa-website-elewa-text-content-item',
   templateUrl: './elewa-text-content-item.component.html',
   styleUrls: ['./elewa-text-content-item.component.scss'],
 })
-export class ElewaTextContentItemComponent {}
+export class ElewaTextContentItemComponent {
+  @Input() content: ContentText = { title: '', descriptions: [] }; // Initialize here
+
+}
