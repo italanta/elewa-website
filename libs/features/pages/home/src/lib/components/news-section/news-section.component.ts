a26c79087b8ad0125ae175373b0459578d984019
kevinkkimutai <kiruikevinkim@gmail.com>
2023-08-08 17:17:09 +0300

    ✨feat: created news-section component

---
+++ libs/features/pages/home/src/lib/components/news-section/news-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-news-section',
+  templateUrl: './news-section.component.html',
+  styleUrls: ['./news-section.component.scss'],
+})
+export class NewsSectionComponent {}
--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
-
+import { NewsSectionComponent } from './components/news-section/news-section.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [HomeComponent],
-  exports:[HomeComponent]
+  declarations: [HomeComponent, NewsSectionComponent],
+  exports: [HomeComponent],
 })
 export class FeaturesPagesHomeModule {}
