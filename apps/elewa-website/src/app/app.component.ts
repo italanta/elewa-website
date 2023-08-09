52f105e7662671dd3f23c537aeda91f575cece36
Preston <prestonosoro56@gmail.com>
2023-08-09 10:31:54 +0300

    feat: split sentence into paragraph

--- apps/elewa-website/src/app/app.component.ts
+++ apps/elewa-website/src/app/app.component.ts
@@ -13,13 +13,16 @@ export class AppComponent {
     {
       title: 'From here to the world',
       descriptions: [
-        'Getting thousands of people up to speed can be hard and expensive. But it doesn\'t have to be. \n Elewa is an education partner which develops the kind of training that work at scale. This without  losing track of what matters most, people!\nPersonalised, enjoyable and effective training solutons at scale. Technology enabled; Human powered.'
+        'Getting thousands of people up to speed can be hard and expensive. But it doesn\'t have to be.', 
+        'Elewa is an education partner which develops the kind of training that work at scale. This without  losing track of what matters most, people!',
+        'Personalised, enjoyable and effective training solutons at scale. Technology enabled; Human powered.'
       ],
     },
     {
       title: 'People at the centre',
       descriptions: [
-        ' People are the secret to successful education programs. THrough in-depth research, we work with your organisation at different levels to understand ("elewa") how people live and learn.\n Based on decades of research, this understanding allows us to co-create strong learning networks within your organisation. Networks that can be re-used every time a training need arises; At a fraction of the cost of current solutions.',
+        ' People are the secret to successful education programs. Through in-depth research, we work with your organisation at different levels to understand ("elewa") how people live and learn.',
+        ' Based on decades of research, this understanding allows us to co-create strong learning networks within your organisation. Networks that can be re-used every time a training need arises; At a fraction of the cost of current solutions.'
       ],
     },
   ];
