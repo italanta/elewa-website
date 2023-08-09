8a510bb8e3a299d9765c0042c8981f7e42d4a6b9
Oduolperis <oduolpepe618@gmail.com>
2023-08-09 15:47:23 +0300

    Generate a ButtonData interface and add the type inputs

--- libs/elements/layout/buttons/src/lib/button-data.ts
+++ libs/elements/layout/buttons/src/lib/button-data.ts
@@ -1,2 +1,10 @@
-export interface ButtonData {
+export interface ButtonData 
+{
+    text: string
+  
+    bgColor?: string; // Background colour
+    color?: string; // text color
+    
+    hoverBgColor?: string;
+    hoverColor?: string;
 }
