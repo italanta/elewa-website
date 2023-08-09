3e12f61d194b80bae6b5336937858a9ce94ad768
paul <paulnyoiken@gmail.com>
2023-08-09 22:29:48 +0300

    add gap property and media query

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElementsLayoutTextsModule } from '@elewa-website/elements/layout/texts';
 
 import { HomePageComponent } from './main/home/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
-import { ElementsLayoutTextsModule } from '@elewa-website/elements/layout/texts';
 
 @NgModule({
   imports: [CommonModule, HomeRoutingModule, ElementsLayoutTextsModule],
