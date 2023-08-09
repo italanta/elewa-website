a9d8bbdd9f698408093c8969af81fce90a237737
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-09 10:58:11 +0300

    :lipstick: Updated the header component and the styling.

--- libs/elements/layout/header/src/lib/components/header/header.component.ts
+++ libs/elements/layout/header/src/lib/components/header/header.component.ts
@@ -5,4 +5,6 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  // logoPath = 'assets/logo.png';
+}
