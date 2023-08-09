dae81436db61e8b14955042d76eb2e6906643988
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-10 00:21:36 +0300

    feat: created the header and importer icons

--- libs/elements/layout/header/app-header-module/src/lib/header/header.component.ts
+++ libs/elements/layout/header/app-header-module/src/lib/header/header.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+
+  ngOnInit() {
+    // Subscribe to language changes
+    const hamburger = document.querySelector(".hamburger");
+    const navMenu = document.querySelector(".nav-menus");
+    const navLink = document.querySelectorAll(".nav-links");
+
+    const mobileMenu = () => {
+      hamburger?.classList.toggle("active")
+      navMenu?.classList.toggle("active")
+    }
+
+    const closeMenu = () => {
+      hamburger?.classList.remove("active")
+      navMenu?.classList.remove("active")
+    }
+
+    hamburger?.addEventListener("click" , mobileMenu)
+    navLink.forEach((l) => l.addEventListener("click",closeMenu))
+   
+    
+  }
 }
