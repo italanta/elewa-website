1f1756ca89899e120241dc18e22e76b7dfd9c5f0
jbdura <bidurajohnson@gmail.com>
2023-08-09 18:53:26 +0300

    Added responsiveness of medium and small screens

--- libs/elements/layout/header/src/lib/header/header.component.ts
+++ libs/elements/layout/header/src/lib/header/header.component.ts
@@ -10,7 +10,14 @@ export class HeaderComponent {
   * This is a header component class. 
   * With this class, you can link the header section
   */
+
   logoUrl = "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg"
+
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
   constructor () {
     
   }
