c7b778552f2793890527008da63899e4c82d6170
kiplangat01 <meshnobie@gmail.com>
2023-08-09 22:04:06 +0300

    Created Reusable InfoCard Component

--- libs/features/pages/home/src/lib/info-card/info-card.component.ts
+++ libs/features/pages/home/src/lib/info-card/info-card.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'elewa-website-info-card',
-  templateUrl: './info-card.component.html',
+  templateUrl: './info-card.component.html', 
   styleUrls: ['./info-card.component.scss']
 })
 export class InfoCardComponent {
