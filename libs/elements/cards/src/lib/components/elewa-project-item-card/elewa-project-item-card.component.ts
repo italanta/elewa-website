602770440404f018821207ec8a28a38adc065da3
sophia <sophiendalo@gmail.com>
2023-08-10 11:27:18 +0300

    1. generate cards module 2. generate  elewa-project-item-card component

---
+++ libs/elements/cards/src/lib/components/elewa-project-item-card/elewa-project-item-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-project-item-card',
+  templateUrl: './elewa-project-item-card.component.html',
+  styleUrls: ['./elewa-project-item-card.component.scss'],
+})
+export class ElewaProjectItemCardComponent {}
---
+++ libs/elements/cards/src/lib/elements-cards.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElewaProjectItemCardComponent } from './components/elewa-project-item-card/elewa-project-item-card.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ElewaProjectItemCardComponent],
+})
+export class ElementsCardsModule {}
