0001a3c3811b07c1e6d2abe8b17500e30bb05a34
Steve Kariuki <stevekariuki5@gmail.com>
2023-08-10 11:12:00 +0300

    info card component made

---
+++ libs/elements/cards/src/lib/elewa-info-card/elewa-info-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-info-card',
+  templateUrl: './elewa-info-card.component.html',
+  styleUrls: ['./elewa-info-card.component.scss'],
+})
+export class ElewaInfoCardComponent {}
