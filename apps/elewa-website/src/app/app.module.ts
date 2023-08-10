eabeec90e4ddf73de5d5fa7894f70d115ee9e6df
Preston <prestonosoro56@gmail.com>
2023-08-10 08:27:42 +0300

    fix: fix order of domain imports to come after angular imports

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { ElementsLayoutTextsModule } from '@elewa-website/elements/layout/texts'; // Import the module
 import { AppComponent } from './app.component';
 import { TranslocoRootModule } from './transloco-root.module';
 
 import { AppRoutingModule } from './app.routing';
-import { ElementsLayoutTextsModule } from '@elewa-website/elements/layout/texts'; // Import the module
 
 @NgModule({
   declarations: [AppComponent],
