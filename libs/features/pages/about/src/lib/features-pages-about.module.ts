be0e0fd00eda0313f1f64a02aeb8aff6cb191891
Oduolperis <oduolpepe618@gmail.com>
2023-08-10 09:39:58 +0300

    Rename to button.module.ts

--- libs/features/pages/about/src/lib/features-pages-about.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ElewaNormalButtonComponent } from './elewa-normal-button/elewa-normal-button.component';
-
-@NgModule({
-  imports: [CommonModule],
-  declarations: [ElewaNormalButtonComponent],
-})
-export class FeaturesPagesAboutModule {}
