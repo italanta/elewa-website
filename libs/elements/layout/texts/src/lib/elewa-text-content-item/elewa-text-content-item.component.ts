d5f2220444eab7f06546a0913f8f6d9bf40215bc
Preston <prestonosoro56@gmail.com>
2023-08-10 08:35:14 +0300

    feat: write raw data to be reused

--- libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component.ts
+++ libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component.ts
@@ -1,13 +1,28 @@
 import { Component, Input } from '@angular/core';
 import { ContentText } from '../../../../../../models/schema/ui/texts/content-text.interface';
 
-
 @Component({
   selector: 'elewa-website-elewa-text-content-item',
   templateUrl: './elewa-text-content-item.component.html',
-  styleUrls: ['./elewa-text-content-item.component.scss'],
+  styleUrls: ['./elewa-text-content-item.component.scss']
 })
 export class ElewaTextContentItemComponent {
-  @Input() content: ContentText = { title: '', descriptions: [] }; // Initialize here
-
+  @Input() content: ContentText = { title: '', descriptions: [] };
+  textContents: ContentText[] = [
+      {
+        title: 'From here to the world',
+        descriptions: [
+          'Getting thousands of people up to speed can be hard and expensive. But it doesn\'t have to be.', 
+          'Elewa is an education partner which develops the kind of training that work at scale. This without  losing track of what matters most, people!',
+          'Personalised, enjoyable and effective training solutons at scale. Technology enabled; Human powered.'
+        ],
+      },
+      {
+        title: 'People at the centre',
+        descriptions: [
+          ' People are the secret to successful education programs. Through in-depth research, we work with your organisation at different levels to understand ("elewa") how people live and learn.',
+          ' Based on decades of research, this understanding allows us to co-create strong learning networks within your organisation. Networks that can be re-used every time a training need arises; At a fraction of the cost of current solutions.'
+        ],
+      },
+    ];
 }
