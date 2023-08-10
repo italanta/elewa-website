536f9051c06557eec132697f7832a51185c545a4
patrick kisyula <pmusyimi42@gmail.com>
2023-08-10 15:03:29 +0300

    imported app header module

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AppHeaderModule} from '@elewa-website/elements/layout/header'
 import { HomePageComponent } from './main/home/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, AppHeaderModule ],
   declarations: [HomePageComponent],
   exports: [HomePageComponent],
 })
