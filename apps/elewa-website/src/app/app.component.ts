cbc7db98333ed3f8cadc816fb0501fc45e63e313
Preston <prestonosoro56@gmail.com>
2023-08-10 08:26:16 +0300

    feat: erase the previous submitted data

--- apps/elewa-website/src/app/app.component.ts
+++ apps/elewa-website/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { ContentText } from 'libs/models/schema/ui/texts/content-text.interface';
 
 @Component({
   selector: 'elewa-website-root',
@@ -8,22 +7,4 @@ import { ContentText } from 'libs/models/schema/ui/texts/content-text.interface'
 })
 export class AppComponent {
   title = 'elewa-website';
-
-  textContents: ContentText[] = [
-    {
-      title: 'From here to the world',
-      descriptions: [
-        'Getting thousands of people up to speed can be hard and expensive. But it doesn\'t have to be.', 
-        'Elewa is an education partner which develops the kind of training that work at scale. This without  losing track of what matters most, people!',
-        'Personalised, enjoyable and effective training solutons at scale. Technology enabled; Human powered.'
-      ],
-    },
-    {
-      title: 'People at the centre',
-      descriptions: [
-        ' People are the secret to successful education programs. Through in-depth research, we work with your organisation at different levels to understand ("elewa") how people live and learn.',
-        ' Based on decades of research, this understanding allows us to co-create strong learning networks within your organisation. Networks that can be re-used every time a training need arises; At a fraction of the cost of current solutions.'
-      ],
-    },
-  ];
 }
