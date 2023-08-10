e8c2d8ecdd6c2b1aa449913a4ac7962c5fae4a86
Mickey254-ja <mickey.awino@student.moringaschool.com>
2023-08-10 12:18:04 +0300

    ✨ feat:generate header

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { TranslocoRootModule } from './transloco-root.module';
 import { FeaturesPagesHomeModule } from '@elewa-website/features/pages/home';
+import { ElementsLayoutHeaderModule } from '@elewa-website/elements/layout/header';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, FeaturesPagesHomeModule],
+  imports: [BrowserModule, FeaturesPagesHomeModule,ElementsLayoutHeaderModule],
   providers: [
     HttpClientModule,
     TranslocoRootModule
