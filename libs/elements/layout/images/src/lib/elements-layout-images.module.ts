947fde6c5c8fd0fe8d9efc75ba845607b10ccea8
T-erry <terrywachuka10@gmail.com>
2023-08-10 10:04:16 +0300

    created a image component

---
+++ libs/elements/layout/images/src/lib/elements-layout-images.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElewaImageContainerComponent } from './elewa-image-container/elewa-image-container.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ElewaImageContainerComponent],
+})
+export class ElementsLayoutImagesModule {}
