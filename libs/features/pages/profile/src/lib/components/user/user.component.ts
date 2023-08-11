680059e7cfe53f74a00e606f0a9a972d80390625
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-11 13:02:07 +0300

    :monocle_face: feat: import and use mock data for user profile

--- libs/features/pages/profile/src/lib/components/user/user.component.ts
+++ libs/features/pages/profile/src/lib/components/user/user.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { __user as mockUser } from '@elewa-website/data/ui/user-profile';
+import { Profile } from '@elewa-website/models/schemas/ui/profile';
 
 @Component({
   selector: 'elewa-website-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss'],
 })
-export class UserComponent {}
+export class UserComponent implements OnInit {
+  @Input() user!:Profile;
+
+  ngOnInit() {
+    this.user = mockUser
+  }
+}
