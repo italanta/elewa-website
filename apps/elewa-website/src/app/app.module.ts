c73b5cb7e2101aa37c78123c4e91f9dbd6567bc2
danielsind <danielsindet@gmail.com>
2023-08-11 11:13:51 +0300

    Imported module ElementsLayoututtonsModule

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -6,10 +6,11 @@ import { FeaturesPagesHomeModule } from '@elewa-website/features/pages/home';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { TranslocoRootModule } from './transloco-root.module';
+import { ElementsLayoutButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule, FeaturesPagesHomeModule],
+  imports: [BrowserModule, FeaturesPagesHomeModule, ElementsLayoutButtonsModule],
   providers: [
     HttpClientModule,
     TranslocoRootModule
