3ee2da5a600699544221500cbb8556c3e112e7d6
Steve Kariuki <stevekariuki5@gmail.com>
2023-08-11 12:59:13 +0300

    created schema/ui/cards and exported interface

--- libs/elements/cards/src/lib/elewa-info-card/elewa-info-card.component.ts
+++ libs/elements/cards/src/lib/elewa-info-card/elewa-info-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input } from '@angular/core';
-import { InfoCard } from '@elewa-website/models/schema/ui/cards/info-card.interface';
+import { InfoCard } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-elewa-info-card',
