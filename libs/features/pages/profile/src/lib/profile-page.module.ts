6f8324a93f51ff8cf1f659485c43c6983a41ae11
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-11 12:02:41 +0300

    :sparkles: feat: import components created and mark as declarations

--- libs/features/pages/profile/src/lib/profile-page.module.ts
+++ libs/features/pages/profile/src/lib/profile-page.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { UserComponent } from './components/user/user.component';
+import { UserSettingsComponent } from './components/user-settings/user-settings.component';
 import { MainProfileComponent } from './main/main-profile/main-profile.component';
 
+import { ProfileRoutesModule } from './profile.routing';
+
 @NgModule({
-  imports: [CommonModule],
-  declarations: [UserComponent, MainProfileComponent],
+  imports: [CommonModule, ProfileRoutesModule],
+  declarations: [UserComponent, MainProfileComponent, UserSettingsComponent],
 })
 export class ProfilePageModule {}
