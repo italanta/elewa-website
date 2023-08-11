35c7fcb82a37517db1420129a9754aa2c990e7f0
jbdura <bidurajohnson@gmail.com>
2023-08-11 09:43:40 +0300

    Edited the ts component file

--- libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component.ts
+++ libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { ContentText } from '@elewa-website/elements/layout/texts';
 
 @Component({
   selector: 'elewa-website-elewa-text-content-item',
   templateUrl: './elewa-text-content-item.component.html',
-  styleUrls: ['./elewa-text-content-item.component.scss'],
+  styleUrls: [ './elewa-text-content-item.component.scss' ],
 })
-export class ElewaTextContentItemComponent {}
+export class ElewaTextContentItemComponent {
+
+  @Input() content: ContentText;
+
+}
