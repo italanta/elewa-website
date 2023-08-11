63b1aeef12d37894a6a753f02b3e4ea1d3bd8cd5
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-11 12:04:39 +0300

    :sparkles: feat: add settings route to profile page

--- libs/features/pages/profile/src/lib/profile.routing.ts
+++ libs/features/pages/profile/src/lib/profile.routing.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainProfileComponent } from './main/main-profile/main-profile.component';
+import { UserSettingsComponent } from './components/user-settings/user-settings.component';
 
 export const PROFILE_ROUTES: Routes = [
-  { path: '', component: MainProfileComponent }
+  { path: '', component: MainProfileComponent },
+
+  { path: 'settings', component: UserSettingsComponent }
 ];
 
 @NgModule({
