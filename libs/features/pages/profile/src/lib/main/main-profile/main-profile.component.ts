d2cd1ec27f09e9873f182c65a3dc248e8fc232ae
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-11 11:44:24 +0300

    :sparkles: feat: create main-profile component

---
+++ libs/features/pages/profile/src/lib/main/main-profile/main-profile.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-main-profile',
+  templateUrl: './main-profile.component.html',
+  styleUrls: ['./main-profile.component.scss'],
+})
+export class MainProfileComponent {}
--- libs/features/pages/profile/src/lib/profile-page.module.ts
+++ libs/features/pages/profile/src/lib/profile-page.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './components/user/user.component';
+import { MainProfileComponent } from './main/main-profile/main-profile.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [UserComponent],
+  declarations: [UserComponent, MainProfileComponent],
 })
 export class ProfilePageModule {}
