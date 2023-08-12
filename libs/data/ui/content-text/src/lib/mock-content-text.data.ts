ab4b172022c438a3b55aa531778e566ddc64c764
thadeus <thadeusnyariki94@gmail.com>
2023-08-12 20:31:05 +0300

    :package: feat: Mock data

--- libs/data/ui/content-text/src/lib/mock-content-text.data.ts
+++ libs/data/ui/content-text/src/lib/mock-content-text.data.ts
@@ -2,11 +2,18 @@ import { ContentText } from '@elewa-website/models/schema/ui/texts';
 
 export const _content: ContentText[] = [
   {
-    title: 'Example Title 1',
-    descriptions: ['Description 1', 'Description 2', 'Description 3'],
+    title: 'From here to the world',
+    descriptions: [
+      "Getting thousands of people up to speed can be hard and expensive. But, it doesn't have to be",
+      'Elewa is an education partner which develops the kind of training that works at scale. This without losing track of what matters most, people!',
+      'Personalized, enjoyable and effective training solutions at scale. Technology enabled; Human powered.',
+    ],
   },
   {
-    title: 'Example Title 2',
-    descriptions: ['Description 1', 'Description 2', 'Description 3'],
+    title: 'People at the center',
+    descriptions: [
+      'People are the secret to successful education programs.',
+      'Through in-depth research, we work with your organization at different levels to understand how people live and learn. Based on decades of research, this understanding allows us to co-create strong learning networks within your organization. Networks that can be reused every time a training need arises; At a fraction of the cost of current solutions.',
+    ],
   },
 ];
