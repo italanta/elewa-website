0c25bc49d8300d97eed9872ba7fecbd54f9ffa57
jbdura <bidurajohnson@gmail.com>
2023-08-12 21:09:05 +0300

    Edited the app html component. deleted the header selector

--- libs/elements/layout/header/src/lib/header/header.component.ts
+++ libs/elements/layout/header/src/lib/header/header.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 
+
+// url for the logo
+const logoUrl: string = "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg"
+
 @Component({
   selector: 'elewa-website-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
+
   /** 
   * This is a header component class. 
   * With this class, you can link the header section
   */
+export class HeaderComponent {
 
-  // url for the logo
-  logoUrl = "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690297/elewa-group-website/Icons/SVG/_Logo/Logo_White_arvoyx.svg"
+  LOGO_URL = logoUrl;
+  
 
   // To check if navigation menu is open or not
   isMenuOpen = false;
@@ -20,7 +25,7 @@ export class HeaderComponent {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
-  constructor () {
-    
-  }
+
 }
+
+// The selector <elewa-website-header> </elewa-website-header> should be added on top of each page
