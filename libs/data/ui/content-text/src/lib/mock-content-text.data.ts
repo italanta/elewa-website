6fe2ccd72aaff68679b09ade3090b296915d79c4
thadeus <thadeusnyariki94@gmail.com>
2023-08-12 19:28:45 +0300

    :package: feat: created mock data

---
+++ libs/data/ui/content-text/src/lib/mock-content-text.data.ts
@@ -0,0 +1,6 @@
+import { ContentText } from '@elewa-website/models/schema/ui/texts';
+
+export const _content: ContentText = {
+  title: 'Example Title',
+  descriptions: ['Description 1', 'Description 2', 'Description 3'],
+};
