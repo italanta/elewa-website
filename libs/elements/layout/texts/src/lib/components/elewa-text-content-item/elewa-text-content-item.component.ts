51f9c9d72176ff9a62b1121b6b1199d665460a51
thadeus <thadeusnyariki94@gmail.com>
2023-08-12 18:53:56 +0300

    :package: feat: generated elewa-text-content-item component

---
+++ libs/elements/layout/texts/src/lib/components/elewa-text-content-item/elewa-text-content-item.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-text-content-item',
+  templateUrl: './elewa-text-content-item.component.html',
+  styleUrls: ['./elewa-text-content-item.component.scss'],
+})
+export class ElewaTextContentItemComponent {}
