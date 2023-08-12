8bceb463cea8036ac1d595f19508694a368c7fab
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-08-12 14:08:24 +0300

    :sparkles: feat: add english and spanish translations to the header

--- libs/elements/layout/header/src/lib/app-header.module.ts
+++ libs/elements/layout/header/src/lib/app-header.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TranslocoModule } from '@ngneat/transloco'
 
 import { HeaderComponent } from './components/header/header.component';
 import { ToggleMenuDirective } from './directives/toggle-menu.directive';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, TranslocoModule],
   declarations: [HeaderComponent, ToggleMenuDirective],
   exports: [HeaderComponent],
 })
