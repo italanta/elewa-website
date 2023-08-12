61749f695b1f3bc3afd6351cff700c4b9bc4eafd
dancunrandy <dancunrandy@gmail.com>
2023-08-12 17:50:14 +0300

    removed errors

--- apps/elewa-website/src/app/app.component.ts
+++ apps/elewa-website/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'elewa-website-slider-button',
+  selector: 'elewa-website-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
