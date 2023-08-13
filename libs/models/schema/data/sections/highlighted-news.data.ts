a98d6d6b59838d64b7536d1238bb5560fbfcb65b
MarkBrian <kariukimarkbrian@gmail.com>
2023-08-13 18:02:55 +0300

    feat: :sparkles: added my mock data

---
+++ libs/models/schema/data/sections/highlighted-news.data.ts
@@ -0,0 +1,19 @@
+import { NewsItem } from "../../ui/cards/news-item.interface";
+
+export  const __highlightedNews: NewsItem[]=[
+    {
+        title: 'Content development for sustainable future',
+        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit ut in, nibh suspendisse sapien natoque interdum molestie feugiat turpis.',
+        buttonText: 'Enviroment',
+    },
+    {
+        title: 'Introducing Conversational learning ipsum dolar',
+        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit ut in, nibh suspendisse sapien natoque interdum molestie feugiat turpis.',
+        buttonText: 'Education',
+    },
+    {
+        title: 'Large scale training for healthcare professinals',
+        description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit ut in, nibh suspendisse sapien natoque interdum molestie feugiat turpis.',
+        buttonText: 'HealthCare',
+    }
+]
