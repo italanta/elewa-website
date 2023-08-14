49a0036228e6e80aaa4479ad0efcadcc2c8fc93a
+ <+>
2023-08-14 20:45:02 +0300

    :sparkles: created elewa-info-card component

--- libs/elements/cards/src/lib/elements-cards.module.ts
+++ libs/elements/cards/src/lib/elements-cards.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaInfoCardComponent],
 })
-export class ElementsCardsModule {}
+export class CardsModule {}
