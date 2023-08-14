3ea842941af943e846e5bff47c499b95b40c4d09
patrick kisyula <pmusyimi42@gmail.com>
2023-08-14 15:00:12 +0300

    :sparkles: feat: created mock data

---
+++ libs/models/schema/ui/cards/cards-module/src/lib/data/sections/highlighted-news.data.ts
@@ -0,0 +1,14 @@
+import { NewsItem } from "../../..";
+export const __highlightedNews: NewsItem[] = [
+  {
+    title: 'Breaking News 1',
+    description: 'This is a brief description of breaking news 1...',
+    buttonText: 'Read more'
+  },
+  {
+    title: 'Breaking News 2',
+    description: 'This is a brief description of breaking news 2...',
+    buttonText: 'Read more'
+  },
+  // Add more news items
+];
