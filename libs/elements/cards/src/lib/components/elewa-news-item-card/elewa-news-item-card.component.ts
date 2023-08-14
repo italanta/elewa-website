ad453f5ac08423373c50c77b9b2f362b3e2994db
patrick kisyula <pmusyimi42@gmail.com>
2023-08-14 15:55:32 +0300

    :bulb: feat: added comments to my news item typescript file

--- libs/elements/cards/src/lib/components/elewa-news-item-card/elewa-news-item-card.component.ts
+++ libs/elements/cards/src/lib/components/elewa-news-item-card/elewa-news-item-card.component.ts
@@ -1,13 +1,19 @@
+// Importing necessary components and modules from Angular and other sources
 import { Component, Input} from '@angular/core';
 import { NewsItem } from '@elewa-website/models/schema/ui/cards/cards-module';
 import { __highlightedNews } from '@elewa-website/models/schema/ui/cards/cards-module';
 
+// Defining a new Angular component named ElewaNewsItemCardComponent
 @Component({
   selector: 'elewa-website-elewa-news-item-card',
   templateUrl: './elewa-news-item-card.component.html',
   styleUrls: ['./elewa-news-item-card.component.scss'],
 })
 export class ElewaNewsItemCardComponent {
+
+  // Declaring an input property named 'item'
   @Input() item!: NewsItem
+
+  // Creating an array 'newsItem' to store highlighted news items
   newsItem: NewsItem[] = __highlightedNews;
 }
