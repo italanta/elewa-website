45b1bda8405968c4af6e9be4deaec2b541044111
sainamercy <sainamercy@gmail.com>
2023-08-14 10:31:44 +0300

    📦️ create slider button component

---
+++ libs/elements/layout/buttons/src/lib/components/elewa-slider-button/elewa-slider-button.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-slider-button',
+  templateUrl: './elewa-slider-button.component.html',
+  styleUrls: ['./elewa-slider-button.component.scss'],
+})
+export class ElewaSliderButtonComponent {}
--- libs/elements/layout/buttons/src/lib/elements-layout-buttons.module.ts
+++ libs/elements/layout/buttons/src/lib/elements-layout-buttons.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaSliderButtonComponent } from './components/elewa-slider-button/elewa-slider-button.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaSliderButtonComponent],
+  exports: [ElewaSliderButtonComponent],
 })
 export class ButtonsModule {}
