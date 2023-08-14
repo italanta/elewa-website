ba212dc99836d1df1e8d62fe11a62a921cec6423
thadeus <thadeusnyariki94@gmail.com>
2023-08-14 18:36:34 +0300

    :sparkles: chore: imported MaterialModule to CardsModule

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ElewaInfoCardComponent } from './components/elewa-info-card/elewa-info-card.component';
 
 import { CardsRoutesModule } from './cards.routing';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  imports: [CommonModule, CardsRoutesModule],
+  imports: [CommonModule, CardsRoutesModule, MaterialModule],
   declarations: [ElewaInfoCardComponent]
 })
 export class CardsModule {}
