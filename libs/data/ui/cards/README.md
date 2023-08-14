c62b40b173433d41da426b3178170feb2a8479f6
thadeus <thadeusnyariki94@gmail.com>
2023-08-14 17:56:49 +0300

    :sparkles: feat: Created mock-cards data

---
+++ libs/data/ui/cards/README.md
@@ -0,0 +1,7 @@
+# data-ui-cards
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test data-ui-cards` to execute the unit tests.
---
+++ libs/data/ui/cards/src/lib/mock-cards.data.ts
@@ -0,0 +1,10 @@
+import { InfoCard } from "@elewa-website/models/schema/ui/cards";
+
+export const _infoCard: InfoCard = [
+  {
+    title: 'Course Design',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor',
+    icon: 'home',
+  },
+];
