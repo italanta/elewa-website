e043f508b271a186a56ff790e872b1746fa5b818
thadeus <thadeusnyariki94@gmail.com>
2023-08-15 22:15:28 +0300

    :art: fixed the Order of imports in AppModule

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppComponent } from './app.component';
 import { TranslocoRootModule } from './transloco-root.module';
-
 import { AppRoutingModule } from './app.routing';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [AppComponent],
