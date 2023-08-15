9c1e85c55894641a41fc85de78f592f5133c1713
+ <+>
2023-08-15 11:18:53 +0300

    :sparkles: created texts module

---
+++ libs/elements/layout/texts/src/lib/elements-layout-texts.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElewaTextContentItemComponent } from './elewa-text-content-item/elewa-text-content-item.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ElewaTextContentItemComponent],
+})
+export class TextsModule {}
