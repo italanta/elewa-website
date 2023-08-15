ae76af19d39e8a849d795e699b6765cc4620f513
thadeus <thadeusnyariki94@gmail.com>
2023-08-15 21:57:44 +0300

    :fire: removed CardsRouting modile

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ElewaInfoCardComponent } from './components/elewa-info-card/elewa-info-card.component';
 
-import { CardsRoutesModule } from './cards.routing';
 import { MaterialModule } from './material.module';
 
 @NgModule({
-  imports: [CommonModule, CardsRoutesModule, MaterialModule],
+  imports: [CommonModule, MaterialModule],
   declarations: [ElewaInfoCardComponent]
 })
 export class CardsModule {}
