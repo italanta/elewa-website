6ecca778e04eb8d2c2eaff8a98de5edb862188aa
thadeus <thadeusnyariki94@gmail.com>
2023-08-15 22:31:17 +0300

    :art: removed MaterialModule and Added MatIconModule to CardsModule

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 import { ElewaInfoCardComponent } from './components/elewa-info-card/elewa-info-card.component';
 
-import { MaterialModule } from './material.module';
+const MaterialComponents = [
+  MatIconModule
+];
 
 @NgModule({
-  imports: [CommonModule, MaterialModule],
-  declarations: [ElewaInfoCardComponent]
+  imports: [CommonModule, MaterialComponents],
+  declarations: [ElewaInfoCardComponent],
 })
 export class CardsModule {}
