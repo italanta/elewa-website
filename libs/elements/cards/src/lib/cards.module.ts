dc84b9e5a1edce993d049ed4830a7790e50c1cc5
jbdura <bidurajohnson@gmail.com>
2023-08-16 09:23:32 +0300

    Created elewa-project-item-card component

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
+import { ElewaProjectItemCardComponent } from './elewa-project-item-card/elewa-project-item-card.component';
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaInfoCardComponent],
-  exports: [ElewaInfoCardComponent]
+  declarations: [ElewaInfoCardComponent, ElewaProjectItemCardComponent],
+  exports: [ElewaInfoCardComponent],
 })
 export class CardsModule {}
