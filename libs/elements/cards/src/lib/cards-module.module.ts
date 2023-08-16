181812dc0048fc579473ba8cd5bb908198b237c2
patrick kisyula <pmusyimi42@gmail.com>
2023-08-16 09:06:27 +0300

    :truck: feat: renamed CardsModule to cards-module

---
+++ libs/elements/cards/src/lib/cards-module.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElewaNewsItemCardComponent } from './components/elewa-news-item-card/elewa-news-item-card.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ElewaNewsItemCardComponent],
+  exports: [ElewaNewsItemCardComponent]
+})
+export class CardsModule {}
