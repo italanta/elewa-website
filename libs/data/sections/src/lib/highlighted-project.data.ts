ce266734afbe7475a9ec3e5733c315fa107fbbb4
paul <ochiengpaul001@gmail.com>
2023-08-16 11:44:03 +0300

    Feature: Initial Commit 1. generate libraries; data/sections, cards 2. create missing routes on tsconfig.json 3. create mock data on the highlighted-project.data.ts

---
+++ libs/data/sections/src/lib/highlighted-project.data.ts
@@ -0,0 +1,12 @@
+import {ProjectItem } from 'libs/models/sections/projects/src/lib/project-item.interface'
+
+
+export const __highlightedProjects: ProjectItem[] = [
+
+  {
+    title : "project1",
+    description:" very enthusiastic about learning angular",
+    tag: "tag1", 
+    imgSrc: "path/image.jpg",
+  }
+]
