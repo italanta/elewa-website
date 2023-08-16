0edea97011d5d8526be0a33523a94e3784188b72
paul <ochiengpaul001@gmail.com>
2023-08-16 14:47:32 +0300

    :cookie: Render data on browser

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ElementsCardsModule } from '@elewa-website/elements/cards';
+
 import { HomePageComponent } from './main/home/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, ElementsCardsModule],
   declarations: [HomePageComponent],
   exports: [HomePageComponent],
 })
