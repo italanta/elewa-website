09599aede9da72b602bf18a379d2985bd3c03b74
levy <lnjiru43@gmail.com>
2023-08-16 10:03:27 +0300

    :sparkles: generated slider button interface type

---
+++ libs/models/schema/ui/buttons/src/lib/slider-button-data.interface.ts
@@ -0,0 +1,16 @@
+export interface SliderButtonData
+{
+  text: string;
+
+  bgColour?: string; // Background colour
+  color?: string;
+  borderColor?: string;
+  iconColor?: string;
+  iconBackgroundColor?: string;
+
+  hoverBgColour?: string;
+  hoverColor?: string;
+  hoverBorderColor?: string;
+  hoverIconColor?: string;
+  hoverIconBackgroundColor?: string;
+}
