bd37c1a6fdfe0dbbe345cfa8019f934d2f6d94de
paul <ochiengpaul001@gmail.com>
2023-08-16 16:10:36 +0300

    mutiple imgSrc sets

--- libs/data/sections/src/lib/highlighted-project.data.ts
+++ libs/data/sections/src/lib/highlighted-project.data.ts
@@ -8,5 +8,17 @@ export const __highlightedProjects: ProjectItem[] = [
     description:"Very enthusiastic about learning angular, I have already learned html, css, react, js, ruby ,ts",
     tag: "education", 
     imgSrc: "https://images.ctfassets.net/cnu0m8re1exe/8mNFyn7Fn15xx8g3lygAp/99f4534bd491f7259fea75f36c67700b/shutterstock_618062045.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill",
+  }, 
+  {
+    title : "Introducing peer learning ipsum dolar",
+    description:"Very enthusiastic about learning angular, I have already learned html, css, react, js, ruby ,ts",
+    tag: "education", 
+    imgSrc: "https://images.ctfassets.net/cnu0m8re1exe/8mNFyn7Fn15xx8g3lygAp/99f4534bd491f7259fea75f36c67700b/shutterstock_618062045.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill",
+  },
+  {
+    title : "Introducing peer learning ipsum dolar",
+    description:"Very enthusiastic about learning angular, I have already learned html, css, react, js, ruby ,ts",
+    tag: "education", 
+    imgSrc: "https://images.ctfassets.net/cnu0m8re1exe/8mNFyn7Fn15xx8g3lygAp/99f4534bd491f7259fea75f36c67700b/shutterstock_618062045.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill",
   }
 ]
