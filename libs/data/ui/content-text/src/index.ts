156013e944c794a87d3337b07bab0daac68be1f7
+ <+>
2023-08-16 10:23:00 +0300

    :sparkles: created the data moule

---
+++ libs/data/ui/content-text/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/data-ui-content-text.module';
--- libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component.ts
+++ libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { ContentText } from '@elewa-website/models/schema/ui/texts';
 
 @Component({
   selector: 'elewa-website-elewa-text-content-item',
   templateUrl: './elewa-text-content-item.component.html',
   styleUrls: ['./elewa-text-content-item.component.scss'],
 })
-export class ElewaTextContentItemComponent {}
+export class ElewaTextContentItemComponent {
+  @Input() textContent!: ContentText [] 
+}
