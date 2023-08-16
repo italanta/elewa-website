10cfc729a3b0f7b91e90130d1786f196ef806851
Randy Kipkurui <randy.kipkurui4@gmail.com>
2023-08-16 10:30:22 +0300

    🎨refactor: Remove the use of highlightedProjects in the project item card component.

--- libs/elements/cards/src/lib/elewa-project-item-card/elewa-project-item-card.component.ts
+++ libs/elements/cards/src/lib/elewa-project-item-card/elewa-project-item-card.component.ts
@@ -1,9 +1,6 @@
 import { Component, Input } from '@angular/core';
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
 import { ProjectItem } from '@elewa-website/models/sections/projects';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { __highlightedProjects } from '@elewa-website/data/sections';
-
 
 @Component({
   selector: 'elewa-website-elewa-project-item-card',
@@ -11,17 +8,7 @@ import { __highlightedProjects } from '@elewa-website/data/sections';
   styleUrls: ['./elewa-project-item-card.component.scss'],
 })
 
-/* A card that displays an image and its description based on
-the values passed to the card. The card should be reusable */
 export class ElewaProjectItemCardComponent {
-  /** Card info to be passed in */
-  @Input() project: ProjectItem = {
-    title: '',
-    description: '',
-    tag: '',
-    imgSrc: '',
-  };
-
-  // incorporate the mock projects data
-  highlightedProjects: ProjectItem[] = __highlightedProjects;
+  /** Use the '!' to determine whether there is data to be put in. */
+  @Input() project!: ProjectItem;
 }
