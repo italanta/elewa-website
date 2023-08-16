dba94c56831250b46610f8276a14917b471b39a4
sainamercy <sainamercy@gmail.com>
2023-08-16 19:04:50 +0300

    🔧 fix dynamic hover effect

--- libs/elements/layout/buttons/src/lib/components/elewa-slider-button/elewa-slider-button.component.ts
+++ libs/elements/layout/buttons/src/lib/components/elewa-slider-button/elewa-slider-button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
 
 @Component({
@@ -6,6 +6,29 @@ import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
   templateUrl: './elewa-slider-button.component.html',
   styleUrls: ['./elewa-slider-button.component.scss'],
 })
-export class SliderButtonComponent {
+export class SliderButtonComponent implements OnInit {
+  isHovered = false;
+  btnBackgroundColor!: string;
+  iconBackgroundColor!: string;
   @Input() buttonData?: SliderButtonData;
+
+  ngOnInit(): void {
+    this.setBgColor();
+    this.setIconColor();
+  }
+
+  setBgColor() {
+    if (this.isHovered) {
+      this.btnBackgroundColor = this.buttonData?.hoverBorderColor as string;
+    } else {
+      this.btnBackgroundColor = this.buttonData?.bgColor as string;
+    }
+  }
+  setIconColor() {
+    if (this.isHovered) {
+      this.iconBackgroundColor = this.buttonData?.hoverIconColor as string;
+    } else {
+      this.iconBackgroundColor = this.buttonData?.iconColor as string;
+    }
+  }
 }
