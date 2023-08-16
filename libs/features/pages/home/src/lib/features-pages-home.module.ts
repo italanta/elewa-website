e7a42ddcd655cddb026de7e9cda6bacd331c2aa7
OgembaJr <manuogemba@gmail.com>
2023-08-16 12:14:16 +0300

    ✨ feat: imported the interface HeroText that allows dynamic rendering of HeroText objects within the component template

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './main/home/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, TextsModule],
   declarations: [HomePageComponent],
   exports: [HomePageComponent],
 })
