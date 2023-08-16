f1a216aeb78c997ba8f834d0ecbc7d1f8d9b8dd3
OgembaJr <manuogemba@gmail.com>
2023-08-16 11:19:58 +0300

    ✨ feat: added a new component in TextModule named elewa-text-section

--- libs/elements/layout/texts/src/lib/texts.module.ts
+++ libs/elements/layout/texts/src/lib/texts.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaTextSectionComponent } from './elewa-text-section/elewa-text-section.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaTextSectionComponent],
+  exports: [ElewaTextSectionComponent]
 })
 export class TextsModule {}
