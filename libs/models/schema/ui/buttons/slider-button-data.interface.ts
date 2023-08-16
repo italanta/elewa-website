9dc86408e6fde4c44ad135072131abab6a9e5210
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-16 09:21:43 +0300

    :lipstick: implemented the slider button interface

---
+++ libs/models/schema/ui/buttons/slider-button-data.interface.ts
@@ -0,0 +1,13 @@
+export interface SliderButtonData {
+    text: string;
+    bgColour?: string;
+    color?: string;
+    borderColor?: string;
+    iconColor?: string;
+    iconBackgroundColor?: string;
+    hoverBgColour?: string;
+    hoverColor?: string;
+    hoverBorderColor?: string;
+    hoverIconColor?: string;
+    hoverIconBackgroundColor?: string;
+}
