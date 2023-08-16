04bd16e315a6d17f066fc91ffe8103d8d156f70f
patrick kisyula <pmusyimi42@gmail.com>
2023-08-16 11:40:03 +0300

    :sparkles: feat: added a new module in data/sections

---
+++ libs/models/data/sections/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/highlighted-news.data';
---
+++ libs/models/data/sections/src/lib/highlighted-news.data.ts
@@ -0,0 +1,14 @@
+import { NewsItem } from '@elewa-website/models/schema/ui/cards/cards-module';
+export const __highlightedNews: NewsItem[] = [
+  {
+    title: 'Breaking News 1',
+    description: 'This is a brief description of breaking news 1...',
+    buttonText: 'Read more'
+  },
+  {
+    title: 'Breaking News 2',
+    description: 'This is a brief description of breaking news 2...',
+    buttonText: 'Read more'
+  },
+  // Add more news items
+];
