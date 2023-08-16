635c46019e286b10f6fbe7ab952b9f2e84dfab76
paul <ochiengpaul001@gmail.com>
2023-08-16 15:04:45 +0300

    revert addons

--- libs/elements/cards/src/lib/elewa-project-item-card/elewa-project-item-card.component.ts
+++ libs/elements/cards/src/lib/elewa-project-item-card/elewa-project-item-card.component.ts
@@ -1,16 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ProjectItem } from '@elewa-website/models/cards';
-import { __highlightedProjects } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-elewa-project-item-card',
   templateUrl: './elewa-project-item-card.component.html',
   styleUrls: ['./elewa-project-item-card.component.scss'],
 })
-export class ElewaProjectItemCardComponent implements OnInit {
-  @Input( ) project!: ProjectItem
-
-  ngOnInit () {
-    this.project = __highlightedProjects[0]
-  }
+export class ElewaProjectItemCardComponent {
+  @Input() project!: ProjectItem;
 }
--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ElementsCardsModule } from '@elewa-website/elements/cards';
 
 import { HomePageComponent } from './main/home/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, ElementsCardsModule],
+  imports: [CommonModule, HomeRoutingModule],
   declarations: [HomePageComponent],
   exports: [HomePageComponent],
 })
