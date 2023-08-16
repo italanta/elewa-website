c880325c399dc7d14ee61286326dab36b629ff95
Darylcosm0 <mutaidaryl@gmail.com>
2023-08-16 12:24:44 +0300

    feat: added moke news

---
+++ libs/data/sections/src/lib/highlighted-news.data.ts
@@ -0,0 +1,9 @@
+import { NewsItem } from "@elewa-website/models/schema/ui/cards";
+
+export const __highlightedNews: NewsItem [] = [
+    {
+        title: 'conversational learning ipsum dolar ',
+        description:'eos qui ratione sequi nescuint',
+        buttonText:'Read more'
+    }
+]
