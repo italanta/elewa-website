e6c82d2e3842a33161d939ff4ba722a02fa28e9d
jbdura <bidurajohnson@gmail.com>
2023-08-16 13:29:40 +0300

    Comments and clean up of redundant data

--- libs/elements/cards/src/lib/elewa-project-item-card/elewa-project-item-card.component.ts
+++ libs/elements/cards/src/lib/elewa-project-item-card/elewa-project-item-card.component.ts
@@ -1,18 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ProjectItem } from '@elewa-website/models/sections/projects/cards'
 
-import { __highlightedProjects } from '@elewa-website/data/sections'
-
 @Component({
   selector: 'elewa-website-elewa-project-item-card',
   templateUrl: './elewa-project-item-card.component.html',
   styleUrls: ['./elewa-project-item-card.component.scss'],
 })
 export class ElewaProjectItemCardComponent implements OnInit {
+  
+  // Import of the interface
   @Input() project!: ProjectItem;
-
-  ngOnInit() {
-    this.project = __highlightedProjects[0]
-  }
 
 }
