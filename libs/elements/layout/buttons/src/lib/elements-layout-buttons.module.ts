fdf35254a09c51aa64c007db63dc95fa8ac95960
levy <lnjiru43@gmail.com>
2023-08-16 09:39:54 +0300

    :sparkles: created elewa-slider-button component

--- libs/elements/layout/buttons/src/lib/elements-layout-buttons.module.ts
+++ libs/elements/layout/buttons/src/lib/elements-layout-buttons.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaSliderButtonComponent } from './elewa-slider-button/elewa-slider-button.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaSliderButtonComponent],
 })
 export class ButtonsModule {}
