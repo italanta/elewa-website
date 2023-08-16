735db05bad4762c0d3ba898cecfb8634cf49ad3f
Darylcosm0 <mutaidaryl@gmail.com>
2023-08-16 12:54:59 +0300

    :art: chore: displayed news items dynamicaly

--- libs/elements/cards/src/lib/components/elewa-news-item-card/elewa-news-item-card.component.ts
+++ libs/elements/cards/src/lib/components/elewa-news-item-card/elewa-news-item-card.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { __highlightedNews as highlightedNewsData } from '@elewa-website/data/sections';
+import { NewsItem } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-elewa-news-item-card',
   templateUrl: './elewa-news-item-card.component.html',
   styleUrls: ['./elewa-news-item-card.component.scss'],
 })
-export class ElewaNewsItemCardComponent {}
+export class ElewaNewsItemCardComponent implements OnInit  {
+  @Input() newsItem!: NewsItem[]
+  
+  ngOnInit() {
+      this.newsItem = highlightedNewsData
+  }
+}
