f9a5beb502686c969b1bbbe48daa25719b23ea96
levy <lnjiru43@gmail.com>
2023-08-17 12:29:18 +0300

    imported MatIconModule  to buttons mmolule

--- libs/elements/layout/buttons/src/lib/buttons.module.ts
+++ libs/elements/layout/buttons/src/lib/buttons.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaSliderButtonComponent } from './elewa-slider-button/elewa-slider-button.component';
 
+import { MatIconModule } from '@angular/material/icon';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule ],
   declarations: [ElewaSliderButtonComponent],
   exports: [ElewaSliderButtonComponent]
 })
