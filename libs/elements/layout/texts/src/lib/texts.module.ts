0ce5339b7bbe6c0c0fca84ed4df4399d0395f9fb
thadeus <thadeusnyariki94@gmail.com>
2023-08-18 00:14:23 +0300

    :package: feat: Created elewa-text-section component

--- libs/elements/layout/texts/src/lib/texts.module.ts
+++ libs/elements/layout/texts/src/lib/texts.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { ElewaTextContentItemComponent } from './components/elewa-text-content-item/elewa-text-content-item.component';
+import { ElewaTextSectionComponent } from './components/elewa-text-section/elewa-text-section.component';
 
-import { TextsRoutesMoule } from './texts.routing';
+// import { TextsRoutesMoule } from './texts.routing';
 
 @NgModule({
-  imports: [CommonModule, TextsRoutesMoule],
-  declarations: [ElewaTextContentItemComponent]
+  imports: [CommonModule],
+  declarations: [ElewaTextSectionComponent]
 })
 export class TextsModule {}
