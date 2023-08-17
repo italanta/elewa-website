e161d1615f0001d07b91e471f37f56b0231cdf48
levy <lnjiru43@gmail.com>
2023-08-17 12:30:22 +0300

    created a button element that takes in inputes

--- libs/elements/layout/buttons/src/lib/elewa-slider-button/elewa-slider-button.component.ts
+++ libs/elements/layout/buttons/src/lib/elewa-slider-button/elewa-slider-button.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
+
 
 @Component({
   selector: 'elewa-website-elewa-slider-button',
   templateUrl: './elewa-slider-button.component.html',
   styleUrls: ['./elewa-slider-button.component.scss'],
 })
-export class ElewaSliderButtonComponent {}
+export class ElewaSliderButtonComponent {
+  @Input() sliderButtonData!: SliderButtonData;
+  @Input() currentStyles!:object;
+  @Input() iconCurrentStyles!:object;
+  @Input() hoveredCurrentStyles!:object;
+  @Input() hoveredIconCurrentStyles!:object;
+  @Input() hovered!:boolean;
+  
+}
