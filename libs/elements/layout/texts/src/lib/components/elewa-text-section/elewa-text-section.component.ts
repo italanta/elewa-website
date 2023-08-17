17c67ae37c701831f5d5f531b3b36959c47b01a7
thadeus <thadeusnyariki94@gmail.com>
2023-08-18 01:09:31 +0300

    :wrench: chore: displayed hero text data dynamically

--- libs/elements/layout/texts/src/lib/components/elewa-text-section/elewa-text-section.component.ts
+++ libs/elements/layout/texts/src/lib/components/elewa-text-section/elewa-text-section.component.ts
@@ -1,8 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { _heroInfo as mockHeroText } from '@elewa-website/data/ui/hero-text'
+import { HeroText } from '@elewa-website/models/schema/ui/texts';
 
 @Component({
   selector: 'elewa-website-elewa-text-section',
   templateUrl: './elewa-text-section.component.html',
   styleUrls: ['./elewa-text-section.component.scss'],
 })
-export class ElewaTextSectionComponent {}
+export class ElewaTextSectionComponent {
+  @Input() heroInfo!: HeroText[]
+
+  ngOnInit(){
+    this.heroInfo = mockHeroText
+  }
+}
