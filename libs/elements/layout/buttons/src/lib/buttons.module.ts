5667aff9af4a54c6cc8c6386c1ec8f272d8311b9
levy <lnjiru43@gmail.com>
2023-08-17 15:14:58 +0300

    :adhesive_bandage: swiched from material fonts to fontawsome

--- libs/elements/layout/buttons/src/lib/buttons.module.ts
+++ libs/elements/layout/buttons/src/lib/buttons.module.ts
@@ -2,10 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaSliderButtonComponent } from './elewa-slider-button/elewa-slider-button.component';
 
-import { MatIconModule } from '@angular/material/icon';
-
 @NgModule({
-  imports: [CommonModule, MatIconModule ],
+  imports: [CommonModule],
   declarations: [ElewaSliderButtonComponent],
   exports: [ElewaSliderButtonComponent]
 })
