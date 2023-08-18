1e133de91b54c4db11af3ae1af99d1f776cbf2c7
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-18 11:19:25 +0300

    feat: add some mock data into the highlighted-projects.data.ts file

--- libs/data/sections/src/lib/highlighted-projects.data.ts
+++ libs/data/sections/src/lib/highlighted-projects.data.ts
@@ -1,5 +1,39 @@
 import { ProjectItem } from './../../../../models/schema/ui/cards/src/lib/project-item.interface';
 
 export const __highlightedProjects: ProjectItem[] = [
+  {
+    title:'Nodejs',
+    description:'A javascript runtime enviroment that lets you run javascript code without the browser.',
+    tag:'runtime',
+    imgSrc:"https://icon2.cleanpng.com/20180416/dqq/kisspng-node-js-javascript-server-side-scripting-ruby-5ad4ed856462f4.9640198115239038774112.jpg"
 
+  },
+  {
+    title:'Mongo Db',
+    description:'A non relational database to store all your data',
+    tag:'Database',
+    imgSrc:"https://icon2.cleanpng.com/20180403/rww/kisspng-mongodb-node-js-npm-open-source-model-angularjs-leaf-5ac44d9eb3d294.1874788615228143667366.jpg"
+
+  },
+  {
+    title:'CSS',
+    description:'A styling language to make your websites eyecatching',
+    tag:'Styling',
+    imgSrc:"https://icon2.cleanpng.com/20180425/fre/kisspng-css3-cascading-style-sheets-logo-html-5ae0e5878323b0.7563296815246882635372.jpg"
+
+  },
+  {
+    title:'HTML',
+    description:'A markup language defines the structure of a webpage',
+    tag:'Markup',
+    imgSrc:"https://icon2.cleanpng.com/20180203/tdw/kisspng-computer-programming-programmer-icon-design-softwa-coder-png-transparent-image-5a756d8976f6e6.1112940115176451934873.jpg"
+
+  },
+  {
+    title:'Angular',
+    description:'A javascript framework that makes designing UI simple',
+    tag:'framework',
+    imgSrc:"https://icon2.cleanpng.com/20180329/bde/kisspng-angularjs-typescript-node-js-javascript-letter-a-5abd8d107a3ee0.4050353815223718565007.jpg"
+
+  }
 ]
