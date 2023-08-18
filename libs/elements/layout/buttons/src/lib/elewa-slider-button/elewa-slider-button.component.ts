8bd560522cd99ca1a81c0ebe484cb6fd91b29862
dancunrandy <dancunrandy@gmail.com>
2023-08-18 11:54:00 +0300

    :sparkles:changed the code to angular

--- libs/elements/layout/buttons/src/lib/elewa-slider-button/elewa-slider-button.component.ts
+++ libs/elements/layout/buttons/src/lib/elewa-slider-button/elewa-slider-button.component.ts
@@ -1,20 +1,32 @@
-// elewa-slider-button.component.ts
-import { Component, Input } from '@angular/core';
 import { SliderButton } from '@elewa-website/models/schema/ui/buttons';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 
- 
 @Component({
-  selector: 'elewa-website-slider-button', 
+  selector: 'elewa-website-slider-button',
   templateUrl: './elewa-slider-button.component.html',
   styleUrls: ['./elewa-slider-button.component.scss']
 })
 export class ElewaSliderButtonComponent {
   @Input() buttonData: SliderButton = {
-    text: '',
-    bgColour: '',
-    color: '',
-    borderColor: '',
-    iconColor: '',
-    iconBackgroundColor: '',
+    text: 'unlock your potential',
+    bgColour: '#000000',
+    color: 'white',
+    borderColor: 'transparent',
+    iconColor: '#606060',
+    iconBackgroundColor: 'white',
   };
+
+  @Output() buttonClicked: EventEmitter<void> = new EventEmitter<void>() ;
+
+  onClick(): void {
+    this.buttonClicked.emit();
+  }
+  onMouseenter(): void {
+    this.onMouseenter
+  }
+
+  onMouseleave(): void {
+    this.onMouseleave
+  }
 }
+
