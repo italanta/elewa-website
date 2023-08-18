d8b600b2cc388e11bff61cb6fc7ed93da72ae662
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-18 11:57:33 +0300

    :lipstick: generated the elewa image container component

---
+++ libs/elements/layout/images/src/lib/components/elewa-image-container/elewa-image-container.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'elewa-website-elewa-image-container',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './elewa-image-container.component.html',
+  styleUrls: ['./elewa-image-container.component.scss'],
+})
+export class ElewaImageContainerComponent {}
