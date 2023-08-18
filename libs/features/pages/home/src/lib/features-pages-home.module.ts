7da81273a171b56ffdeeb9b871cda012436d049e
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-18 14:55:43 +0300

    feat: import the cards module

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { CardsModule } from '@elewa-website/elements/cards';
 import { HomePageComponent } from './main/home/home-page.component';
-
 import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, CardsModule],
   declarations: [HomePageComponent],
   exports: [HomePageComponent],
 })
