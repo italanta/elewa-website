bf05d56229378810437c8e17615ce94341ffb1cb
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-18 11:50:33 +0300

    feat: Add @Input() binding for ProjectItem in image carousel component

--- libs/elements/cards/src/lib/components/elewa-project-item-card/elewa-project-item-card.component.ts
+++ libs/elements/cards/src/lib/components/elewa-project-item-card/elewa-project-item-card.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+import { ProjectItem } from './../../../../../../models/schema/ui/cards/src/lib/project-item.interface';
+import { Component, Input } from '@angular/core';
+// import { Component, Input } from '@angular/core';
+
+
 
 @Component({
   selector: 'elewa-website-elewa-project-item-card',
   templateUrl: './elewa-project-item-card.component.html',
   styleUrls: ['./elewa-project-item-card.component.scss'],
 })
-export class ElewaProjectItemCardComponent {}
+export class ElewaProjectItemCardComponent {
+  @Input() projectCard!: ProjectItem;
+}
