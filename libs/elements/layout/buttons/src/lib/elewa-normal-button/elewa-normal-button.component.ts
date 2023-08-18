9667cd7df32c4f59437444625f770058fd52233a
Oduol Peris <oduolpepe618@gmail.com>
2023-08-18 10:40:23 +0300

    ✨feat:input and output data from the component

--- libs/elements/layout/buttons/src/lib/elewa-normal-button/elewa-normal-button.component.ts
+++ libs/elements/layout/buttons/src/lib/elewa-normal-button/elewa-normal-button.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+import { ButtonData } from '@elewa-website/models/schema/ui/buttons';
 
 @Component({
   selector: 'elewa-website-elewa-normal-button',
   templateUrl: './elewa-normal-button.component.html',
   styleUrls: ['./elewa-normal-button.component.scss'],
 })
-export class ElewaNormalButtonComponent {}
+
+/*Pass data to the component */
+/* emit events from the template that the normal button can respond to*/
+export class ElewaNormalButtonComponent {
+  @Input() buttonData!: ButtonData;
+
+  @Output() buttonClick: EventEmitter<void> = new EventEmitter<void>();
+  isHovered = false;
+
+  handleClick(): void {
+    this.buttonClick.emit();
+  }
+}
