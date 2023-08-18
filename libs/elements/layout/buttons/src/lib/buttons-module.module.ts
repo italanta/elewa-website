01121f2b3d808ca564b930bb9e6b2397bc0fe1e3
x-russian <philophilo037@gmail.com>
2023-08-18 09:41:30 +0300

    :sparkles:feat: Export elewa-normal-button component.

--- libs/elements/layout/buttons/src/lib/buttons-module.module.ts
+++ libs/elements/layout/buttons/src/lib/buttons-module.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaNormalButtonComponent } from './components/elewa-normal-button/elewa-normal-button.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaNormalButtonComponent],
+  exports:[ElewaNormalButtonComponent]
 })
 export class ButtonsModule {}
