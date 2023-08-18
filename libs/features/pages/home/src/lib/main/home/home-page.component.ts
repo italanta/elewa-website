e0b01947412418e63630ab87644a3236be178b53
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-18 14:58:23 +0300

    feat:Add project mock data to HomePageComponent

--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import { ProjectItem } from 'libs/models/schema/ui/cards/src/lib/project-item.interface';
 
 @Component({
   selector: 'elewa-website-home',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  project : ProjectItem = {
+    title: "Google",
+    description: "kkkkk",
+    imgSrc:"https://icon2.cleanpng.com/20180416/dqq/kisspng-node-js-javascript-server-side-scripting-ruby-5ad4ed856462f4.9640198115239038774112.jpg",
+    tag: "Google Inc"
+  }
+}
+
