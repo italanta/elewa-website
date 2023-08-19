78945b375c4a016718f5c19b23d4bd3dad6cca10
x-russian <philophilo037@gmail.com>
2023-08-19 11:55:02 +0300

    :sparkles:feat: Export + update button.interface file.

--- libs/models/schema/ui/buttons/src/lib/button.interface.ts
+++ libs/models/schema/ui/buttons/src/lib/button.interface.ts
@@ -1,2 +1,9 @@
-export interface ButtonInterface {
+export interface ButtonData {
+    text: string
+  
+    bgColor?: string; // Background colour
+    color?: string; // text color
+    
+    hoverBgColor?: string;
+    hoverColor?: string;
 }
