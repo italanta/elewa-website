a3e2f0d89a9d17bf6f05a5fe5ffdfb4f60822e3c
x-russian <philophilo037@gmail.com>
2023-08-19 12:07:42 +0300

    :sparkles:feat: Import data from interface file to be used in the component.

--- libs/elements/layout/buttons/src/lib/components/elewa-normal-button/elewa-normal-button.component.ts
+++ libs/elements/layout/buttons/src/lib/components/elewa-normal-button/elewa-normal-button.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { ButtonData } from '@elewa-website/models/schema/ui/buttons';
 
 @Component({
   selector: 'elewa-website-elewa-normal-button',
   templateUrl: './elewa-normal-button.component.html',
   styleUrls: ['./elewa-normal-button.component.scss'],
 })
-export class ElewaNormalButtonComponent {}
+export class ElewaNormalButtonComponent {
+
+
+
+
+
+}
