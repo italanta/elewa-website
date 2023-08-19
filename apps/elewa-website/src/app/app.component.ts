a17183e0dfc5a7ce347b3c2dbb0ea324af3767cf
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-19 16:35:53 +0300

    :lipstick: add imageConfig array and srcset

--- apps/elewa-website/src/app/app.component.ts
+++ apps/elewa-website/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { ImageConfig, ImageVisualisation } from './../../../../libs/models/schema/ui/images/image-config.interface';
+
 @Component({
   selector: 'elewa-website-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'elewa-website';
+  imageConfigArray: ImageConfig[] = [
+    {
+      title: 'Pill',
+      imageSrc: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQv-5Vcj_zz3Vq4crryp-2I_MhIP_t_o1GbH1UuWUvlU3swMTfG',
+      visualisation: ImageVisualisation.Pill,
+    },
+    {
+      title: 'Stacked',
+      imageSrc: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQCjBU0IXG5EfsqIJ6NGVKCudkfqFKxDZ_CdPjrfrBlAUDcFiy6',
+      visualisation: ImageVisualisation.Stacked,
+    },
+    {
+      title: 'Window',
+      imageSrc: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQLhuHJmD_Q8zMr_NB-4FmV9XrswgNqReV3uP3Rrg-gONa5NtuS',
+      visualisation: ImageVisualisation.Window,
+    },
+    {
+      title: 'Card',
+      imageSrc: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQDXsG9AQ26ZAGdH-XPnZKTlhTcy5d1kEJHV48FwdWRg3aXgzyA',
+      visualisation: ImageVisualisation.Card,
+    }
+  ];
 }
--- libs/elements/layout/images/src/lib/components/elewa-image-container/elewa-image-container.component.ts
+++ libs/elements/layout/images/src/lib/components/elewa-image-container/elewa-image-container.component.ts
@@ -1,8 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { ImageConfig, ImageVisualisation } from '../../../../../../../models/schema/ui/images/image-config.interface';
 
 @Component({
   selector: 'elewa-website-elewa-image-container',
   templateUrl: './elewa-image-container.component.html',
   styleUrls: ['./elewa-image-container.component.scss'],
 })
-export class ElewaImageContainerComponent {}
+export class ElewaImageContainerComponent {
+  @Input() config: ImageConfig[] = []; // Expecting an array of ImageConfig objects
+
+  getImageClass(visualisation: ImageVisualisation): string {
+    switch (visualisation) {
+      case ImageVisualisation.Pill:
+        return 'pill-style';
+      case ImageVisualisation.Stacked:
+        return 'stacked-style';
+      case ImageVisualisation.Window:
+        return 'window-style';
+      case ImageVisualisation.Card:
+        return 'card-style';
+      default:
+        return '';
+    }
+  }
+
+  getHighResImage(imageSrc: string): string {
+    // imageSrc is the original image source (image.jpg)
+    // This line replaces '.jpg' with '-highres.jpg' in the image source
+    return imageSrc.replace('.jpg', '-highres.jpg');
+  }
+}
+
