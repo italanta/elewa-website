ec6883d275e492160b06f8af366def1ec86fffed
levy <lnjiru43@gmail.com>
2023-08-21 19:12:39 +0300

    imported input interface to component

--- libs/elements/cards/src/lib/elewa-website-price-item-card/elewa-website-price-item-card.component.ts
+++ libs/elements/cards/src/lib/elewa-website-price-item-card/elewa-website-price-item-card.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { PriceItem } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-elewa-website-price-item-card',
   templateUrl: './elewa-website-price-item-card.component.html',
   styleUrls: ['./elewa-website-price-item-card.component.scss'],
 })
-export class ElewaWebsitePriceItemCardComponent {}
+export class ElewaWebsitePriceItemCardComponent {
+  @Input() priceItem!: PriceItem;
+}
