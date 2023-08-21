afb70049325a994e7f777dfb62cf131348b332ef
x-russian <philophilo037@gmail.com>
2023-08-21 20:27:01 +0300

    :fire:fix: Remove duplicate properties + change comments.

--- libs/models/schema/ui/buttons/src/lib/button.interface.ts
+++ libs/models/schema/ui/buttons/src/lib/button.interface.ts
@@ -1,19 +1,11 @@
 export interface ButtonData {
-<<<<<<< HEAD
     text: string
   
-    bgColor?: string; // Background colour
-    color?: string; // text color
+    bgColor?: string; /* Background colour */
+    color?: string; /* text color */
     
     hoverBgColor?: string;
     hoverColor?: string;
-=======
-  text: string;
 
-  bgColor?: string;
-  color?: string;
 
-  hoverBgColor?: string;
-  hoverColor?: string;
->>>>>>> refs/remotes/origin/Reusable-normal-button-#23
 }
