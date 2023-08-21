05898a80e20a2c221ac1db91b5181f4d0c3a7abb
kiplangat01 <meshnobie@gmail.com>
2023-08-21 18:22:05 +0300

    ft: moved components to their right folders

--- libs/elements/layout/carousel/src/lib/elements-layout-carousel.module.ts
+++ libs/elements/layout/carousel/src/lib/elements-layout-carousel.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProjectCarouselComponent } from './project-carousel/project-carousel.component';
-import { NgImageSliderModule } from 'ng-image-slider';
-import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { NextDirective } from './Directives/next.directive';
-import { PrevDirective } from './Directives/prev.directive';
+import { ProjectCarouselComponent } from './comoponents/project-carousel/project-carousel.component';
+import { PrevDirective } from './directives/prev.directive';
+import { NextDirective } from './directives/next.directive';
 
 @NgModule({
   imports: [CommonModule],
   declarations: [ProjectCarouselComponent, NextDirective, PrevDirective],
-  exports: [ProjectCarouselComponent, NgImageSliderModule],
+  exports: [ProjectCarouselComponent, ],
 })
 export class ElementsLayoutCarouselModule {}
