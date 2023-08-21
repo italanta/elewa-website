a5f3c4c7d6aa96c22214be4367e132a3c8c3705c
levy <lnjiru43@gmail.com>
2023-08-21 23:24:46 +0300

    converted amount to a price fomat string

--- libs/elements/cards/src/lib/elewa-website-price-item-card/elewa-website-price-item-card.component.ts
+++ libs/elements/cards/src/lib/elewa-website-price-item-card/elewa-website-price-item-card.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-
 import { PriceItem } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
@@ -9,4 +8,23 @@ import { PriceItem } from '@elewa-website/models/schema/ui/cards';
 })
 export class ElewaWebsitePriceItemCardComponent {
   @Input() priceItem!: PriceItem;
+
+  /**
+   * Converts a number to a formatted price string with commas for thousands.
+   * 
+   * @param {number} number - The number to be converted.
+   * @returns {string} The formatted price string.
+   */
+  formatPriceWithCommas(number: number): string {
+    return number.toLocaleString('en-US');
+  }
+
+  // Property to store the formatted price string
+  formattedPrice = '';
+
+  ngOnChanges() {
+    if (this.priceItem) {
+      this.formattedPrice = this.formatPriceWithCommas(this.priceItem.amount);
+    }
+  }
 }
