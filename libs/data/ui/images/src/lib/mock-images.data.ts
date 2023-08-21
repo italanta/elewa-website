6e4c70b795a367012b1a62cbd42b3aa0b0822547
thadeus <thadeusnyariki94@gmail.com>
2023-08-21 23:45:14 +0300

    :bug: :lipstick: chore: displayed pill, window, card, & stacked and styles

--- libs/data/ui/images/src/lib/mock-images.data.ts
+++ libs/data/ui/images/src/lib/mock-images.data.ts
@@ -3,31 +3,31 @@ import { ImageVisualisation } from '@elewa-website/models/schema/ui/images';
 
 export const _images: ImageConfig[] = [
   {
-    title: 'Test Image 1',
+    title: 'Pill',
     imageSrc:
       'https://cdn.pixabay.com/photo/2023/08/11/10/13/goat-8183257_640.png',
     visualisation: ImageVisualisation.Pill,
-    maxWidth: '300px',
+    maxWidth: '125px',
   },
   {
-    title: 'Test Image 2',
+    title: 'Stacked',
     imageSrc:
       'https://cdn.pixabay.com/photo/2023/08/07/15/02/bird-8175276_640.jpg',
     visualisation: ImageVisualisation.stacked,
-    maxWidth: '300px',
+    maxWidth: '125px',
   },
   {
-    title: 'Test Image 3',
+    title: 'Window',
     imageSrc:
       'https://cdn.pixabay.com/photo/2023/08/12/04/24/mabry-mill-8184715_640.jpg',
     visualisation: ImageVisualisation.Window,
-    maxWidth: '300px',
+    maxWidth: '125px',
   },
   {
-    title: 'Test Image 4',
+    title: 'Card',
     imageSrc:
       'https://cdn.pixabay.com/photo/2023/02/21/23/04/ocean-7805312_640.jpg',
     visualisation: ImageVisualisation.Card,
-    maxWidth: '300px',
+    maxWidth: '125px',
   },
 ];
