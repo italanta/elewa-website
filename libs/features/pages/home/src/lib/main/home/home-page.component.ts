c507fba5fdf4474020912164f40ca0e10f1f2eba
levy <lnjiru43@gmail.com>
2023-08-21 13:14:48 +0300

    restored home removed slider button component and sample interface data

--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,52 +1,8 @@
 import { Component } from '@angular/core';
-import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
 
 @Component({
   selector: 'elewa-website-home',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
-  sliderButtonData: SliderButtonData = {
-
-    text: "Unlock your Potential",
-
-    bgColour: "black", 
-    color: "white",
-    borderColor: "",
-    iconColor: "black",
-    iconBackgroundColor: "white",
-  
-    hoverBgColour: "white",
-    hoverColor: "black",
-    hoverBorderColor: "",
-    hoverIconColor: "white",
-    hoverIconBackgroundColor: "black"
-
-  }
-
-  sliderButton: SliderButtonData = {
-
-    text: "Unlock",
-
-    bgColour: "black", 
-    color: "white",
-    borderColor: "2px solid red",
-    iconColor: "black",
-    iconBackgroundColor: "white",
-  
-    hoverBgColour: "white",
-    hoverColor: "black",
-    hoverBorderColor: "2px solid green",
-    hoverIconColor: "white",
-    hoverIconBackgroundColor: "black"
-
-  }
-
-  handleSliderButtonClick(){
-
-    console.log("button clicked")
-    
-  }
-
-}
+export class HomePageComponent {}
