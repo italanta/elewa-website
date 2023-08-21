16588d77c131e5ff48e9c579ccd9a8e6aface675
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-21 18:00:04 +0300

    feat: import the cards module and remove commented code

--- libs/elements/cards/src/lib/components/elewa-project-item-card/elewa-project-item-card.component.ts
+++ libs/elements/cards/src/lib/components/elewa-project-item-card/elewa-project-item-card.component.ts
@@ -1,15 +1,13 @@
 // eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
-import { ProjectItem } from './../../../../../../models/schema/ui/cards/src/lib/project-item.interface';
+import { ProjectItem } from '@elewa-website/models/schema/ui/cards';
 import { Component, Input } from '@angular/core';
-// import { Component, Input } from '@angular/core';
-
-
 
 @Component({
   selector: 'elewa-website-elewa-project-item-card',
   templateUrl: './elewa-project-item-card.component.html',
   styleUrls: ['./elewa-project-item-card.component.scss'],
 })
+
 export class ElewaProjectItemCardComponent {
   @Input() projectCard!: ProjectItem;
 }
