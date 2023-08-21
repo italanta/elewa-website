6626df93361db4e112677c0c08fb1b511c9769f1
levy <lnjiru43@gmail.com>
2023-08-21 18:51:22 +0300

    :sparkles: created elewa website price item card component and added its export in card module

---
+++ libs/elements/cards/src/lib/elewa-website-price-item-card/elewa-website-price-item-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-website-price-item-card',
+  templateUrl: './elewa-website-price-item-card.component.html',
+  styleUrls: ['./elewa-website-price-item-card.component.scss'],
+})
+export class ElewaWebsitePriceItemCardComponent {}
