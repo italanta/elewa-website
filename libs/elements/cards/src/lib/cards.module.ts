1b59a3a93ef58768052bb2796fe1d0127c9f59b4
levy <lnjiru43@gmail.com>
2023-08-21 18:51:22 +0300

    :sparkles: created elewa website price item card component and added its export in card module

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
+import { ElewaWebsitePriceItemCardComponent } from './elewa-website-price-item-card/elewa-website-price-item-card.component';
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaInfoCardComponent],
-  exports: [ElewaInfoCardComponent]
+  declarations: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent],
+  exports: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent],
 })
 export class CardsModule {}
