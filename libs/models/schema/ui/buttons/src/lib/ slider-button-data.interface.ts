64e657eece24bc65869a94dc0dcbb48adbc7e175
paul <ochiengpaul001@gmail.com>
2023-08-21 22:40:27 +0300

    remove p tag not need on elewa-card component

---
+++ libs/models/schema/ui/buttons/src/lib/ slider-button-data.interface.ts
@@ -0,0 +1,16 @@
+interface SliderButtonData extends ButtonData
+{
+  text: string;
+
+  bgColour?: string; // Background colour
+  color?: string;
+  borderColor?: string;
+  iconColor?: string;
+  iconBackgroundColor?: string;
+
+  hoverBgColour?: string;
+  hoverColor?: string;
+  hoverBorderColor?: string;
+  hoverIconColor?: string;
+  hoverIconBackgroundColor?: string;
+}
