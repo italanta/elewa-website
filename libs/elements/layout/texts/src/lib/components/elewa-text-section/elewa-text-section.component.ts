5233014f050480fba9953cb34c2518a96ca42b20
+ <+>
2023-08-21 20:18:21 +0300

    :sparkles: entered the mock data

--- libs/elements/layout/texts/src/lib/components/elewa-text-section/elewa-text-section.component.ts
+++ libs/elements/layout/texts/src/lib/components/elewa-text-section/elewa-text-section.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { HeroText } from '@elewa-website/models/schema/ui/texts';
 
 @Component({
   selector: 'elewa-website-elewa-text-section',
   templateUrl: './elewa-text-section.component.html',
   styleUrls: ['./elewa-text-section.component.scss'],
 })
-export class ElewaTextSectionComponent {}
+export class ElewaTextSectionComponent {
+  @Input () textSection!: HeroText [];
+}
