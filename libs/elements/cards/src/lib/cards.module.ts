b885020d63befda838b5f76e8bb0a5a2be0fc403
levy <lnjiru43@gmail.com>
2023-08-21 20:54:44 +0300

    imported ButtonsModule to use slider button component

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
 import { ElewaWebsitePriceItemCardComponent } from './elewa-website-price-item-card/elewa-website-price-item-card.component';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonsModule],
   declarations: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent],
   exports: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent],
 })
