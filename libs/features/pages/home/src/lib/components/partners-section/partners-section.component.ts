2a17d37130f2ef381dc65c743e328b505d04eb92
kevinkkimutai <kiruikevinkim@gmail.com>
2023-08-22 11:19:16 +0300

    ✨feat: generated home page news section components

---
+++ libs/features/pages/home/src/lib/components/partners-section/partners-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-partners-section',
+  templateUrl: './partners-section.component.html',
+  styleUrls: ['./partners-section.component.scss'],
+})
+export class PartnersSectionComponent {}
