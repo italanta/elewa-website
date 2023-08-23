c1e76782b66cde0226fa772f63d853fa883e8cac
Yahya <yahyahussein670@gmail.com>
2023-08-24 00:44:39 +0300

    ✨feat(news-page): import NewsPageComponent in FeaturesPagesNewsPageModule

--- libs/features/pages/news-page/src/lib/features-pages-news-page.module.ts
+++ libs/features/pages/news-page/src/lib/features-pages-news-page.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewsPageComponent } from './main/news-page/news-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [NewsPageComponent],
 })
 export class FeaturesPagesNewsPageModule {}
