4a68a9fd101e777379a98cc20b3da96fd65b7c03
levy <lnjiru43@gmail.com>
2023-08-23 13:40:12 +0300

    :adhesive_bandage: fixed imports order

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
 import { ElewaWebsitePriceItemCardComponent } from './elewa-website-price-item-card/elewa-website-price-item-card.component';
-import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 @NgModule({
   imports: [CommonModule, ButtonsModule],
