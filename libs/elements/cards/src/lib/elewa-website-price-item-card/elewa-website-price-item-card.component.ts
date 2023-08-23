7489446cdd0d5b4c68908637401e23fa9af3be3f
levy <lnjiru43@gmail.com>
2023-08-23 14:25:55 +0300

    :recycle: used input setter instead of ngOnchange to detect input changes

--- libs/elements/cards/src/lib/elewa-website-price-item-card/elewa-website-price-item-card.component.ts
+++ libs/elements/cards/src/lib/elewa-website-price-item-card/elewa-website-price-item-card.component.ts
@@ -6,8 +6,36 @@ import { PriceItem } from '@elewa-website/models/schema/ui/cards';
   templateUrl: './elewa-website-price-item-card.component.html',
   styleUrls: ['./elewa-website-price-item-card.component.scss'],
 })
+
 export class ElewaWebsitePriceItemCardComponent {
-  @Input() priceItem!: PriceItem;
+  private _priceItem!: PriceItem;
+
+  /**
+   * Property to store the formatted price string.
+   */
+  formattedPrice = '';
+
+  /**
+   * Setter for the `priceItem` input property.
+   * This method is automatically called when the `priceItem` input is set externally.
+   * It updates the private `_priceItem` property and formats the price using commas.
+   * 
+   * @param {PriceItem} value - The new value for the `priceItem` input.
+   */
+  @Input()
+  set priceItem(value: PriceItem) {
+    this._priceItem = value;
+    this.formattedPrice = this.formatPriceWithCommas(value.amount);
+  }
+
+  /**
+   * Getter for the `priceItem` input property.
+   * 
+   * @returns {PriceItem} The current value of the `priceItem` input.
+   */
+  get priceItem(): PriceItem {
+    return this._priceItem;
+  }
 
   /**
    * Converts a number to a formatted price string with commas for thousands.
@@ -17,14 +45,5 @@ export class ElewaWebsitePriceItemCardComponent {
    */
   formatPriceWithCommas(number: number): string {
     return number.toLocaleString('en-US');
-  }
-
-  // Property to store the formatted price string
-  formattedPrice = '';
-
-  ngOnChanges() {
-    if (this.priceItem) {
-      this.formattedPrice = this.formatPriceWithCommas(this.priceItem.amount);
-    }
   }
 }
