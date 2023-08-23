6ec1ecc958284eb9b8ab379fd6e79a4855e21aaf
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-08-23 23:36:11 +0300

    docs: delete the project-item.

--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,16 +1,8 @@
 import { Component } from '@angular/core';
-import { ProjectItem } from '@elewa-website/models/schema/ui/cards';
 @Component({
   selector: 'elewa-website-home',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
-  project : ProjectItem = {
-    title:'Mongo Db. Beginner to advanced',
-    description:'MongoDB is a widely-used NoSQL database that offers a flexible and scalable approach to managing data. ',
-    imgSrc:"https://rb.gy/07l7z",
-    tag:'Database'
-  }
-}
+export class HomePageComponent {}
 
