31f8d059354c6f338300d37797c45ef873343d3a
levy <lnjiru43@gmail.com>
2023-08-23 17:15:34 +0300

    :sparkles: generated consultancy page component

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ConsultancyPageComponent],
 })
 export class ConsultancyPageModule {}
