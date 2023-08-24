20e5cc8909a03a399063c307eb5967e8b76f6390
+ <+>
2023-08-24 11:59:34 +0300

    :sparkles: created the routes

---
+++ libs/features/pages/content-development/src/lib/lib.routes.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
+
+import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
+export const ELEWA_CONTENT_DEVELOPMENT_ROUTES: Route[] = [
+
+  { path: '', component: ContentDevelopmentPageComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(ELEWA_CONTENT_DEVELOPMENT_ROUTES)],
+  exports: [RouterModule]
+})
+export class ContentDevelopmentPageRoutingModule { }
