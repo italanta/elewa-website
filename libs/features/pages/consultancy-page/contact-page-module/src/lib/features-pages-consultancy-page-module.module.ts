d9d307f7c9832d6cc069f25d3f20fc55a2ff43c9
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-24 14:28:04 +0300

    fixed bug

--- libs/features/pages/consultancy-page/contact-page-module/src/lib/features-pages-consultancy-page-module.module.ts
+++ libs/features/pages/consultancy-page/contact-page-module/src/lib/features-pages-consultancy-page-module.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ConsultancyPageComponent } from './consultancy-page/consultancy-page.component';
+import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
  import { ConsultancyRoutingModule } from './consultancy.routing';
 @NgModule({
   imports: [CommonModule,ConsultancyRoutingModule],
