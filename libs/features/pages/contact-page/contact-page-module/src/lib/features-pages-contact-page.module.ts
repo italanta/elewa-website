723c213f1d8cdb4d513af90ca53e486edd091852
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-24 10:33:43 +0300

    moved components to main folder

--- libs/features/pages/contact-page/contact-page-module/src/lib/features-pages-contact-page.module.ts
+++ libs/features/pages/contact-page/contact-page-module/src/lib/features-pages-contact-page.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ContactPageComponent } from './main/contact-page/contact-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ContactPageComponent],
 })
 export class FeaturesPagesContactPageModule {}
