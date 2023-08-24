2ba3fa7de11b0b7b0487759811cded6acd932ec1
KaranjaK <karanjakelvin77@gmail.com>
2023-08-24 11:57:17 +0300

    ✨ Imports the interface into the component

--- libs/elements/cards/src/lib/elewa-project-item-card/elewa-project-item-card.component.ts
+++ libs/elements/cards/src/lib/elewa-project-item-card/elewa-project-item-card.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { ProjectItem } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-elewa-project-item-card',
   templateUrl: './elewa-project-item-card.component.html',
   styleUrls: ['./elewa-project-item-card.component.scss'],
 })
-export class ElewaProjectItemCardComponent {}
+export class ElewaProjectItemCardComponent {
+  projectitem : ProjectItem = {
+    title: 'Introducing Conversational learning ipsum dolar',
+    description: 'Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam dolerem ipsum quia',
+    tag: 'Education',
+    imgSrc: 'img/gettyimages-1017628288-2048x2048.png'
+  }
+}
