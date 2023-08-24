c13320b5f16b1860d2922552088395e91da628d6
Steve Kariuki <stevekariuki5@gmail.com>
2023-08-24 11:12:58 +0300

    :recycle: added impor/export for the routing

--- libs/features/pages/contact-page/src/lib/contact-page.module.ts
+++ libs/features/pages/contact-page/src/lib/contact-page.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { ContactPageComponent } from './main/contact-page/contact-page.component';
 
+import { ContactPageRoutingModule } from './contact-page.routing';
+
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ContactPageRoutingModule],
   declarations: [ContactPageComponent],
   exports: [ContactPageComponent],
 })
