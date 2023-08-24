5869317ba524492a769e81064a40cf0f707818d6
lekopien <mavericklekopien@gmail.com>
2023-08-24 14:35:12 +0300

    :lipstick:added the Booklet page component in import and declaration

--- libs/features/pages/booklets-page/src/lib/features-pages-booklets-page.module.ts
+++ libs/features/pages/booklets-page/src/lib/features-pages-booklets-page.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BookletsPageComponent } from './main/booklets-page/booklets-page.component';
+
 @NgModule({
-  imports: [CommonModule],
+    imports: [CommonModule],
+    exports: [BookletsPageComponent],
+    declarations: [BookletsPageComponent]
 })
 export class FeaturesPagesBookletsPageModule {}
