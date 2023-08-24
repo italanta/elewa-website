e469ee07acd307adf578a2e66a17d6b9580a76de
KaranjaK <karanjakelvin77@gmail.com>
2023-08-24 10:59:01 +0300

    ✨ Creates the elewa-project-item-card component

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
 import { ElewaProjectItemCardComponent } from './elewa-project-item-card/elewa-project-item-card.component';
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaInfoCardComponent, ElewaProjectItemCardComponent],
+  declarations: [
+    ElewaInfoCardComponent,
+    ElewaProjectItemCardComponent,
+    ElewaProjectItemCardComponent,
+  ],
   exports: [ElewaInfoCardComponent],
 })
 export class CardsModule {}
