3b8d888754915dfa6fa418476384670945eaa791
dancunrandy <dancunrandy@gmail.com>
2023-08-24 13:23:31 +0300

    created booklets module

---
+++ libs/features/pages/booklets-page/src/lib/main/booklets-page/booklets-page.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BookletsPageComponent } from './booklets-page.component';
+
+describe('BookletsPageComponent', () => {
+  let component: BookletsPageComponent;
+  let fixture: ComponentFixture<BookletsPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [BookletsPageComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(BookletsPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
