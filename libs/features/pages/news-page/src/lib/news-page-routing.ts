9c03af252028719c0502cfd4d1337dd7a43a2f68
Yahya <yahyahussein670@gmail.com>
2023-08-24 08:49:02 +0300

    ✨ feat: add child route to the NewsPage module

---
+++ libs/features/pages/news-page/src/lib/news-page-routing.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
+
+import { NewsPageComponent } from './main/news-page/news-page.component';
+
+export const ELEWA_NEWS_ROUTES: Route[] = [
+
+  { path: 'news', component: NewsPageComponent },
+
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(ELEWA_NEWS_ROUTES)],
+  exports: [RouterModule]
+})
+export class NewsPageRoutingModule { }
