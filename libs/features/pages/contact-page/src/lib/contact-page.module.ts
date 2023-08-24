83515504dfce14362e9da643b81e5edd77ca4483
dancunrandy <dancunrandy@gmail.com>
2023-08-24 10:51:29 +0300

    import contact page to the module

--- libs/features/pages/contact-page/src/lib/contact-page.module.ts
+++ libs/features/pages/contact-page/src/lib/contact-page.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ContactRoutingModule } from './contact.routing';
+
 import { ContactPageComponent } from './main/contact-page/contact-page.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ContactRoutingModule],
   declarations: [ContactPageComponent],
 })
 export class ContactPageModule {}
