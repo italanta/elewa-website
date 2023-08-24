136df8b12925eed7aaaefa5607ecfb0a52bb6079
paul <ochiengpaul001@gmail.com>
2023-08-24 09:10:27 +0300

    create data file

---
+++ libs/data/sections/highlighted-projects.data.ts
@@ -0,0 +1,16 @@
+export const __highlightedProjects: ProjectItem[] = [
+    {
+      title: "project1",
+      description: "Very enthusiastic about learning angular",
+      tag: "tag1",
+      imgSrc: "path/image.jpg",
+    },
+    {
+      title: "Introducing peer learning ipsum dolar",
+      description: "Very enthusiastic about learning angular, I have already learned html, css, react, js, ruby, ts",
+      tag: "education",
+      imgSrc: "https://images.ctfassets.net/cnu0m8re1exe/8mNFyn7Fn15xx8g3lygAp/99f4534bd491f7259fea75f36c67700b/shutterstock_618062045.jpg?fm=jpg&fl=progressive&w=660&h=433&fit=fill",
+    },
+    
+  ];
+  
