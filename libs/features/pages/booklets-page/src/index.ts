5787e0e9a09c82073dfd5ce1f83f500e3288ab4a
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-24 11:14:39 +0300

    :sparkles: generated booklets page routes and modules

---
+++ libs/features/pages/booklets-page/src/index.ts
@@ -0,0 +1,4 @@
+export * from './lib/features-pages-booklets-page.module';
+export * from './lib/lib.routes';
+
+export * from './lib/lib.routes';
---
+++ libs/features/pages/booklets-page/src/lib/lib.routes.ts
@@ -0,0 +1,3 @@
+import { Route } from '@angular/router';
+
+export const featuresPagesBookletsPageRoutes: Route[] = [];
