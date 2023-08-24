5b6b148bf19de647d406699c85f28bbecf42c129
thadeus <thadeusnyariki94@gmail.com>
2023-08-24 12:00:52 +0300

    :bug: fix: Imported and added ElewaTextContentItemComponent to TextsMoule

--- libs/elements/layout/texts/src/lib/texts.module.ts
+++ libs/elements/layout/texts/src/lib/texts.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ElewaTextContentItemComponent } from './components/elewa-text-content-item/elewa-text-content-item.component';
 import { ElewaTextSectionComponent } from './components/elewa-text-section/elewa-text-section.component';
 
 import { TextsRoutesMoule } from './texts.routing';
 
 @NgModule({
   imports: [CommonModule, TextsRoutesMoule],
-  declarations: [ElewaTextSectionComponent]
+  declarations: [ElewaTextSectionComponent, ElewaTextContentItemComponent]
 })
 export class TextsModule {}
