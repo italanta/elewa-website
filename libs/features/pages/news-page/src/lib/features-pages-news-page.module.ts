5cd132e206d5d0a65d0a0e273db254ad13dc3bfa
Yahya <yahyahussein670@gmail.com>
2023-08-24 08:45:40 +0300

    ✨feat: import NewsPageRoutingModule to NewsPage module

--- libs/features/pages/news-page/src/lib/features-pages-news-page.module.ts
+++ libs/features/pages/news-page/src/lib/features-pages-news-page.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NewsPageComponent } from './main/news-page/news-page.component';
+import { NewsPageRoutingModule } from './news-page-routing';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, NewsPageRoutingModule],
   declarations: [NewsPageComponent],
 })
 export class FeaturesPagesNewsPageModule {}
