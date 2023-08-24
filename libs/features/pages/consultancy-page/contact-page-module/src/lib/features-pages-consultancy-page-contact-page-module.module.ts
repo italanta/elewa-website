46ca41beeca42cd50dc18deaf3a6a0f5832a4f29
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-24 12:57:51 +0300

    created consultancy-page component

--- libs/features/pages/consultancy-page/contact-page-module/src/lib/features-pages-consultancy-page-contact-page-module.module.ts
+++ libs/features/pages/consultancy-page/contact-page-module/src/lib/features-pages-consultancy-page-contact-page-module.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConsultancyPageComponent } from './consultancy-page/consultancy-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ConsultancyPageComponent],
 })
 export class FeaturesPagesConsultancyPageModule {}
