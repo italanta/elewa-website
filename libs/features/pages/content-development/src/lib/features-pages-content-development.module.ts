1d2774391c9e7d30534c4ebbd09df7a0d7a1bb00
+ <+>
2023-08-24 12:02:06 +0300

    :art: updated content development module

---
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
+
+import { ContentDevelopmentPageRoutingModule } from './lib.routes';
+@NgModule({
+  imports: [CommonModule, ContentDevelopmentPageRoutingModule],
+  declarations: [ContentDevelopmentPageComponent],
+  exports: [ContentDevelopmentPageComponent]
+})
+export class ContentDevelopmentModule {}
