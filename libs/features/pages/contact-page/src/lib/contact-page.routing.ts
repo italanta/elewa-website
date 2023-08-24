28362278afbbd847ddb64b8055837d6c28a6744a
Steve Kariuki <stevekariuki5@gmail.com>
2023-08-24 10:34:14 +0300

    :sparkles: created and configured contact routing

---
+++ libs/features/pages/contact-page/src/lib/contact-page.routing.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Route } from '@angular/router';
+import { ContactPageComponent } from './main/contact-page/contact-page.component';
+
+const ELEWA_CONTACT_ROUTES: Route[] = [
+  {
+    path: '', // The URL path for your page
+    component: ContactPageComponent, // The component to display for this route
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(ELEWA_CONTACT_ROUTES)],
+  exports: [RouterModule],
+})
+export class PageNameRoutingModule {}
