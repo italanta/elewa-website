fce40b133ecdf17543a09cfa68c594eee445d3c6
kevinkkimutai <kiruikevinkim@gmail.com>
2023-08-24 18:20:09 +0300

    ✨ feat: generated image data

---
+++ libs/models/schema/ui/images/src/lib/image-config.interface.ts
@@ -0,0 +1,16 @@
+export interface ImageConfig {
+  title: string
+  imageSrc: string
+  visualisation: ImageVisualisation
+  maxWidth?: string
+}
+
+/**
+ * Image interface enum
+ */
+export enum ImageVisualisation {
+  Pill = 0,
+  Stacked = 1,
+  Window = 2,
+  Card = 3
+}
