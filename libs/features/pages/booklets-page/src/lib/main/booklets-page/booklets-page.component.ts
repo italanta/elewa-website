ffbcd9f48e08c007652d6eca2496aa1292e4825b
lekopien <mavericklekopien@gmail.com>
2023-08-24 14:26:20 +0300

    :sparkles:generated a booklet page component and module with the same name of bokklet page

---
+++ libs/features/pages/booklets-page/src/lib/main/booklets-page/booklets-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-booklets-page',
+  templateUrl: './booklets-page.component.html',
+  styleUrls: ['./booklets-page.component.scss'],
+})
+export class BookletsPageComponent {}
