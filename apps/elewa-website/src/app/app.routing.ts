5fa727909490b61011e00a5f4c6da968b4026a62
Darylcosm0 <mutaidaryl@gmail.com>
2023-08-24 10:16:21 +0300

    :fire: Removed card route

--- apps/elewa-website/src/app/app.routing.ts
+++ apps/elewa-website/src/app/app.routing.ts
@@ -10,9 +10,6 @@ export const ELEWA_WEBSITE_ROUTES: Route[] = [
   { path: 'home', loadChildren: () => import('@elewa-website/features/pages/home').then(m => m.FeaturesPagesHomeModule) },
 
   { path: 'about', loadChildren: () => import('@elewa-website/features/pages/about').then(m => m.FeaturesPagesAboutModule) },
-
-  { path: 'cards', loadChildren: () => import('@elewa-website/elements/cards').then(m => m.CardsModule) },
-
 ];
 
 
--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -3,10 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { ElewaNewsItemCardComponent } from './components/elewa-news-item-card/elewa-news-item-card.component';
 
-import { CardsRoutesModule } from './cards.routing';
-
 @NgModule({
-  imports: [CommonModule, CardsRoutesModule],
+  imports: [CommonModule],
   declarations: [ElewaNewsItemCardComponent]
 })
 export class CardsModule {}
