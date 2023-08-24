771c0f3930e1997d2f70d8d29170f3df0c875b74
Darylcosm0 <mutaidaryl@gmail.com>
2023-08-24 10:32:40 +0300

    :pencil: Refactor news item rendering logic

--- libs/elements/cards/src/lib/components/elewa-news-item-card/elewa-news-item-card.component.ts
+++ libs/elements/cards/src/lib/components/elewa-news-item-card/elewa-news-item-card.component.ts
@@ -1,17 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
-
-import { __highlightedNews as highlightedNewsData } from '@elewa-website/data/sections';
+import { Component, Input } from '@angular/core';
 import { NewsItem } from '@elewa-website/models/schema/ui/cards';
+
 
 @Component({
   selector: 'elewa-website-elewa-news-item-card',
   templateUrl: './elewa-news-item-card.component.html',
   styleUrls: ['./elewa-news-item-card.component.scss'],
 })
-export class ElewaNewsItemCardComponent implements OnInit  {
-  @Input() newsItem!: NewsItem[]
-  
-  ngOnInit() {
-      this.newsItem = highlightedNewsData
-  }
+export class ElewaNewsItemCardComponent {
+  @Input() newsItem!: NewsItem;
 }
