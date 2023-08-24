cf41907241eabbc109e85a0bc87e88d46a459aae
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-08-24 10:32:37 +0300

    generated the conversational-learning-page component

---
+++ libs/features/pages/conversational-learning/src/lib/main/conversational-learning-page/conversational-learning-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-conversational-learning-page',
+  templateUrl: './conversational-learning-page.component.html',
+  styleUrls: ['./conversational-learning-page.component.scss'],
+})
+export class ConversationalLearningPageComponent {}
