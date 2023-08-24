39fd5811df800cb2afb95c99df418fba4487efce
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-24 11:06:57 +0300

    :recycle: refactor: remove heroTexts property on homepage

--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,14 +1,8 @@
 import { Component } from '@angular/core';
-import { HeroText } from '@elewa-website/models/schema/ui/texts';
 
 @Component({
   selector: 'elewa-website-home',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
-
-  // display the text using text-section component
-
-  public heroTexts: HeroText[] = [];
-}
+export class HomePageComponent {}
