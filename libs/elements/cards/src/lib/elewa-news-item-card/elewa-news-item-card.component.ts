67ae053c581a2a057ce090fd6701e3f3e37c21d4
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-25 11:23:47 +0300

    :sparkles: generated elewa-news-item-card component

---
+++ libs/elements/cards/src/lib/elewa-news-item-card/elewa-news-item-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'elewa-website-elewa-news-item-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './elewa-news-item-card.component.html',
+  styleUrls: ['./elewa-news-item-card.component.scss'],
+})
+export class ElewaNewsItemCardComponent {}
