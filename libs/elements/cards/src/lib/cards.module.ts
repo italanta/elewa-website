a2b284524cb47bb79c027714679ff22ac4ecc843
zac717 <isaacongera717@gmail.com>
2023-08-25 13:18:58 +0300

    made changes to resolve the merge conflict

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
-
-import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
-import { ElewaWebsitePriceItemCardComponent } from './elewa-website-price-item-card/elewa-website-price-item-card.component';
 import { ElewaNewsItemCardComponent } from './elewa-news-item-card/elewa-news-item-card.component';
 
 @NgModule({
-  imports: [CommonModule, ButtonsModule],
-  declarations: [
-    ElewaInfoCardComponent,
-    ElewaWebsitePriceItemCardComponent,
-    ElewaNewsItemCardComponent,
-  ],
-  exports: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent],
+  imports: [CommonModule],
+  declarations: [ElewaNewsItemCardComponent],
+  exports: [ElewaNewsItemCardComponent]
 })
-export class CardsModule {}
+export class ElementsCardsModule {}
