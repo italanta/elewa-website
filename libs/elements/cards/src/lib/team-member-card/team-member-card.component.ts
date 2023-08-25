30dc0abca2670def82a23145c9024ca290d272b0
x-russian <philophilo037@gmail.com>
2023-08-25 18:37:44 +0300

    :sparkles:feat: Import inerface and assign it to a property.

--- libs/elements/cards/src/lib/team-member-card/team-member-card.component.ts
+++ libs/elements/cards/src/lib/team-member-card/team-member-card.component.ts
@@ -1,8 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { TeamMemberInterface } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-team-member-card',
   templateUrl: './team-member-card.component.html',
   styleUrls: ['./team-member-card.component.scss'],
 })
-export class TeamMemberCardComponent {}
+export class TeamMemberCardComponent {
+
+  /**  Pass member property as input in the component */
+  @Input() member!: TeamMemberInterface;
+}
+
