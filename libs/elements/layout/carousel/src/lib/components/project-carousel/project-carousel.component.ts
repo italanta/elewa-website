2c0d3a8ac582a2e36e1e554abfdd4f5d6e14696e
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-25 12:15:49 +0300

    :recycle: refactor: set carousel input type to projectItems

--- libs/elements/layout/carousel/src/lib/components/project-carousel/project-carousel.component.ts
+++ libs/elements/layout/carousel/src/lib/components/project-carousel/project-carousel.component.ts
@@ -1,16 +1,13 @@
 import { Component, Input } from '@angular/core';
 
+import { ProjectItem } from '@elewa-website/models/sections/projects';
+
 @Component({
   selector: 'elewa-website-project-carousel',
   templateUrl: './project-carousel.component.html',
   styleUrls: ['./project-carousel.component.scss'],
 })
-
 export class ProjectCarouselComponent {
-
-  // TO DO!: It should use the component in #14 to display the projects.
-  @Input() projectItems!: string[] 
-  
-
- }
-
+  /** a list of project items */
+  @Input() projectItems!: ProjectItem[];
+}
