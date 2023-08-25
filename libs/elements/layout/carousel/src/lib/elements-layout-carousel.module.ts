93e6a87f31455c2c179759c67bf07c2c1ffeac6f
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-25 12:19:01 +0300

    :recycle: refactor: pass each projectItem to the reusable project-item component

--- libs/elements/layout/carousel/src/lib/elements-layout-carousel.module.ts
+++ libs/elements/layout/carousel/src/lib/elements-layout-carousel.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { CardsModule } from '@elewa-website/elements/cards';
+
 import { ProjectCarouselComponent } from './components/project-carousel/project-carousel.component';
 import { PrevDirective } from './directives/prev.directive';
 import { NextDirective } from './directives/next.directive';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, CardsModule],
   declarations: [ProjectCarouselComponent, NextDirective, PrevDirective],
   exports: [ProjectCarouselComponent, ],
 })
