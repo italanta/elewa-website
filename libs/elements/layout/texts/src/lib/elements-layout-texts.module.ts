ada487d5feb2f714511811e49930392545b13970
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-08-25 15:18:21 +0300

    🔥 deleted some errors

--- libs/elements/layout/texts/src/lib/elements-layout-texts.module.ts
+++ libs/elements/layout/texts/src/lib/elements-layout-texts.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ElewaTextSectionComponent } from './components/elewa-text-section/elewa-text-section.component';
+
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaTextSectionComponent],
-  exports: [ElewaTextSectionComponent]
 })
 export class TextsModule {}
