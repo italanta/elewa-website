e385daf920c657abdf9cd2beb0b106ec45b0f489
joanne muthoni <joanne.mumbi@student.moringaschool.com>
2023-08-25 15:15:14 +0300

    🔥 removed   some wrong files that i had created

--- libs/elements/layout/texts/src/lib/components/elewa-text-section/elewa-text-section.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { HeroText } from '../../interfaces/text-interface';
-
-@Component({
-  selector: 'elewa-website-elewa-text-section',
-  templateUrl: './elewa-text-section.component.html',
-  styleUrls: ['./elewa-text-section.component.scss'],
-})
-export class ElewaTextSectionComponent {}
