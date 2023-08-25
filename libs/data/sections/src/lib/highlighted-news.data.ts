7f6997a6297d0c866d6381e5fca8ff7eef8c895f
OgembaJr <manuogemba@gmail.com>
2023-08-25 15:51:25 +0300

    ✨ feat: added a new mock data named highlighted-news.data

---
+++ libs/data/sections/src/lib/highlighted-news.data.ts
@@ -0,0 +1,9 @@
+import { NewsItem } from "@elewa-website/models/schema/ui/cards";
+
+export const __highlightedNews: NewsItem[] = [
+    {
+        title: 'Conversational learning ipsum dolar',
+        description: 'Eos qui ratione voluptatem sequi nesciunt',
+        buttonText: 'Read more'
+    }
+]
--- libs/elements/cards/src/lib/elewa-news-item-card/elewa-news-item-card.component.ts
+++ libs/elements/cards/src/lib/elewa-news-item-card/elewa-news-item-card.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NewsItem } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-elewa-news-item-card',
   templateUrl: './elewa-news-item-card.component.html',
   styleUrls: ['./elewa-news-item-card.component.scss'],
 })
-export class ElewaNewsItemCardComponent {}
+export class ElewaNewsItemCardComponent {
+  @Input() newsitem! : NewsItem
+}
