7793fa53e43971a5fa58bb54008506b4bf2bbd24
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-25 10:05:16 +0300

    :recycle:  refactor: update cards module components

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -5,7 +5,7 @@ import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
 import { ElewaProjectItemCardComponent } from './elewa-project-item-card/elewa-project-item-card.component';
-import { ElewaWebsitePriceItemCardComponent } from './elewa-website-price-item-card/elewa-website-price-item-card.component';
+import { ElewaWebsitePriceItemCardComponent } from './elewa-price-item-card/elewa-website-price-item-card.component';
 
 @NgModule({
   imports: [CommonModule, ButtonsModule],
--- libs/elements/cards/src/lib/elewa-project-item-card/elewa-project-item-card.component.ts
+++ libs/elements/cards/src/lib/elewa-project-item-card/elewa-project-item-card.component.ts
@@ -1,14 +1,13 @@
 import { Component, Input } from '@angular/core';
-// eslint-disable-next-line @nrwl/nx/enforce-module-boundaries
+
 import { ProjectItem } from '@elewa-website/models/sections/projects';
 
 @Component({
-  selector: 'elewa-website-elewa-project-item-card',
+  selector: 'elewa-website-project-item-card',
   templateUrl: './elewa-project-item-card.component.html',
   styleUrls: ['./elewa-project-item-card.component.scss'],
 })
-
 export class ElewaProjectItemCardComponent {
-  /** Use the '!' to determine whether there is data to be put in. */
+  /** input project of type @interface ProjectItem */
   @Input() project!: ProjectItem;
 }
