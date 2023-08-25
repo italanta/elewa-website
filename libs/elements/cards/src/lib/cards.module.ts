4444d8d3b12e444d3492a94e07077d6a42b99da8
Steve Kariuki <stevekariuki5@gmail.com>
2023-08-25 20:24:03 +0300

    :sparkles: created the team member component

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -6,10 +6,20 @@ import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
 import { ElewaProjectItemCardComponent } from './elewa-project-item-card/elewa-project-item-card.component';
 import { ElewaWebsitePriceItemCardComponent } from './elewa-price-item-card/elewa-website-price-item-card.component';
+import { TeamMemberCardComponent } from './team-member-card/team-member-card.component';
 
 @NgModule({
   imports: [CommonModule, ButtonsModule],
-  declarations: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent, ElewaProjectItemCardComponent],
-  exports: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent, ElewaProjectItemCardComponent],
+  declarations: [
+    ElewaInfoCardComponent,
+    ElewaWebsitePriceItemCardComponent,
+    ElewaProjectItemCardComponent,
+    TeamMemberCardComponent,
+  ],
+  exports: [
+    ElewaInfoCardComponent,
+    ElewaWebsitePriceItemCardComponent,
+    ElewaProjectItemCardComponent,
+  ],
 })
 export class CardsModule {}
---
+++ libs/elements/cards/src/lib/team-member-card/team-member-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-team-member-card',
+  templateUrl: './team-member-card.component.html',
+  styleUrls: ['./team-member-card.component.scss'],
+})
+export class TeamMemberCardComponent {}
