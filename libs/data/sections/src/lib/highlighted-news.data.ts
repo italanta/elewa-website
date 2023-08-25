71f41391ecd05166952787270e30a3e16f67d130
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-25 11:58:08 +0300

    :lipstick: added mock data and data in the component's html

---
+++ libs/data/sections/src/lib/highlighted-news.data.ts
@@ -0,0 +1,10 @@
+import { NewsItem } from "@elewa-website/models/schema/ui/cards";
+
+/* sample data for the news item card */
+export const __highlightedNews: NewsItem[] = [
+    {
+        title: 'Conversational learning ipsum dolar',
+        description: 'Eos qui ratione voluptatem sequi nesciunt',
+        buttonText: 'Read more'
+    }
+];
