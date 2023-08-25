d4a6927439f996226ac5d5452b479ec020b51299
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-25 11:28:34 +0300

    :sparkles: exported the elewa-news-item-card component inside the Cards Module

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -5,10 +5,11 @@ import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
 import { ElewaWebsitePriceItemCardComponent } from './elewa-website-price-item-card/elewa-website-price-item-card.component';
+import { ElewaNewsItemCardComponent } from './elewa-news-item-card/elewa-news-item-card.component';
 
 @NgModule({
   imports: [CommonModule, ButtonsModule],
-  declarations: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent],
-  exports: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent],
+  declarations: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent, ElewaNewsItemCardComponent],
+  exports: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent, ElewaNewsItemCardComponent],
 })
 export class CardsModule {}
--- libs/elements/cards/src/lib/elewa-news-item-card/elewa-news-item-card.component.ts
+++ libs/elements/cards/src/lib/elewa-news-item-card/elewa-news-item-card.component.ts
@@ -1,10 +1,7 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'elewa-website-elewa-news-item-card',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './elewa-news-item-card.component.html',
   styleUrls: ['./elewa-news-item-card.component.scss'],
 })
