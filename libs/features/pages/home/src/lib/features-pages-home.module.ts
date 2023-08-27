48e626154520b1e704536b8e29ac6aedc71d0326
paul <ochiengpaul001@gmail.com>
2023-08-27 23:38:58 +0300

    unstaged changes

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ElewaTextContentItemComponent } from 'libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component';
 import { HomePageComponent } from './main/home/home-page.component';
 import { ElementsLayoutTextsModule } from '../../../../../elements/layout/texts/src/lib/elements-layout-texts.module';
 import { HomeRoutingModule } from './home.routing';
--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 import { ElewaTextContentItemComponent } from 'libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component';
-
+import { ContentText } from '@elewa-website/models/schema/ui/texts';
 @Component({
   selector: 'elewa-website-text-content-item',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  yourContentData: ContentText[] = [
+    {
+      title: 'Your Title 1',
+      descriptions: ['Description 1', 'Description 2', 'Description 3'],
+    },
+    {
+      title: 'Your Title 2',
+      descriptions: ['Description 1', 'Description 2', 'Description 3'],
+    },
+    // Add more items as needed
+  ];
+}
