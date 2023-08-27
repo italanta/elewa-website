e93eb93e0675aef7717637e950f7e232eaff3a89
paul <ochiengpaul001@gmail.com>
2023-08-27 23:01:06 +0300

    import content-item module to home-page module

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomePageComponent } from './main/home/home-page.component';
-
+import { ElementsLayoutTextsModule } from '../../../../../elements/layout/texts/src/lib/elements-layout-texts.module';
 import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, ElementsLayoutTextsModule],
   declarations: [HomePageComponent],
   exports: [HomePageComponent],
 })
--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { ElewaTextContentItemComponent } from 'libs/elements/layout/texts/src/lib/elewa-text-content-item/elewa-text-content-item.component';
 
 @Component({
-  selector: 'elewa-website-home',
+  selector: 'elewa-website-text-content-item',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
