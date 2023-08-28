57bef79ada9aa1be98e94304f66eab4fc1d5c5ac
paul <ochiengpaul001@gmail.com>
2023-08-28 18:00:28 +0300

    initial setup

---
+++ libs/models/schema/ui/images/src/lib/image-config.interface.ts
@@ -0,0 +1,13 @@
+export interface ImageConfig {
+  title: string;
+  imageSrc: string;
+  visualisation: ImageVisualisation;
+  maxWidth?: string;
+}
+
+export enum ImageVisualisation {
+  Pill = 0,
+  stacked = 1,
+  Window = 2,
+  Card = 3,
+}
