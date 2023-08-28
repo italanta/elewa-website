2d3b72e3439fa29ceea5f9c5fb2b87ede49ed20c
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-29 01:26:13 +0300

    deleted components folder

--- libs/features/pages/home/src/lib/components/partners-section/partners-section.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-website-partners-section',
-  templateUrl: './partners-section.component.html',
-  styleUrls: ['./partners-section.component.scss'],
-})
-export class PartnersSectionComponent {}
