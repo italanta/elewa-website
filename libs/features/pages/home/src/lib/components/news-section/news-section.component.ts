a7e3fc2acfe3a8649c869e3a85bc2375fec03faf
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-28 20:19:47 +0300

    :lipstick: defined the highlighted news item once and created an array with three identical items

--- libs/features/pages/home/src/lib/components/news-section/news-section.component.ts
+++ libs/features/pages/home/src/lib/components/news-section/news-section.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NewsItem } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-news-section',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './news-section.component.html',
   styleUrls: ['./news-section.component.scss'],
 })
-export class NewsSectionComponent {}
+export class NewsSectionComponent {
+  highlightedNews: NewsItem = {
+    title: 'Conversational learning ipsum dolar',
+    description: 'Eos qui ratione voluptatem sequi nesciunt.',
+    buttonText: 'Read more',
+  };
+
+  highlightedNewsArray: NewsItem[] = Array(3).fill(this.highlightedNews);
+}
--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomePageComponent } from './main/home/home-page.component';
+import { NewsSectionComponent } from './components/news-section/news-section.component';
 
 import { HomeRoutingModule } from './home.routing';
 
-
 @NgModule({
   imports: [CommonModule, HomeRoutingModule],
-  declarations: [HomePageComponent],
-  exports: [HomePageComponent],
+  declarations: [HomePageComponent, NewsSectionComponent],
+  exports: [HomePageComponent, NewsSectionComponent],
 })
 export class FeaturesPagesHomeModule {}
