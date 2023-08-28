6c0a5893e9f65c5d89c7faea2c437cc1fc8399ca
paul <ochiengpaul001@gmail.com>
2023-08-28 15:42:33 +0300

    style content-items

--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -9,12 +9,12 @@ import { ContentText } from '@elewa-website/models/schema/ui/texts';
 export class HomePageComponent {
   yourContentData: ContentText[] = [
     {
-      title: 'Your Title 1',
-      descriptions: ['Description 1', 'Description 2', 'Description 3'],
+      title: 'from here to the world',
+      descriptions: ['Getting thousands of people up to speed can be hard and expensive.But it doesnt have to be.', 'Elewa is an education partner which develops the kind of training that work at scale.This without losing track of what matters most,people!', 'Personalized, enjoyable and effective training at scale.Technology enabled, human powered.'],
     },
     {
-      title: 'Your Title 2',
-      descriptions: ['Description 1', 'Description 2', 'Description 3'],
+      title: 'people at the center',
+      descriptions: ['People are the secret to successful education programs.Through indepth research, we work with your oragnization at different levels, to understand "elewa" how people live and learn', 'Based on decades of research, this understanding allows us to co-create strong learning networks withing your organization,networks can be reused everytime a training need arises; at a fraction of the cost of the current solution.'],
     },
     // Add more items as needed
   ];
