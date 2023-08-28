e0db932c8cb15d004ab287ef4b5f4264a283d9ab
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-28 20:12:06 +0300

    :sparkles: generated the news-section component

---
+++ libs/features/pages/home/src/lib/components/news-section/news-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'elewa-website-news-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './news-section.component.html',
+  styleUrls: ['./news-section.component.scss'],
+})
+export class NewsSectionComponent {}
