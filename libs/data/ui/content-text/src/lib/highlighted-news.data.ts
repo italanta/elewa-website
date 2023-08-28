19df806c18f567211ad56ca7037c66795a5a17cd
Isaac Ongera <117748841+zac717@users.noreply.github.com>
2023-08-28 16:40:22 +0300

    created mock data for news item component and made some changes to the news interface file

---
+++ libs/data/ui/content-text/src/lib/highlighted-news.data.ts
@@ -0,0 +1,19 @@
+import { NewsItem } from "@elewa-website/models/schema/ui/cards";
+
+export const _highlightedNews: NewsItem[] = [
+    {
+      title: "Conversational learning 1",
+      description: "Lorem ipsum dolor sit amet. Vel vitae enim non placeat suscipit qui galisum itaque qui voluptas quia in provident sunt.",
+      buttonText: "Learn More",
+    },
+    {
+      title: "Conversational learning 2",
+      description: "Lorem ipsum dolor sit amet. Vel vitae enim non placeat suscipit",
+      buttonText: "Register Now",
+    },
+    {
+      title: "Conversational learning 3",
+      description: "Lorem ipsum dolor sit amet. Vel vitae enim non placeat suscipit qui galisum itaque qui voluptas quia in provident sunt.",
+      buttonText: "Get Involved",
+    },
+  ];  
