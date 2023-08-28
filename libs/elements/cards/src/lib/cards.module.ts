f440073056c77465b162edcc67edeebfedf4520f
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-28 16:42:58 +0300

    refactor: move team-member into components folder

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -7,7 +7,7 @@ import { ElewaInfoCardComponent } from './components/elewa-info-card/elewa-info-
 import { ElewaProjectItemCardComponent } from './components/elewa-project-item-card/elewa-project-item-card.component';
 import { ElewaNewsItemCardComponent } from './components/elewa-news-item-card/elewa-news-item-card.component';
 import { ElewaWebsitePriceItemCardComponent } from './components/elewa-price-item-card/elewa-website-price-item-card.component';
-import { ElewaTeamMemberCardComponent } from './team-member-card/team-member-card.component';
+import { ElewaTeamMemberCardComponent } from './components/elewa-team-member-card/team-member-card.component';
 
 @NgModule({
   imports: [CommonModule, ButtonsModule],
