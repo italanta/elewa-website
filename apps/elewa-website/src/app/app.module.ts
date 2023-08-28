fdf80ced1b555cae4364c9c4682a426b736d0a13
Isaac Ongera <117748841+zac717@users.noreply.github.com>
2023-08-28 16:45:08 +0300

    added the import to the elewa app module'

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TranslocoRootModule } from './transloco-root.module';
 import { AppHeaderModule } from '@elewa-website/elements/layout/header';
+import { CardsModule } from '@elewa-website/elements/cards';
 
 import { AppRoutingModule } from './app.routing';
 
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app.routing';
     HttpClientModule,
     TranslocoRootModule,
     AppHeaderModule,
+    CardsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
