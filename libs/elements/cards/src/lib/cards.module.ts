9fd81110a3d6d4148fb534032925806b820f1a74
Isaac Ongera <117748841+zac717@users.noreply.github.com>
2023-08-28 15:40:46 +0300

    created elewa-news-item component and added it to the cards module

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -6,10 +6,11 @@ import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 import { ElewaInfoCardComponent } from './elewa-info-card/elewa-info-card.component';
 import { ElewaProjectItemCardComponent } from './elewa-project-item-card/elewa-project-item-card.component';
 import { ElewaWebsitePriceItemCardComponent } from './elewa-price-item-card/elewa-website-price-item-card.component';
+import { ElewaNewsItemCardComponent } from './elewa-news-item-card/elewa-news-item-card.component';
 
 @NgModule({
   imports: [CommonModule, ButtonsModule],
-  declarations: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent, ElewaProjectItemCardComponent],
+  declarations: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent, ElewaProjectItemCardComponent, ElewaNewsItemCardComponent],
   exports: [ElewaInfoCardComponent, ElewaWebsitePriceItemCardComponent, ElewaProjectItemCardComponent],
 })
 export class CardsModule {}
---
+++ libs/elements/cards/src/lib/elewa-news-item-card/elewa-news-item-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-news-item-card',
+  templateUrl: './elewa-news-item-card.component.html',
+  styleUrls: ['./elewa-news-item-card.component.css']
+})
+export class ElewaNewsItemCardComponent {
+
+}
