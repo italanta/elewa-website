c919e6855c715577c53ad23df2233a3b785fbe73
Chrispine Ochieng <chrispine.oduor@student.moringaschool.com>
2023-08-28 10:25:45 +0300

    :package: generated an elewa-image-container component, imported it into the Home parent component for display

---
+++ libs/elements/layout/images/src/lib/components/elewa-image-container/elewa-image-container.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-image-container',
+  templateUrl: './elewa-image-container.component.html',
+  styleUrls: ['./elewa-image-container.component.scss'],
+})
+export class ElewaImageContainerComponent {}
