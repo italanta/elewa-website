748515e6c67217c55eb7e4192ae35f07d233f10b
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-29 12:12:01 +0300

    :sparkles: feat: add header to homepage

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CardsModule } from '@elewa-website/elements/cards';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 
 import { HomePageComponent } from './main/home/home-page.component';
 import { NewsSectionComponent } from './components/news-section/news-section.component';
@@ -9,7 +10,7 @@ import { NewsSectionComponent } from './components/news-section/news-section.com
 import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, CardsModule],
+  imports: [CommonModule, HomeRoutingModule, CardsModule, AppHeaderModule],
   declarations: [HomePageComponent, NewsSectionComponent],
   exports: [HomePageComponent],
 })
