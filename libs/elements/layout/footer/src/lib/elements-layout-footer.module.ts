b7734c4480beea3dd429ee1a9a1ee40b6bf8547a
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-29 23:29:13 +0300

    :art:imported footer module

--- libs/elements/layout/footer/src/lib/elements-layout-footer.module.ts
+++ libs/elements/layout/footer/src/lib/elements-layout-footer.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './footer/footer.component';
-
+import { FooterComponent } from './components/footer/footer.component';
 @NgModule({
   imports: [CommonModule],
   declarations: [FooterComponent, FooterComponent],
--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -4,10 +4,12 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './main/home/home-page.component';
 
 import { HomeRoutingModule } from './home.routing';
+//import the footer module 
+import { ElementsLayoutFooterModule } from '@elewa-website/elements/layout/footer';
 
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule,ElementsLayoutFooterModule],
   declarations: [HomePageComponent],
   exports: [HomePageComponent],
 })
