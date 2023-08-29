b6e156404538f59d23345dcd548fc9f4421be80b
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-08-29 11:43:06 +0300

    :sparkles: feat: import cardsModule and reusable newsSection comp

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CardsModule } from '@elewa-website/elements/cards';
+
 import { HomePageComponent } from './main/home/home-page.component';
+import { NewsSectionComponent } from './components/news-section/news-section.component';
 
 import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule],
-  declarations: [HomePageComponent],
+  imports: [CommonModule, HomeRoutingModule, CardsModule],
+  declarations: [HomePageComponent, NewsSectionComponent],
   exports: [HomePageComponent],
 })
 export class FeaturesPagesHomeModule {}
