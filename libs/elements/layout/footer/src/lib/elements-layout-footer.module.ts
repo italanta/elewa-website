f909deba8719ead5a123d859249940344ef7c035
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-29 23:20:48 +0300

    :sparkles: created footer component

--- libs/elements/layout/footer/src/lib/elements-layout-footer.module.ts
+++ libs/elements/layout/footer/src/lib/elements-layout-footer.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [FooterComponent, FooterComponent],
 })
 export class ElementsLayoutFooterModule {}
