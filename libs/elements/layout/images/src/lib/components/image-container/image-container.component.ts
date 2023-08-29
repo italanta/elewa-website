98e5f343ef668c8b1d88c477feb7d44c1bd54186
levy <lnjiru43@gmail.com>
2023-08-29 11:50:47 +0300

    removed setter and input setter

--- libs/elements/layout/images/src/lib/components/image-container/image-container.component.ts
+++ libs/elements/layout/images/src/lib/components/image-container/image-container.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 import { ImageConfig } from '@elewa-website/models/schema/ui/images';
+import { ImageVisualisation } from '@elewa-website/models/schema/ui/images';
 
 @Component({
   selector: 'elewa-website-image-container',
@@ -8,58 +9,28 @@ import { ImageConfig } from '@elewa-website/models/schema/ui/images';
   styleUrls: ['./image-container.component.scss'],
 })
 export class ImageContainerComponent {
-  private _imageConfig!: ImageConfig;
+  /**
+   * The configuration object for the image.
+   */
+  @Input() imageConfig!: ImageConfig;
 
   /**
-   * Property to store the class to be used string.
+   * Returns the CSS class name based on the provided image visualization.
+   * @param {ImageVisualisation} visualisation - The image visualization type.
+   * @returns {string} The corresponding CSS class name.
    */
-  imageClassName = '';
-  
-
-  /**
-   * Setter for the `imageConfig` input property.
-   * This method is automatically called when the `imageConfig` input is set externally.
-   * It updates the private `_imageConfig` property and formats the price using commas.
-   * 
-   * @param {PriceItem} value - The new value for the `imageConfig` input.
-   */
-  @Input()
-  set imageConfig(value: ImageConfig) {
-    this._imageConfig = value;
-    this.imageClassName = this.getClassName(value.visualisation);
-  }
-
-  /**
-   * Getter for the `imageConfig` input property.
-   * 
-   * @returns {imageConfig} The current value of the `imageConfig` input.
-   */
-  get imageConfig(): ImageConfig {
-    return this._imageConfig;
-  }
-
-   /**
-   *  function to check visualisation passed and it returns the classname that will be used.
-   * 
-   * @param {any} visualisations - The number to be converted.
-   * @returns {string} The className string.
-   */
-
-  getClassName(visualisations: any){
-    let className = ''
-    if (visualisations == 0 || visualisations === 'pill'){
-      className= "image-pill"
-    }else if(visualisations == 1 || visualisations === 'stacked'){
-      className = 'image-stacked'
-    }else if(visualisations == 2 || visualisations === 'Window'){
-      className = 'image-window'
-    }else if(visualisations == 3 || visualisations === 'card'){
-      className = 'image-card'
-    }else{
-      className = ''
+  getClassName(visualisation: ImageVisualisation): string {
+    switch (visualisation) {
+      case ImageVisualisation.Pill:
+        return 'image-pill';
+      case ImageVisualisation.stacked:
+        return 'image-stacked';
+      case ImageVisualisation.Window:
+        return 'image-window';
+      case ImageVisualisation.Card:
+        return 'image-card';
+      default:
+        return '';
     }
-    return className
   }
-
-
 }
