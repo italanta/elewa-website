9a24a189d1dc9a24906f325b63eeaafb985a5362
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-29 23:29:13 +0300

    :art:imported footer module

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -8,9 +8,11 @@ import { HomePageComponent } from './main/home/home-page.component';
 import { NewsSectionComponent } from './components/news-section/news-section.component';
 
 import { HomeRoutingModule } from './home.routing';
+//import the footer module 
+import { ElementsLayoutFooterModule } from '@elewa-website/elements/layout/footer';
 
 @NgModule({
-  imports: [CommonModule, HomeRoutingModule, CardsModule, AppHeaderModule],
+  imports: [CommonModule, HomeRoutingModule, CardsModule, AppHeaderModule,ElementsLayoutFooterModule],
   declarations: [HomePageComponent, NewsSectionComponent],
   exports: [HomePageComponent],
 })
