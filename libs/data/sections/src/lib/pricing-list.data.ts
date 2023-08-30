96608066b18c40b171ac429a6569108647d9a607
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-31 00:54:18 +0300

    :sparkles: created mock data file

---
+++ libs/data/sections/src/lib/pricing-list.data.ts
@@ -0,0 +1,38 @@
+import { PriceItem } from "@elewa-website/models/schema/ui/cards";
+  export const __highlightedPricingList: PriceItem[]=[
+    {title: 'Bronze',
+    amount:500, 
+    subTitle: 'Basic Features',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sliderButton: {
+      text: 'Choose Plan',
+    },
+    amountColor: '#00FF00',
+    textColor: '#000000',  
+    backgroundColor: '#FFFFFF' 
+  },
+  {
+    title: 'Silver',
+    amount: 1000, 
+    subTitle: 'Advanced Features',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    sliderButton: {
+      text: 'Choose Plan',
+    },
+    amountColor: '#FF0000', 
+    textColor: '#FFFFFF',  
+    backgroundColor: '#000000' 
+  },{
+  title: 'Gold',
+    amount:1500, 
+    subTitle: 'Exculisive Features',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+    sliderButton: {
+      text: 'Choose Plan',
+    },
+    amountColor: '#00FF00',
+    textColor: '#000000',  
+    backgroundColor: '#FFFFFF' 
+  },
+    
+  ]
