811c70d012bab793a6d02fa054c93c0bbac770fe
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-31 01:29:40 +0300

    :sparkles:Initialize pricingList with predefined pricingdata

--- libs/elements/cards/src/lib/components/elewa-price-list/elewa-price-list.component.ts
+++ libs/elements/cards/src/lib/components/elewa-price-list/elewa-price-list.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { __highlightedPricingList } from '@elewa-website/data/sections';
+import { PriceItem } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-elewa-price-list',
   templateUrl: './elewa-price-list.component.html',
   styleUrls: ['./elewa-price-list.component.scss'],
 })
-export class ElewaPriceListComponent {}
+export class ElewaPriceListComponent {
+  pricingList: PriceItem [];
+  constructor(){
+    this.pricingList =__highlightedPricingList;
+  }
+}
