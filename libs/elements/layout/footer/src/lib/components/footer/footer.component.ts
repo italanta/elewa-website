7028f74f39e04de6fcced6ea0b494e30da1e59ad
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-08-30 17:40:20 +0300

    chore: import slider button data in footer and pass it down to slider button element

--- libs/elements/layout/footer/src/lib/components/footer/footer.component.ts
+++ libs/elements/layout/footer/src/lib/components/footer/footer.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
 
+import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
+import { __mockSliderButtonData } from '@elewa-website/data/ui/slider-button';
+
 @Component({
   selector: 'elewa-website-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  buttonData: SliderButtonData = __mockSliderButtonData;
+}
