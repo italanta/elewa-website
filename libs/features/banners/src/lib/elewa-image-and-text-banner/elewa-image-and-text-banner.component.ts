99ee75ad06d09dd98dd1d86d65c3b72d67a5086c
paul <ochiengpaul001@gmail.com>
2023-08-30 21:01:08 +0300

    render data based on the design

--- libs/features/banners/src/lib/elewa-image-and-text-banner/elewa-image-and-text-banner.component.ts
+++ libs/features/banners/src/lib/elewa-image-and-text-banner/elewa-image-and-text-banner.component.ts
@@ -1,21 +1,19 @@
 import { Component, Input } from '@angular/core';
-import { ImageAndText } from '../../../../../models/schema/ui/image-text/src/lib/image-text.module';
 import { ContentText } from 'libs/models/schema/ui/texts/src/lib/content-text.interface';
 import { ImageConfig } from '@elewa-website/models/schema/ui/images';
-import { ImageVisualisation } from '../../../../../models/schema/ui/images/src/index';
+import { ImageVisualisation } from '@elewa-website/models/schema/ui/images';
+
 @Component({
   selector: 'elewa-website-elewa-image-and-text-banner',
   templateUrl: './elewa-image-and-text-banner.component.html',
   styleUrls: ['./elewa-image-and-text-banner.component.scss'],
 })
 export class ElewaImageAndTextBannerComponent {
-  @Input() content!: ContentText;
-  @Input() image!: ImageConfig;
+  @Input() contentData!: ContentText[];
+  @Input() imageData!: ImageConfig[];
 
   getImageVisualisationClass(visualisation: ImageVisualisation): string {
     switch (visualisation) {
-      case ImageVisualisation.Pill:
-        return 'pill';
       case ImageVisualisation.Pill:
         return 'pill';
       default:
--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { ContentText } from 'libs/models/schema/ui/texts/src/lib/content-text.interface';
 import { ImageConfig } from '@elewa-website/models/schema/ui/images';
 import { ImageVisualisation } from '@elewa-website/models/schema/ui/images';
+
 @Component({
   selector: 'elewa-website-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
+  selectedIndex: number = 0; // Specify the desired index here
+
   contentData: ContentText[] = [
     {
       title: 'From here to the world',
