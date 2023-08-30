4d4b1d2b5a14ba724e0f3be099f1cb02313b9069
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-08-30 17:33:28 +0300

    :sparkles: feat: generate slider-button mock data library

---
+++ libs/data/ui/slider-button/README.md
@@ -0,0 +1,7 @@
+# data-ui-slider-button
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test data-ui-slider-button` to execute the unit tests.
---
+++ libs/data/ui/slider-button/src/lib/mock-slider-button.data.ts
@@ -0,0 +1,15 @@
+import { SliderButtonData } from "@elewa-website/models/schema/ui/buttons";
+
+export const __mockSliderButtonData: SliderButtonData = {
+  text: 'Unlock your potential',
+  bgColor: 'black',
+  color: 'white',
+  hoverBgColor: 'white',
+  hoverColor: 'black',
+  borderColor: '#3498db',
+  iconColor: '#3498db',
+  iconBackgroundColor: '#ffffff',
+  hoverBorderColor: '#2980b9',
+  hoverIconColor: 'red',
+  hoverIconBackgroundColor: 'black',
+};
