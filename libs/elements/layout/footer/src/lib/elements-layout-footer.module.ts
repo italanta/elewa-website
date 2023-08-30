871902d51733b30940d40883e86da2e167f9d81e
jbdura <bidurajohnson@gmail.com>
2023-08-30 18:18:04 +0300

    Import slider button in component

--- libs/elements/layout/footer/src/lib/elements-layout-footer.module.ts
+++ libs/elements/layout/footer/src/lib/elements-layout-footer.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+
+import { TranslocoModule } from '@ngneat/transloco';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [ CommonModule, TranslocoModule, ButtonsModule ],
   declarations: [FooterComponent],
   exports: [FooterComponent]
 })
