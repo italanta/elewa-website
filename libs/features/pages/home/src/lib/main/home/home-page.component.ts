420caac309d93018c2db2c7c52f43d8c5ab95594
paul <ochiengpaul001@gmail.com>
2023-08-30 05:02:18 +0300

    create imports at homepage

--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
-
+import { ContentText } from 'libs/models/schema/ui/texts/src/lib/content-text.interface';
+import { ImageConfig } from '@elewa-website/models/schema/ui/images';
 @Component({
   selector: 'elewa-website-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  contentData: ContentText = { /* Populate with content data */ };
+  imageData: ImageConfig = { /* Populate with image configuration data */ };
+}
