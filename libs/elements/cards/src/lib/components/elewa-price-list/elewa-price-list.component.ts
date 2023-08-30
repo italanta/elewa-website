0c138f65827df8492bdee545af064a0af79e7a8a
Preston <prestonosoro56@gmail.com>
2023-08-30 23:03:12 +0300

    feat: import __highlightedPricingList && assign mock data to property

--- libs/elements/cards/src/lib/components/elewa-price-list/elewa-price-list.component.ts
+++ libs/elements/cards/src/lib/components/elewa-price-list/elewa-price-list.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import {__highlightedPricingList } from '@elewa-website/models/data/sections';
+import { PriceItem } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-elewa-price-list',
   templateUrl: './elewa-price-list.component.html',
   styleUrls: ['./elewa-price-list.component.scss'],
 })
-export class ElewaPriceListComponent {}
+export class ElewaPriceListComponent {
+  somePriceItemData: any;
+  pricingList: PriceItem[];
+
+  constructor(){
+    // assign mock data to the property;
+    this.pricingList = __highlightedPricingList;
+  }
+}
