b0d3504bf04a595a085c382e6bad62abc00e4d72
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-08-30 17:26:41 +0300

    chore: import reusable slider button on footer module

--- libs/elements/layout/footer/src/lib/app-footer.module.ts
+++ libs/elements/layout/footer/src/lib/app-footer.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+
 import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonsModule],
   declarations: [FooterComponent],
   exports: [FooterComponent]
 })
