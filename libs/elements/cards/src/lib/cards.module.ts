adace48ebfdfe05b2ea1fd28a73c8aec8f6f13b9
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-31 00:33:32 +0300

    :art::sparkles: created component

--- libs/elements/cards/src/lib/cards.module.ts
+++ libs/elements/cards/src/lib/cards.module.ts
@@ -8,6 +8,7 @@ import { ElewaProjectItemCardComponent } from './components/elewa-project-item-c
 import { ElewaNewsItemCardComponent } from './components/elewa-news-item-card/elewa-news-item-card.component';
 import { ElewaWebsitePriceItemCardComponent } from './components/elewa-price-item-card/elewa-website-price-item-card.component';
 import { ElewaTeamMemberCardComponent } from './components/elewa-team-member-card/team-member-card.component';
+import { ElewaPriceListComponent } from './components/elewa-price-list/elewa-price-list.component';
 
 @NgModule({
   imports: [CommonModule, ButtonsModule],
@@ -17,6 +18,7 @@ import { ElewaTeamMemberCardComponent } from './components/elewa-team-member-car
     ElewaNewsItemCardComponent,
     ElewaProjectItemCardComponent,
     ElewaTeamMemberCardComponent,
+    ElewaPriceListComponent,
   ],
   exports: [
     ElewaInfoCardComponent,
