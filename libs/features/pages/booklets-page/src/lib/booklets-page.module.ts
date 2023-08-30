cf4feb5634ba3696bad7d039206a3e39ef4bb8e8
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-08-31 01:17:39 +0300

    :sparkles: imported Cardmodule intobookletModule

--- libs/features/pages/booklets-page/src/lib/booklets-page.module.ts
+++ libs/features/pages/booklets-page/src/lib/booklets-page.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookletsPageComponent } from './main/booklets-page/booklets-page.component';
 import { BookletsRoutingModule } from './booklets.routing';
-
+import { CardsModule } from '@elewa-website/elements/cards';
 @NgModule({
-  imports: [CommonModule, BookletsRoutingModule],
+  imports: [CommonModule, BookletsRoutingModule,CardsModule],
   declarations: [BookletsPageComponent],
 })
 export class BookletsPageModule {}
