51415419485f7eb8dd445c5f2c28977171fddd86
paul <paulnyoiken@gmail.com>
2023-08-30 17:54:11 +0300

    ✨ feat: added mock footer data in content-text module named mock-footer.data

--- libs/elements/layout/footer/src/lib/components/footer/footer.component.ts
+++ libs/elements/layout/footer/src/lib/components/footer/footer.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { footerLinks, socialMedia } from '@elewa-website/data/ui/content-text';
 
 @Component({
   selector: 'elewa-website-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+export class FooterComponent {
+  footerLinks = footerLinks
+  socialMedia = socialMedia
+}
