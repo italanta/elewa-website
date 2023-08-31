f7942ca759cd2b329b5de75f42fd0a2d12b1031e
Preston <prestonosoro56@gmail.com>
2023-08-31 12:07:54 +0300

    feat: import CardsModule

--- libs/features/pages/about/src/lib/features-pages-about.module.ts
+++ libs/features/pages/about/src/lib/features-pages-about.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AboutPageComponent } from './main/about-page/about-page.component';
 
 import { AboutRoutingModule } from './about.routing'
+import { CardsModule } from '@elewa-website/elements/cards';
 
 @NgModule({
-  imports: [CommonModule, AboutRoutingModule],
+  imports: [CommonModule, AboutRoutingModule, CardsModule],
   declarations: [AboutPageComponent],
 })
 export class FeaturesPagesAboutModule {}
