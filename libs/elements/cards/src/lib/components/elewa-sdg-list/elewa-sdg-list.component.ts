3b9ccf958be2a48984eb5a03c82a5d31817cf54e
Preston <prestonosoro56@gmail.com>
2023-08-31 11:05:40 +0300

    feat: move the component to the required path

---
+++ libs/elements/cards/src/lib/components/elewa-sdg-list/elewa-sdg-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { InfoCard } from '@elewa-website/models/schema/ui/cards';
+
+@Component({
+  selector: 'elewa-website-elewa-sdg-list',
+  templateUrl: './elewa-sdg-list.component.html',
+  styleUrls: ['./elewa-sdg-list.component.scss'],
+})
+export class ElewaSdgListComponent {
+  @Input() sdgList: InfoCard[] = [];
+
+}
