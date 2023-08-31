0e857c4a7bfc653d4ad12ddf773dc88ed87ad9e2
Preston <prestonosoro56@gmail.com>
2023-08-31 09:18:48 +0300

    feat: refactor data to match the required deliverable

--- libs/models/data/sections/src/lib/pricing-list.data.ts
+++ libs/models/data/sections/src/lib/pricing-list.data.ts
@@ -3,24 +3,36 @@ import { PriceItem } from "@elewa-website/models/schema/ui/cards";
 // Define your mock data for pricing list
 export const __highlightedPricingList: PriceItem[] = [
   {
-    title: 'Basic Plan',
-    amount: 100, // Example amount
-    subTitle: 'Standard Features',
+    title: 'Elewa Model',
+    amount:  500, // Example amount
+    subTitle: 'Lorem ipsum',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     sliderButton: {
-      text: 'Choose Plan',
+      text: 'Unlock your potential',
     },
     amountColor: '#00FF00', // Example amount color
     textColor: '#000000',   // Example text color
     backgroundColor: '#FFFFFF' // Example background color
   },
   {
-    title: 'Premium Plan',
-    amount: 200, // Example amount
-    subTitle: 'Advanced Features',
+    title: 'Elewa Model',
+    amount: 1000, // Example amount
+    subTitle: 'Lorem ipsum',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    sliderButton: {
+      text: 'Unlock your potential',
+    },
+    amountColor: '#00FF00', // Example amount color
+    textColor: '#000000',   // Example text color
+    backgroundColor: '#FFFFFF' // Example background color
+  },
+  {
+    title: 'Elewa Model',
+    amount: 1250, // Example amount
+    subTitle: 'Lorem ipsum',
     description: 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
     sliderButton: {
-      text: 'Choose Plan',
+      text: 'Unlock your potential',
     },
     amountColor: '#FF0000', // Example amount color
     textColor: '#FFFFFF',   // Example text color
