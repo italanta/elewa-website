d8365c442712e813f9cdcb6fb06db9faffbf2654
Preston <prestonosoro56@gmail.com>
2023-08-31 21:31:44 +0300

    feat: mock data

---
+++ libs/data/ui/content-text/src/lib/mock-slider-button.data.ts
@@ -0,0 +1,15 @@
+import { SliderButtonData } from "@elewa-website/models/schema/ui/buttons";
+
+export const __mockSliderButtonData: SliderButtonData = {
+  text: 'Learn more about SDG',
+  bgColor: 'transparent',
+  color: 'var(--elewa-website-color-primary-white)',
+  hoverBgColor: 'white',
+  hoverColor: 'var(--elewa-website-color-primary-black)',
+  borderColor: 'var(--elewa-website-color-primary-white)',
+  iconColor: 'var(--sdg-card-background-color)',
+  iconBackgroundColor: 'var(--elewa-website-color-primary-white)',
+  hoverBorderColor: 'var(--elewa-website-color-primary-black)',
+  hoverIconColor: 'var(--elewa-website-color-primary-white)',
+  hoverIconBackgroundColor: 'var(--elewa-website-color-primary-black)',
+};
