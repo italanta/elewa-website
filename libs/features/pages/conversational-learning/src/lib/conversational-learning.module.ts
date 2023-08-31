ece6ab9af34478619ce869d4cd663420aea83ad7
dancunrandy <dancunrandy@gmail.com>
2023-08-31 19:46:13 +0300

    styling

--- libs/features/pages/conversational-learning/src/lib/conversational-learning.module.ts
+++ libs/features/pages/conversational-learning/src/lib/conversational-learning.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { ConversationalLearningPageComponent } from './main/conversational-learning-page/conversational-learning-page.component';
 
 import { ConversationalLearningRoutingModule } from './conversational-learning-routing.module';
+import { ConvHeroSectionComponent } from './component/conv-hero-section./conv-hero-section..component';
 
 @NgModule({
   imports: [CommonModule, ConversationalLearningRoutingModule],
-  declarations: [ConversationalLearningPageComponent],
+  declarations: [ConversationalLearningPageComponent, ConvHeroSectionComponent],
 })
 export class ConversationalLearningModule {}
