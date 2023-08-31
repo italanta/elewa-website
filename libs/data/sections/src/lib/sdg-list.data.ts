e0d90606756d7e282d3291e2ecc94c24ae6e7d21
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-31 13:12:01 +0300

    :lipstick: styled the sdg list items

--- libs/data/sections/src/lib/sdg-list.data.ts
+++ libs/data/sections/src/lib/sdg-list.data.ts
@@ -2,28 +2,28 @@ import { InfoCard } from "@elewa-website/models/schema/ui/cards";
 
 export const __highlightedSDGList: InfoCard[] = [
     {
-        title: "SDG 1",
-        description: "The first Sustainable Development Goals (SDG) is the foundation of sustainable development. It is a measure of how sustainable the economy is and how well it is being developed.",
+        title: "Quality Education",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
         icon: "fas fa-book-open"
     },
     {
-        title: "SDG 2",
-        description: "The second Sustainable Development Goals (SDG) is the foundation of sustainable development. It is a measure of how sustainable the economy is and how well it is being developed.",
+        title: "Reduced Inequalities",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
         icon: "fas fa-balance-scale-left"
     },
     {
-        title: "SDG 3",
-        description: "The third Sustainable Development Goals (SDG) is the foundation of sustainable development. It is a measure of how sustainable the economy is and how well it is being developed.",
+        title: "Gender Equality",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
         icon: "fas fa-venus-mars"
     },
     {
-        title: "SDG 4",
-        description: "The fourth Sustainable Development Goals (SDG) is the foundation of sustainable development. It is a measure of how sustainable the economy is and how well it is being developed.",
+        title: "Economic Growth",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
         icon: "fas fa-chart-line"
     },
     {
-        title: "SDG 5",
-        description: "The fifth Sustainable Development Goals (SDG) is the foundation of sustainable development. It is a measure of how sustainable the economy is and how well it is being developed.",
+        title: "Partnership For The Goals",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
         icon: "fas fa-handshake"
     }
 ]
