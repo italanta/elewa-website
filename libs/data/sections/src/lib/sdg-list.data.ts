2705f129fc2fabd21052b7f9d50ca45ca3a10a03
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-31 10:51:56 +0300

    :lipstick: created the sdg list mock data

---
+++ libs/data/sections/src/lib/sdg-list.data.ts
@@ -0,0 +1,29 @@
+import { InfoCard } from "@elewa-website/models/schema/ui/cards";
+
+export const __highlightedSDGList: InfoCard[] = [
+    {
+        title: "SDG 1",
+        description: "The first Sustainable Development Goals (SDG) is the foundation of sustainable development. It is a measure of how sustainable the economy is and how well it is being developed.",
+        icon: "sdg1"
+    },
+    {
+        title: "SDG 2",
+        description: "The second Sustainable Development Goals (SDG) is the foundation of sustainable development. It is a measure of how sustainable the economy is and how well it is being developed.",
+        icon: "sdg2"
+    },
+    {
+        title: "SDG 3",
+        description: "The third Sustainable Development Goals (SDG) is the foundation of sustainable development. It is a measure of how sustainable the economy is and how well it is being developed.",
+        icon: "sdg3"
+    },
+    {
+        title: "SDG 4",
+        description: "The fourth Sustainable Development Goals (SDG) is the foundation of sustainable development. It is a measure of how sustainable the economy is and how well it is being developed.",
+        icon: "sdg4"
+    },
+    {
+        title: "SDG 5",
+        description: "The fifth Sustainable Development Goals (SDG) is the foundation of sustainable development. It is a measure of how sustainable the economy is and how well it is being developed.",
+        icon: "sdg5"
+    }
+]
--- libs/elements/cards/src/lib/components/elewa-sdg-list/elewa-sdg-list.component.ts
+++ libs/elements/cards/src/lib/components/elewa-sdg-list/elewa-sdg-list.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { InfoCard } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-elewa-sdg-list',
   templateUrl: './elewa-sdg-list.component.html',
   styleUrls: ['./elewa-sdg-list.component.scss'],
 })
-export class ElewaSdgListComponent {}
+export class ElewaSdgListComponent {
+  @Input () cardsData: InfoCard[] = [];
+}
