89e33863183c97263c2a1459242b8c81e6e3690e
kevinkkimutai <kiruikevinkim@gmail.com>
2023-08-31 14:20:10 +0300

    ✨ feat: generate footer component

--- apps/elewa-website/src/app/app.module.ts
+++ apps/elewa-website/src/app/app.module.ts
@@ -6,10 +6,13 @@ import { AppComponent } from './app.component';
 import { TranslocoRootModule } from './transloco-root.module';
 
 import { AppRoutingModule } from './app.routing';
+import { ElementsLayoutFooterModule } from '@elewa-website/elements/layout/footer';
+
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, ElementsLayoutFooterModule],
   providers: [HttpClientModule, TranslocoRootModule],
   bootstrap: [AppComponent],
 })
