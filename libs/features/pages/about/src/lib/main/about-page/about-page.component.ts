44379fad44ed91439090e906b96f4e0fbc13a990
Preston <prestonosoro56@gmail.com>
2023-08-31 12:10:10 +0300

    feat:import __highlightedSDGList && InfoCard

--- libs/features/pages/about/src/lib/main/about-page/about-page.component.ts
+++ libs/features/pages/about/src/lib/main/about-page/about-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { __highlightedSDGList } from '@elewa-website/models/data/sections';
+import { InfoCard } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-about-page',
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.scss'],
 })
-export class AboutPageComponent {}
+export class AboutPageComponent {
+  sdgListData: InfoCard[] = __highlightedSDGList; // Assign your data here
+}
