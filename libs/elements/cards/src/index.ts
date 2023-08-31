53c83badfcecd61451bb11ae631c7865e32cab28
Preston <prestonosoro56@gmail.com>
2023-08-31 11:12:39 +0300

    feat: export ElewaSdgListComponent

--- libs/elements/cards/src/index.ts
+++ libs/elements/cards/src/index.ts
@@ -1,2 +1,3 @@
 export * from './lib/cards.module';
 export { ElewaPriceListComponent } from './lib/components/elewa-price-list/elewa-price-list.component';
+export {ElewaSdgListComponent} from './lib/components/elewa-sdg-list/elewa-sdg-list.component';
