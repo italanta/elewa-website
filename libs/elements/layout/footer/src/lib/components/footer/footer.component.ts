6235c51d8895196fa2a8a6434aecd41616021be6
jbdura <bidurajohnson@gmail.com>
2023-08-31 11:23:05 +0300

    edit button data

--- libs/elements/layout/footer/src/lib/components/footer/footer.component.ts
+++ libs/elements/layout/footer/src/lib/components/footer/footer.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
 
+
+// socials urls
+const ig = "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/_Social/instagram_x8ujcq.svg"
+const linkedIn = "https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690294/elewa-group-website/Icons/SVG/_Social/linkedin_x8smhm.svg"
+const fb ="https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690294/elewa-group-website/Icons/SVG/_Social/facebook_lhhwk2.svg"
+
+
 @Component({
   selector: 'elewa-website-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
-export class FooterComponent {}
+
+
+export class FooterComponent  {
+  fb_URL = fb;
+  ig_URL = ig;
+  linkedIn_URL = linkedIn;
+
+  //Button data info
+  buttonData = {
+    text: 'Unlock your potential ', // Fill the require
+    bgColor: 'transparent',
+    color: 'white',
+    iconBackgroundColor: 'white',
+    hoverIconColor: 'black',
+    hoverIconackgroundColor: 'black',
+    hoverBgColor: 'white',
+    hoverColor:'black',
+    iconColor: 'black'
+  }
+}
