48b87076261d4df64cdfb884fcd2fee0c2962bd3
Preston <prestonosoro56@gmail.com>
2023-08-31 10:50:15 +0300

    feat: creat mock data

---
+++ libs/models/data/sections/src/lib/sdg-list.data.ts
@@ -0,0 +1,15 @@
+import { InfoCard } from '@elewa-website/models/schema/ui/cards';
+
+export const __highlightedSDGList: InfoCard[] = [
+  {
+    title: 'SDG 1: No Poverty',
+    description: 'End poverty in all its forms everywhere.',
+    icon: 'sdg1.svg', // You can specify the icon URL or path here
+  },
+  {
+    title: 'SDG 2: Zero Hunger',
+    description: 'End hunger, achieve food security and improved nutrition.',
+    icon: 'sdg2.svg',
+  },
+  // Add more SDG items as needed
+];
