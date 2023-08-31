88a0b0a0b5420a59b616f73911e9e16778fd0d9d
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-08-31 11:15:43 +0300

    chore: add highlited-SDG-List mock data

---
+++ libs/data/sections/src/lib/highlighted-sdg-list.data.ts
@@ -0,0 +1,34 @@
+import { InfoCard } from '@elewa-website/models/schema/ui/cards';
+
+export const __highlightedSDGList: InfoCard[] = [
+  {
+    title: 'Quality Education',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor',
+    icon: '',
+  },
+  {
+    title: 'Quality Education',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor',
+    icon: '',
+  },
+  {
+    title: 'Quality Education',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor',
+    icon: '',
+  },
+  {
+    title: 'Quality Education',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor',
+    icon: '',
+  },
+  {
+    title: 'Quality Education',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor',
+    icon: '',
+  },
+];
