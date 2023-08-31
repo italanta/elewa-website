58efbf5d40b0cf25b294e3520bbee18e95f9a8f6
Preston <prestonosoro56@gmail.com>
2023-08-31 12:31:59 +0300

    feat: create SdgSectionComponent

---
+++ libs/features/pages/about/src/lib/sdg-section/sdg-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-sdg-section',
+  templateUrl: './sdg-section.component.html',
+  styleUrls: ['./sdg-section.component.scss'],
+})
+export class SdgSectionComponent {}
