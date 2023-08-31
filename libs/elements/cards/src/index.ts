844ca69b1a63cc2debb6ee82e799dd16d98a82e0
Preston <prestonosoro56@gmail.com>
2023-08-31 23:07:51 +0300

    feat: remove exported components

--- libs/elements/cards/src/index.ts
+++ libs/elements/cards/src/index.ts
@@ -1,3 +1,1 @@
 export * from './lib/cards.module';
-export { ElewaPriceListComponent } from './lib/components/elewa-price-list/elewa-price-list.component';
-export {ElewaSdgListComponent} from './lib/components/elewa-sdg-list/elewa-sdg-list.component';
