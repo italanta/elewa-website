c442cfd70c7fd5208363622e799ffcbf8b17bc53
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-08-31 11:00:59 +0300

    :lipstick: added property in the home template

--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { __highlightedSDGList } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-home',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  highlightedSDGList = __highlightedSDGList;
+}
