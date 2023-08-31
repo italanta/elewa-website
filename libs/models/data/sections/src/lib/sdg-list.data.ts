9d98649116b9171ebb1c1757413995d828d55b6e
Preston <prestonosoro56@gmail.com>
2023-08-31 21:32:34 +0300

    feat: sdg data | slider button

--- libs/models/data/sections/src/lib/sdg-list.data.ts
+++ libs/models/data/sections/src/lib/sdg-list.data.ts
@@ -1,3 +1,4 @@
+import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
 import { InfoCard } from '@elewa-website/models/schema/ui/cards';
 
 export const __highlightedSDGList: InfoCard[] = [
@@ -7,14 +8,14 @@ export const __highlightedSDGList: InfoCard[] = [
     icon: 'sdg1.svg', // You can specify the icon URL or path here
   },
   {
-    title: 'Economic Growth',
+    title:'Economic Growth',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
     icon: 'sdg2.svg',
   },
   {
     title: 'Reduce Inequalities',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    icon: 'sdg3.svg',
+    icon:'sdg3.svg',
   },
   {
     title: 'Parnership fo the Goals',
@@ -28,3 +29,17 @@ export const __highlightedSDGList: InfoCard[] = [
   },
   // Add more SDG items as needed
 ];
+
+export const __mockSliderButtonData: SliderButtonData = {
+  text: 'Learn more about SDG',
+  bgColor: 'transparent',
+  color: 'var(--elewa-website-color-primary-white)',
+  hoverBgColor: 'white',
+  hoverColor: 'var(--elewa-website-color-primary-black)',
+  borderColor: 'var(--elewa-website-color-primary-white)',
+  iconColor: 'var(--sdg-card-background-color)',
+  iconBackgroundColor: 'var(--elewa-website-color-primary-white)',
+  hoverBorderColor: 'var(--elewa-website-color-primary-black)',
+  hoverIconColor: 'var(--elewa-website-color-primary-white)',
+  hoverIconBackgroundColor: 'var(--elewa-website-color-primary-black)',
+};
