1618137088dface659dc7bac31c7606e2f704399
levy <lnjiru43@gmail.com>
2023-08-31 19:55:51 +0300

    mock data for members

---
+++ libs/data/sections/src/lib/team-members-list.data.ts
@@ -0,0 +1,23 @@
+import { TeamMember } from "@elewa-website/models/schema/ui/cards";
+
+export const __highlightedTeamMembers: TeamMember[] = [
+    {
+      fullName: 'John Doe',
+      imgSrc: 'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
+    },
+
+    {
+    fullName: 'Mark zukerburg',
+    imgSrc: 'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
+    },
+
+    {
+    fullName: 'Elon Musk',
+    imgSrc: 'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
+    },
+    {
+      fullName: 'Elon Musk',
+      imgSrc: 'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
+      }
+  //more data
+];
