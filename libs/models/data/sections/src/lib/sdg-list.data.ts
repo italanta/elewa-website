5e9dd0e0ae5354f7f119dd9ed81e60e02a73f291
Preston <prestonosoro56@gmail.com>
2023-08-31 22:35:58 +0300

    feat: add link to icons

--- libs/models/data/sections/src/lib/sdg-list.data.ts
+++ libs/models/data/sections/src/lib/sdg-list.data.ts
@@ -5,27 +5,27 @@ export const __highlightedSDGList: InfoCard[] = [
   {
     title: 'Quality Education',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    icon: 'sdg1.svg', // You can specify the icon URL or path here
+    icon: 'https://thenounproject.com/api/private/icons/3262972/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0', // You can specify the icon URL or path here
   },
   {
-    title:'Economic Growth',
+    title: 'Economic Growth',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    icon: 'sdg2.svg',
+    icon: 'https://thenounproject.com/api/private/icons/4795689/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0', // You can specify the icon URL or path here
   },
   {
-    title: 'Reduce Inequalities',
+    title: 'Reduced Inequalities',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    icon:'sdg3.svg',
+    icon: 'https://thenounproject.com/api/private/icons/3194319/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0',
   },
   {
     title: 'Parnership fo the Goals',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    icon: 'sdg4.svg',
+    icon: 'https://thenounproject.com/api/private/icons/5750943/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0',
   },
   {
     title: 'Gender Equality',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-    icon: 'sdg5.svg',
+    icon: 'https://thenounproject.com/api/private/icons/1401165/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0',
   },
   // Add more SDG items as needed
 ];
