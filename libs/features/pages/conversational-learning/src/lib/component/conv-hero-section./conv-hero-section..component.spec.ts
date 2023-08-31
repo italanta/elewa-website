3d4d230977beff9aa7828a5ddb8d63001ae67fc7
dancunrandy <dancunrandy@gmail.com>
2023-08-31 19:45:42 +0300

    added style

---
+++ libs/features/pages/conversational-learning/src/lib/component/conv-hero-section./conv-hero-section..component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ConvHeroSectionComponent } from './conv-hero-section..component';
+
+describe('ConvHeroSectionComponent', () => {
+  let component: ConvHeroSectionComponent;
+  let fixture: ComponentFixture<ConvHeroSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ConvHeroSectionComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ConvHeroSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
