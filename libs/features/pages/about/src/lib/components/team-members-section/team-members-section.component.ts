eae08d8a3ad33a3377642a2299e9a94b7eb2ddad
levy <lnjiru43@gmail.com>
2023-08-31 14:21:53 +0300

    :sparkles: generated team member section component

---
+++ libs/features/pages/about/src/lib/components/team-members-section/team-members-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-team-members-section',
+  templateUrl: './team-members-section.component.html',
+  styleUrls: ['./team-members-section.component.scss'],
+})
+export class TeamMembersSectionComponent {}
--- libs/features/pages/about/src/lib/features-pages-about.module.ts
+++ libs/features/pages/about/src/lib/features-pages-about.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AboutPageComponent } from './main/about-page/about-page.component';
 
-import { AboutRoutingModule } from './about.routing'
+import { AboutRoutingModule } from './about.routing';
+import { TeamMembersSectionComponent } from './components/team-members-section/team-members-section.component';
 
 @NgModule({
   imports: [CommonModule, AboutRoutingModule],
-  declarations: [AboutPageComponent],
+  declarations: [AboutPageComponent, TeamMembersSectionComponent],
 })
 export class FeaturesPagesAboutModule {}
