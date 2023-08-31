01b520661287afbefc75e140fa926994c3a9e049
Oduol Peris <oduolpepe618@gmail.com>
2023-08-31 12:25:54 +0300

    🎨feat: Mock news items are defined

---
+++ libs/data/sections/src/lib/pricing-list.data.ts
@@ -0,0 +1,26 @@
+import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
+import { PriceItem } from '@elewa-website/models/schema/ui/cards';
+
+// const tempSliderButton: SliderButtonData = {
+//   label: 'Select Plan',
+//   action: 'selectPlan',
+// };
+/** price list mock data*/
+export const __highlightedPricingList: PriceItem[] = [
+  {
+    title: 'Elewa Model',
+    amount: 500,
+    subTitle: 'Lorem Ipsum',
+    description:'Pretium quam vulputate dignissim suspendisse in est ante in. Ultrices gravida dictum fusce ut placerat orci nulla. Ipsum consequat nisl vel pretium lectus quam id leo. Rutrum tellus pellentesque eu tincidunt tortor aliquam.',
+    sliderButton: {
+      text: 'Unlock your potential',
+
+
+    },
+    amountColor: '#292A50',
+    textColor: '#000000',
+    backgroundColor: '', 
+  },
+  
+];
+
