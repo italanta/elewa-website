559dd21a7b9e8910ab8701468e98947d67b40152
levy <lnjiru43@gmail.com>
2023-08-31 19:57:47 +0300

    imported cards module and ElementsLayoutCarouselModule so as to use in pages module

--- libs/features/pages/about/src/lib/features-pages-about.module.ts
+++ libs/features/pages/about/src/lib/features-pages-about.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CardsModule } from '@elewa-website/elements/cards';
+import { ElementsLayoutCarouselModule } from '@elewa-website/elements/layout/carousel';
+
 import { AboutPageComponent } from './main/about-page/about-page.component';
 
 import { AboutRoutingModule } from './about.routing';
 import { TeamMembersSectionComponent } from './components/team-members-section/team-members-section.component';
 
 @NgModule({
-  imports: [CommonModule, AboutRoutingModule],
+  imports: [CommonModule, AboutRoutingModule, ElementsLayoutCarouselModule, CardsModule],
   declarations: [AboutPageComponent, TeamMembersSectionComponent],
 })
 export class FeaturesPagesAboutModule {}
