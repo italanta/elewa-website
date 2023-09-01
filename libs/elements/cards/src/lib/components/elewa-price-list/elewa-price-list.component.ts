bac9a4f1786e4397da45dca4f8f5602b845bd0e3
mutagah <kihahumutagah@gmail.com>
2023-09-01 10:32:33 +0300

    :art:lipstick:feat: Created the elewa-price-list-component

---
+++ libs/elements/cards/src/lib/components/elewa-price-list/elewa-price-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+import { ElewaWebsitePriceItemCardComponent } from '../elewa-price-item-card/elewa-website-price-item-card.component';
+@Component({
+  selector: 'elewa-website-elewa-price-list',
+  templateUrl: './elewa-price-list.component.html',
+  styleUrls: ['./elewa-price-list.component.scss']
+})
+export class ElewaPriceListComponent {
+  
+}
