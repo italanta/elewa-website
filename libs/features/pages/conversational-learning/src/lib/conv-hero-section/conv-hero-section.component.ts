34e153f3c8502ccc6f500bebfe54f62c9401537c
Steve Kariuki <stevekariuki5@gmail.com>
2023-09-01 10:15:05 +0300

    :sparkles: created conv-hero-section component

---
+++ libs/features/pages/conversational-learning/src/lib/conv-hero-section/conv-hero-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'elewa-website-conv-hero-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './conv-hero-section.component.html',
+  styleUrls: ['./conv-hero-section.component.scss'],
+})
+export class ConvHeroSectionComponent {}
