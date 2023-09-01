6a04eaf13742d7e1abeb1cb1eb6fe93919d3a77f
Terry Wachuka <79338912+T-erry@users.noreply.github.com>
2023-09-01 10:07:21 +0300

    :sparkles: add reusable image container
    * set up
    
    * created a image component
    
    * created imageConfig interface
    
    * displayed the images
    
    * removed title
    
    * ✨ feat: added images in ImageModule
    
    * :recycle: refactor: add example env files
    
    ---------
    
    Co-authored-by: lemmyMwaura <lemmymwauracodes@gmail.com>

---
+++ libs/elements/layout/images/src/lib/images.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ElewaImageContainerComponent } from './elewa-image-container/elewa-image-container.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ElewaImageContainerComponent],
+  exports: [ElewaImageContainerComponent]
+})
+export class ImagesModule {}
