5578428a094c2ef09391ff93033da0a89d12c649
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-09-01 15:28:40 +0300

    :recycle: change icons data

--- libs/data/sections/src/lib/highlighted-sdg-list.data.ts
+++ libs/data/sections/src/lib/highlighted-sdg-list.data.ts
@@ -5,30 +5,30 @@ export const __highlightedSDGList: InfoCard[] = [
     title: 'Quality Education',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor',
-    icon: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/_Social/instagram_x8ujcq.svg',
+    icon: 'fa-solid fa-book-open',
   },
   {
     title: 'Reduced Inequalities',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor',
-    icon: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/_Social/instagram_x8ujcq.svg',
+    icon: 'fa-solid fa-arrows-up-down-left-right',
   },
   {
     title: 'Gender Equality',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor',
-    icon: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/_Social/instagram_x8ujcq.svg',
+    icon: 'fa-solid fa-arrows-to-circle',
   },
   {
     title: 'Economic Growth',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor',
-    icon: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/_Social/instagram_x8ujcq.svg',
+    icon: 'fa-solid fa-arrow-up-right-dots',
   },
   {
     title: 'Partnership for the goals',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do elusmod tempor',
-    icon: 'https://res.cloudinary.com/dyl3rncv3/image/upload/v1675690295/elewa-group-website/Icons/SVG/_Social/instagram_x8ujcq.svg',
+    icon: 'fa-solid fa-check-double',
   },
 ];
