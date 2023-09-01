959aad37e6d530626b9b56256c143af3f3d51612
Steve Kariuki <stevekariuki5@gmail.com>
2023-09-01 12:16:35 +0300

    :recycle: undoing changes made in testing

--- libs/features/pages/conversational-learning/src/lib/conversational-learning.module.ts
+++ libs/features/pages/conversational-learning/src/lib/conversational-learning.module.ts
@@ -3,10 +3,9 @@ import { CommonModule } from '@angular/common';
 import { ConversationalLearningPageComponent } from './main/conversational-learning-page/conversational-learning-page.component';
 
 import { ConversationalLearningRoutingModule } from './conversational-learning-routing.module';
-import { ConvHeroSectionComponent } from './conv-hero-section/conv-hero-section.component';
 
 @NgModule({
-  imports: [CommonModule, ConversationalLearningRoutingModule, ConvHeroSectionComponent],
+  imports: [CommonModule, ConversationalLearningRoutingModule],
   declarations: [ConversationalLearningPageComponent],
 })
 export class ConversationalLearningModule {}
