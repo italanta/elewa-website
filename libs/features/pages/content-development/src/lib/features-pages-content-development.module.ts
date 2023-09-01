1369eb72d5fa64d4b2f3c6b8d3b34e933abd0183
levy <lnjiru43@gmail.com>
2023-09-01 10:49:00 +0300

    imported and re used elewa website header

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
+
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
+import { ContentDevHeroSectionComponent } from './components/content-dev-hero-section/content-dev-hero-section.component';
 
 @NgModule({
-  imports: [CommonModule, ContentDevelopmentPageRoutingModule],
-  declarations: [ContentDevelopmentPageComponent],
-  exports: [ContentDevelopmentPageComponent]
+  imports: [CommonModule, ContentDevelopmentPageRoutingModule, AppHeaderModule],
+  declarations: [
+    ContentDevelopmentPageComponent,
+    ContentDevHeroSectionComponent,
+  ],
+  exports: [ContentDevelopmentPageComponent],
 })
 export class ContentDevelopmentModule {}
