4679daa91e7472dcb4aca45c67d457d46c2dd223
Preston <prestonosoro56@gmail.com>
2023-09-01 12:58:40 +0300

    feat: generate content-dev-hero-section component

---
+++ libs/features/pages/content-development/src/lib/content-dev-hero-section/content-dev-hero-section.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'elewa-website-content-dev-hero-section',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './content-dev-hero-section.component.html',
+  styleUrls: ['./content-dev-hero-section.component.scss'],
+})
+export class ContentDevHeroSectionComponent {}
