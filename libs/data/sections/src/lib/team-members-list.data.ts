95a8f5d8ebc6f96edec77f3e39d55fd09eeb61e5
levy <lnjiru43@gmail.com>
2023-09-01 10:10:21 +0300

    added one more mock data

--- libs/data/sections/src/lib/team-members-list.data.ts
+++ libs/data/sections/src/lib/team-members-list.data.ts
@@ -16,8 +16,13 @@ export const __highlightedTeamMembers: TeamMember[] = [
     imgSrc: 'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
     },
     {
-      fullName: 'Elon Musk',
-      imgSrc: 'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
-      }
+    fullName: 'Elon Musk',
+    imgSrc: 'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
+    },
+    {
+    fullName: 'Elon Musk',
+    imgSrc: 'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80'
+    },
+
   //more data
 ];
