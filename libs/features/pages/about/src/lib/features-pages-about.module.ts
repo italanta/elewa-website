7a0b866afa624fcbf477f53561babb3358bacbbd
levy <lnjiru43@gmail.com>
2023-09-01 13:23:05 +0300

    added translation for the website header text of team mamber section

--- libs/features/pages/about/src/lib/features-pages-about.module.ts
+++ libs/features/pages/about/src/lib/features-pages-about.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TranslocoModule } from '@ngneat/transloco';
+
 import { CardsModule } from '@elewa-website/elements/cards';
 import { ElementsLayoutCarouselModule } from '@elewa-website/elements/layout/carousel';
 
@@ -10,7 +12,7 @@ import { AboutRoutingModule } from './about.routing';
 import { TeamMembersSectionComponent } from './components/team-members-section/team-members-section.component';
 
 @NgModule({
-  imports: [CommonModule, AboutRoutingModule, ElementsLayoutCarouselModule, CardsModule],
+  imports: [CommonModule, AboutRoutingModule, ElementsLayoutCarouselModule, CardsModule, TranslocoModule],
   declarations: [AboutPageComponent, TeamMembersSectionComponent],
 })
 export class FeaturesPagesAboutModule {}
