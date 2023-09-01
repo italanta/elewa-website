152f053f0509ecd7bf01975c7925a02d7d5c5b18
levy <lnjiru43@gmail.com>
2023-09-01 10:47:45 +0300

    :sparkles: generated content dev hero sction component

---
+++ libs/features/pages/content-development/src/lib/components/content-dev-hero-section/content-dev-hero-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-content-dev-hero-section',
+  templateUrl: './content-dev-hero-section.component.html',
+  styleUrls: ['./content-dev-hero-section.component.scss'],
+})
+export class ContentDevHeroSectionComponent {}
