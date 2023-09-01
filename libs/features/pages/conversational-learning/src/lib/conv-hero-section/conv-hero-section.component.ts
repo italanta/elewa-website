3a6ed23d6d742b7b6561cf90124ffe486b4c9a47
Steve Kariuki <stevekariuki5@gmail.com>
2023-09-01 11:09:16 +0300

    :lipstick: styling and structuring

--- libs/features/pages/conversational-learning/src/lib/conv-hero-section/conv-hero-section.component.ts
+++ libs/features/pages/conversational-learning/src/lib/conv-hero-section/conv-hero-section.component.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 import { TextsModule } from '@elewa-website/elements/layout/texts';
-
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 @Component({
   selector: 'elewa-website-conv-hero-section',
   standalone: true,
-  imports: [CommonModule, AppHeaderModule, TextsModule],
+  imports: [CommonModule, AppHeaderModule, TextsModule, ButtonsModule],
   templateUrl: './conv-hero-section.component.html',
   styleUrls: ['./conv-hero-section.component.scss'],
 })
 export class ConvHeroSectionComponent {
   heroTextData = {
-    title: 'Welcome to My Angular App',
-    subtitle: 'Built with Angular',
-    description: 'This is a sample Angular app.'
+    title: 'Conversational Learning',
+    subtitle: '',
+    description: 'Your organisation has the potential to turn every task into an opportunity to learn. From growing crops, to nursing someone to good health, to developing the next-generation technology, let’s work together to make the magic happen.'
   };
+  sliderButtonData = {
+    text: 'Unlock your potential',
+  }
 }
