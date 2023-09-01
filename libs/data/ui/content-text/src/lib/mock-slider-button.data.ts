19076cd02f9e9f9e00b773a3647c35791f41bf61
+ <+>
2023-09-01 13:19:19 +0300

    :sparkles: created mock data for the slider Button

---
+++ libs/data/ui/content-text/src/lib/mock-slider-button.data.ts
@@ -0,0 +1,15 @@
+import { SliderButtonData } from "@elewa-website/models/schema/ui/buttons";
+
+export const _mockSliderButtonData : SliderButtonData = {
+    text: 'Learn more about SDG',
+    bgColor: 'transparent',
+    color: 'var(--elewa-website-color-primary-white)',
+    borderColor: 'var(--elewa-website-color-primary-white)',
+    iconColor: 'var(--elewa-website-sdg-background-color)',
+    iconBackgroundColor: 'var(--elewa-website-color-primary-white)',
+    hoverBgColor: 'var(--elewa-website-color-primary-white)',
+    hoverColor: 'var(--elewa-website-color-primary-black)',
+    hoverBorderColor: 'var(--elewa-website-color-primary-black)',
+    hoverIconColor: 'var(--elewa-website-color-primary-white)',
+    hoverIconBackgroundColor: 'var(--elewa-website-color-primary-black)'
+}
