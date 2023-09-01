b4e5da733e938ccdb214b1e641a703bf5b5c83d4
KaranjaK <karanjakelvin77@gmail.com>
2023-09-01 10:08:59 +0300

    🩹 Updates the image links in the component

--- libs/elements/layout/images/src/lib/components/elewa-image-container/elewa-image-container.component.ts
+++ libs/elements/layout/images/src/lib/components/elewa-image-container/elewa-image-container.component.ts
@@ -9,22 +9,22 @@ import { ImageConfig } from '../../../../../../../models/schema/ui/images/image-
 export class ElewaImageContainerComponent {
   pill: ImageConfig = {
     title: 'Pill',
-    imageSrc: '',
+    imageSrc: 'https://media.gettyimages.com/id/1346973410/photo/young-woman-at-a-library.jpg?s=612x612&w=0&k=20&c=PW6KYwtS3oUx1mPcbfyqDcZotnyjFO-cwfbOsyG2ERk=',
     visualisation: 0,
   };
   stacked: ImageConfig = {
     title: 'Stacked',
-    imageSrc: '',
+    imageSrc: 'https://media.istockphoto.com/id/1178524829/photo/young-woman-making-choice-donut-or-apple.jpg?s=612x612&w=0&k=20&c=WeZ6-DQ8npDAhDNDZkcE48gtFFbqfTIIvpAfSfsh0Uo=',
     visualisation: 1,
   };
   window: ImageConfig = {
     title: 'Window',
-    imageSrc: '',
+    imageSrc: 'https://media.gettyimages.com/id/1392205193/photo/brazilian-man-working-and-using-technology-in-the-field.jpg?s=2048x2048&w=gi&k=20&c=3ACQa-OGvMCBa-bJ0TjeVb-Y820Ys2IEwimShEDFYm0=',
     visualisation: 2,
   };
   card: ImageConfig = {
     title: 'Card',
-    imageSrc: '',
+    imageSrc: 'https://media.gettyimages.com/id/1017628288/photo/happy-african-family-together-using-laptop.jpg?s=2048x2048&w=gi&k=20&c=fcjastVF8goNKDaqdzqzqMEW9Xf6xQKMCWH2wEAy2WA=',
     visualisation: 3,
   };
 }
