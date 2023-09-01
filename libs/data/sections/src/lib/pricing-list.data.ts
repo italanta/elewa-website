f989321d2deb0148f3a3cbf439101875680192c6
Oduol Peris <oduolpepe618@gmail.com>
2023-09-01 10:43:32 +0300

    🐛fix:solve a bug

--- libs/data/sections/src/lib/pricing-list.data.ts
+++ libs/data/sections/src/lib/pricing-list.data.ts
@@ -1,12 +1,36 @@
-import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
 import { PriceItem } from '@elewa-website/models/schema/ui/cards';
 
-// const tempSliderButton: SliderButtonData = {
-//   label: 'Select Plan',
-//   action: 'selectPlan',
-// };
 /** price list mock data*/
 export const __highlightedPricingList: PriceItem[] = [
+  {
+    title: 'Elewa Model',
+    amount: 500,
+    subTitle: 'Lorem Ipsum',
+    description:'Pretium quam vulputate dignissim suspendisse in est ante in. Ultrices gravida dictum fusce ut placepellentesque eu tincidunt tortor aliquam.rat orci nulla. Ipsum consequat nisl vel pretium lectus quam id leo.',
+    sliderButton: {
+      text: 'Unlock your potential',
+
+
+    },
+    amountColor: '#292A50',
+    textColor: '#000000',
+    backgroundColor: '#F4EDFD', 
+  },
+  
+  {
+    title: 'Elewa Model',
+    amount: 500,
+    subTitle: 'Lorem Ipsum',
+    description:'Pretium quam vulputate dignissim suspendisse in est ante in. Ultrices gravida dictum fusce ut placerat orci nulla. Ipsum consequat nisl vel pretium lectus quam id leo.',
+    sliderButton: {
+      text: 'Unlock your potential',
+
+
+    },
+    amountColor: '#292A50',
+    textColor: '#000000',
+    backgroundColor: '#F4EDFD', 
+  },
   {
     title: 'Elewa Model',
     amount: 500,
@@ -19,7 +43,7 @@ export const __highlightedPricingList: PriceItem[] = [
     },
     amountColor: '#292A50',
     textColor: '#000000',
-    backgroundColor: '', 
+    backgroundColor: '#F4EDFD', 
   },
   
 ];
