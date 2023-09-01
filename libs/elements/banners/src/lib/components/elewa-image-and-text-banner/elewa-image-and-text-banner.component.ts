49b8beb5c11275140cc3844d4897ff0ff72f9d2a
x-russian <philophilo037@gmail.com>
2023-09-01 12:14:32 +0300

    :sparkles:feat: import custom models + input property to be used in our component.

--- libs/elements/banners/src/lib/components/elewa-image-and-text-banner/elewa-image-and-text-banner.component.ts
+++ libs/elements/banners/src/lib/components/elewa-image-and-text-banner/elewa-image-and-text-banner.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+/* imports for customizing models from external packages */
+import { ImageConfig } from '@elewa-website/models/schema/ui/images';
+import { ContentText } from '@elewa-website/models/schema/ui/texts';
 
 @Component({
   selector: 'elewa-website-elewa-image-and-text-banner',
   templateUrl: './elewa-image-and-text-banner.component.html',
   styleUrls: ['./elewa-image-and-text-banner.component.scss'],
 })
-export class ElewaImageAndTextBannerComponent {}
+export class ElewaImageAndTextBannerComponent {
+
+  /* input property for this component */
+  @Input() ImageAndText!:{
+
+    /* 0bject for text content */
+    content: ContentText
+
+    /*object for image configuration */
+    image: ImageConfig
+  }
+}
