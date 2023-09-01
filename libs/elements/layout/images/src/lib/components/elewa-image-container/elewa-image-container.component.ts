c71efe1aa3c616738b8e7c4db26c266de83a46f1
KaranjaK <karanjakelvin77@gmail.com>
2023-09-01 10:02:11 +0300

    🍱 Imports the interface into the component

--- libs/elements/layout/images/src/lib/components/elewa-image-container/elewa-image-container.component.ts
+++ libs/elements/layout/images/src/lib/components/elewa-image-container/elewa-image-container.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { ImageConfig } from '../../../../../../../models/schema/ui/images/image-config.interface';
 
 @Component({
   selector: 'elewa-website-elewa-image-container',
   templateUrl: './elewa-image-container.component.html',
   styleUrls: ['./elewa-image-container.component.scss'],
 })
-export class ElewaImageContainerComponent {}
+export class ElewaImageContainerComponent {
+  pill: ImageConfig = {
+    title: 'Pill',
+    imageSrc: '',
+    visualisation: 0,
+  };
+  stacked: ImageConfig = {
+    title: 'Stacked',
+    imageSrc: '',
+    visualisation: 1,
+  };
+  window: ImageConfig = {
+    title: 'Window',
+    imageSrc: '',
+    visualisation: 2,
+  };
+  card: ImageConfig = {
+    title: 'Card',
+    imageSrc: '',
+    visualisation: 3,
+  };
+}
