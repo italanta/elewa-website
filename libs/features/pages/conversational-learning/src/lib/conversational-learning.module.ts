a3e030e8107d0df6d23383d5ae5e7eab3a8cbcdb
Steve Kariuki <stevekariuki5@gmail.com>
2023-09-01 17:12:06 +0300

    :recycle: order of imports changed

--- libs/features/pages/conversational-learning/src/lib/conversational-learning.module.ts
+++ libs/features/pages/conversational-learning/src/lib/conversational-learning.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ConversationalLearningPageComponent } from './main/conversational-learning-page/conversational-learning-page.component';
-import { ConvHeroSectionComponent } from './conv-hero-section/conv-hero-section.component';
-
-import { ConversationalLearningRoutingModule } from './conversational-learning-routing.module';
 
 import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 import { TextsModule } from '@elewa-website/elements/layout/texts';
 import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+
+import { ConversationalLearningRoutingModule } from './conversational-learning-routing.module';
+
+import { ConversationalLearningPageComponent } from './main/conversational-learning-page/conversational-learning-page.component';
+import { ConvHeroSectionComponent } from './conv-hero-section/conv-hero-section.component';
+
 
 
 @NgModule({
