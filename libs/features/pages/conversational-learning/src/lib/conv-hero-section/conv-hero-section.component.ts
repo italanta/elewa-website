557227030c94cc6aa388b39d71374539ff8448cc
Steve Kariuki <stevekariuki5@gmail.com>
2023-09-01 10:29:27 +0300

    :sparkles: imported header and hero to page

--- libs/features/pages/conversational-learning/src/lib/conv-hero-section/conv-hero-section.component.ts
+++ libs/features/pages/conversational-learning/src/lib/conv-hero-section/conv-hero-section.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+
 @Component({
   selector: 'elewa-website-conv-hero-section',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppHeaderModule, TextsModule],
   templateUrl: './conv-hero-section.component.html',
   styleUrls: ['./conv-hero-section.component.scss'],
 })
