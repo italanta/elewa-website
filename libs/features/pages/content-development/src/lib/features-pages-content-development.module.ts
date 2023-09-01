dd8b3b104dffd9d2e350705facc9f390557bb161
levy <lnjiru43@gmail.com>
2023-09-01 11:29:59 +0300

    imported TextsModule and ButtonsModule to be used in ContentDevelopmentModule page

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AppHeaderModule } from '@elewa-website/elements/layout/header';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 
@@ -9,7 +11,7 @@ import { ContentDevelopmentPageRoutingModule } from './content-development.routi
 import { ContentDevHeroSectionComponent } from './components/content-dev-hero-section/content-dev-hero-section.component';
 
 @NgModule({
-  imports: [CommonModule, ContentDevelopmentPageRoutingModule, AppHeaderModule],
+  imports: [CommonModule, ContentDevelopmentPageRoutingModule, AppHeaderModule, TextsModule, ButtonsModule],
   declarations: [
     ContentDevelopmentPageComponent,
     ContentDevHeroSectionComponent,
