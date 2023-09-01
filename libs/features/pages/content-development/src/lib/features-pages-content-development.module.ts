443496598b42871c3c537287b299763e673aa429
Preston <prestonosoro56@gmail.com>
2023-09-01 15:57:33 +0300

    feat: import Texts,Header && button modules

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -4,9 +4,12 @@ import { CommonModule } from '@angular/common';
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 @NgModule({
-  imports: [CommonModule, ContentDevelopmentPageRoutingModule],
+  imports: [CommonModule, ContentDevelopmentPageRoutingModule,TextsModule,AppHeaderModule,ButtonsModule], /**import modules*/
   declarations: [ContentDevelopmentPageComponent],
   exports: [ContentDevelopmentPageComponent]
 })
