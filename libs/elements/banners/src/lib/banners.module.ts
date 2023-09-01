3561bc3bc4e372189d000265de3e32bb90b4fad6
x-russian <philophilo037@gmail.com>
2023-09-01 11:50:26 +0300

    :sparkles:feat: Create elewa-image-and-text-banner component + export it.

--- libs/elements/banners/src/lib/banners.module.ts
+++ libs/elements/banners/src/lib/banners.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaImageAndTextBannerComponent } from './components/elewa-image-and-text-banner/elewa-image-and-text-banner.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaImageAndTextBannerComponent],
+  exports: [ElewaImageAndTextBannerComponent]
 })
 export class BannersModule {}
---
+++ libs/elements/banners/src/lib/components/elewa-image-and-text-banner/elewa-image-and-text-banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-image-and-text-banner',
+  templateUrl: './elewa-image-and-text-banner.component.html',
+  styleUrls: ['./elewa-image-and-text-banner.component.scss'],
+})
+export class ElewaImageAndTextBannerComponent {}
