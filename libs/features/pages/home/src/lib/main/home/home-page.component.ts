1457d83638596efd9b4a68a6600c2be321edb4bb
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-02 11:45:18 +0300

    :sparkles: feat: add translations using the translation api

--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+
+import { SubSink } from 'subsink';
+import { filter } from 'rxjs';
+import { TranslocoService } from '@ngneat/transloco';
 
 import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
 
@@ -7,9 +11,11 @@ import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
+export class HomePageComponent implements OnDestroy {
+  private _sBs = new SubSink();
+
   buttonData: SliderButtonData = {
-    text: 'unlock your potential',
+    text: '',
 
     /** base colors on button */
     color: '#ffffff',
@@ -25,4 +31,19 @@ export class HomePageComponent {
     hoverBorderColor: '',
     hoverIconBackgroundColor: '',
   };
+
+  constructor(private _translocoServ: TranslocoService) {
+    this._sBs.sink = this._translocoServ.load('en').subscribe();
+    this._sBs.sink = this._translocoServ.events$
+      .pipe(filter((event) => event.type === 'translationLoadSuccess'))
+      .subscribe(() => {
+        this.buttonData.text = this._translocoServ.translate(
+          'ELEWA.PAGES.HOME.HERO-SECTION.BUTTON-TEXT'
+        );
+      });
+  }
+
+  ngOnDestroy() {
+    this._sBs.unsubscribe();
+  }
 }
