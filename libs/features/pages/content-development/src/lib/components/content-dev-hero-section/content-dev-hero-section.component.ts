5c65854786f90bea5159ffe7382fa28ad5d6aed2
levy <lnjiru43@gmail.com>
2023-09-02 12:02:50 +0300

    added sample data

--- libs/features/pages/content-development/src/lib/components/content-dev-hero-section/content-dev-hero-section.component.ts
+++ libs/features/pages/content-development/src/lib/components/content-dev-hero-section/content-dev-hero-section.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
 
+import { HeroText } from '@elewa-website/models/schema/ui/texts';
+import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
+
 @Component({
   selector: 'elewa-website-content-dev-hero-section',
   templateUrl: './content-dev-hero-section.component.html',
   styleUrls: ['./content-dev-hero-section.component.scss'],
 })
-export class ContentDevHeroSectionComponent {}
+export class ContentDevHeroSectionComponent {
+  heroTexts: HeroText = {
+    title:"Content development",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor dolorem ipsum quia dolor sit Eos qui ratione voluptatem sequi nesciunt."
+  }
+
+  buttonData: SliderButtonData = {
+    text: "unlock your potential"
+  }
+
+
+}
