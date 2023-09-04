6640e2678966f9777bfbf1ecfb1ae9a1d9d2fdc2
levy <lnjiru43@gmail.com>
2023-09-04 06:51:22 +0300

    :sparkles: generated booklets hero section component

--- libs/features/pages/booklets-page/src/lib/booklets-page.module.ts
+++ libs/features/pages/booklets-page/src/lib/booklets-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookletsPageComponent } from './main/booklets-page/booklets-page.component';
 import { BookletsRoutingModule } from './booklets.routing';
+import { BookletsHeroSectionComponent } from './components/booklets-hero-section/booklets-hero-section.component';
 
 @NgModule({
   imports: [CommonModule, BookletsRoutingModule],
-  declarations: [BookletsPageComponent],
+  declarations: [BookletsPageComponent, BookletsHeroSectionComponent],
 })
 export class BookletsPageModule {}
---
+++ libs/features/pages/booklets-page/src/lib/components/booklets-hero-section/booklets-hero-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-booklets-hero-section',
+  templateUrl: './booklets-hero-section.component.html',
+  styleUrls: ['./booklets-hero-section.component.scss'],
+})
+export class BookletsHeroSectionComponent {}
