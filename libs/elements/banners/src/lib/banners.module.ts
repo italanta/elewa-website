a9e6aab4e169f1ca7525c31b8c5f098adae14719
x-russian <philophilo037@gmail.com>
2023-09-04 14:16:35 +0300

    :sparkles:feat: Import images-module to re-use one of its compoent.

--- libs/elements/banners/src/lib/banners.module.ts
+++ libs/elements/banners/src/lib/banners.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaImageAndTextBannerComponent } from './elewa-image-and-text-banner/elewa-image-and-text-banner.component';
+import { ImagesModule } from '@elewa-website/elements/layout/images';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ImagesModule],
   declarations: [ElewaImageAndTextBannerComponent],
   exports: [ElewaImageAndTextBannerComponent]
 })
