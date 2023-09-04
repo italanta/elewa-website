fb0b2b6d2a7b332c9be7dcb168c6110fd93242de
levy <lnjiru43@gmail.com>
2023-09-04 18:59:50 +0300

    component logic

--- libs/elements/banners/src/lib/components/elewa-image-and-details-banner/elewa-image-and-details-banner.component.ts
+++ libs/elements/banners/src/lib/components/elewa-image-and-details-banner/elewa-image-and-details-banner.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { ImageAndDetails } from '@elewa-website/models/schema/ui/banners';
 
 @Component({
   selector: 'elewa-website-elewa-image-and-details-banner',
   templateUrl: './elewa-image-and-details-banner.component.html',
   styleUrls: ['./elewa-image-and-details-banner.component.scss'],
 })
-export class ElewaImageAndDetailsBannerComponent {}
+export class ElewaImageAndDetailsBannerComponent {
+  @Input() imageAnddetails!: ImageAndDetails;
+
+    /**
+   * Get the CSS style for the image position based on the provided string.
+   *
+   * @param {string} imagePosition - The position of the image ('left' or 'right').
+   * @returns {{ [key: string]: string }} - An object representing CSS styles.
+   */
+
+  getPositionStyle(imagePosition: string): { [key: string]: string } {
+    switch (imagePosition) {
+      case 'left':
+        return { 'order': '0' };
+      case 'right':
+        return { 'order': '1' };
+      default:
+        return { 'order': '0' }; // default value
+    }
+  }
+  
+}
