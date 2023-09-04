1cf52e4286ddc2d59b8d45974ea65916d593b158
levy <lnjiru43@gmail.com>
2023-09-04 18:54:48 +0300

    imported imagemodule and cards module

--- libs/elements/banners/src/lib/banners.module.ts
+++ libs/elements/banners/src/lib/banners.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ImagesModule } from '@elewa-website/elements/layout/images';
+import { CardsModule } from '@elewa-website/elements/cards';
+
 import { ElewaImageAndDetailsBannerComponent } from './components/elewa-image-and-details-banner/elewa-image-and-details-banner.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ImagesModule, CardsModule],
   declarations: [ElewaImageAndDetailsBannerComponent],
+  exports: [ElewaImageAndDetailsBannerComponent]
 })
 export class BannersModule {}
