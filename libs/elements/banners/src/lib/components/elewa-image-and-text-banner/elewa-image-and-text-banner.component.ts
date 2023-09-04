367cf55c989c878b4108d4a76c572b393f4b384e
levy <lnjiru43@gmail.com>
2023-09-04 14:57:45 +0300

    component that displays items depending on flex order number

--- libs/elements/banners/src/lib/components/elewa-image-and-text-banner/elewa-image-and-text-banner.component.ts
+++ libs/elements/banners/src/lib/components/elewa-image-and-text-banner/elewa-image-and-text-banner.component.ts
@@ -1,8 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { ImageAndText } from '@elewa-website/models/schema/ui/banners';
 
 @Component({
   selector: 'elewa-website-elewa-image-and-text-banner',
   templateUrl: './elewa-image-and-text-banner.component.html',
   styleUrls: ['./elewa-image-and-text-banner.component.scss'],
 })
-export class ElewaImageAndTextBannerComponent {}
+export class ElewaImageAndTextBannerComponent {
+  @Input() imageAndText!: ImageAndText;
+
+  /**
+   * Calculates the CSS style for positioning the image within the banner.
+   *
+   * @param {string} imagePosition - The desired position of the image ('left', 'right', or other).
+   * @returns {Object} An object representing the CSS style.
+   * @memberof ElewaImageAndTextBannerComponent
+   */
+  getPositionStyle(imagePosition: string): { [key: string]: string } {
+    switch (imagePosition) {
+      case 'left':
+        return { 'order': '1' };
+      case 'right':
+        return { 'order': '2' };
+      default:
+        return { 'order': '0' }; // default value
+    }
+  }
+}
