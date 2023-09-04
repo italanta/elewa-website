fa1165a8a5c4d983993bd576d4858e74912ada87
Preston <prestonosoro56@gmail.com>
2023-09-04 21:15:46 +0300

    feat: define hereTextData and sliderButton

--- libs/features/pages/content-development/src/lib/content-dev-hero-section/content-dev-hero-section.component.ts
+++ libs/features/pages/content-development/src/lib/content-dev-hero-section/content-dev-hero-section.component.ts
@@ -1,21 +1,17 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { AppHeaderModule } from '@elewa-website/elements/layout/header';
-import { TextsModule } from '@elewa-website/elements/layout/texts';
-import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 @Component({
   selector: 'elewa-website-content-dev-hero-section',
-  standalone: true,
-  imports: [
-    CommonModule,
-    AppHeaderModule,
-    TextsModule,
-    ButtonsModule,
-  ] /**import modules to be reused*/,
   templateUrl: './content-dev-hero-section.component.html',
   styleUrls: ['./content-dev-hero-section.component.scss'],
 })
 export class ContentDevHeroSectionComponent {
-  
+  heroTextData = {
+    title: 'Content Development',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor dolorem ipsum quia dolor sit Eos qui ratione voluptatem sequi nesciunt.',
+  };
+  sliderButtonData = {
+    text: 'Unlock your potential',
+  };
 }
