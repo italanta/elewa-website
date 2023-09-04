36564ec71394f0f0b2e851274380f0e44397f58b
levy <lnjiru43@gmail.com>
2023-09-04 10:24:27 +0300

    :sparkles: generated image and text banner component

--- libs/elements/banners/src/lib/elements-banners.module.ts
+++ libs/elements/banners/src/lib/elements-banners.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaImageAndTextBannerComponent } from './components/elewa-image-and-text-banner/elewa-image-and-text-banner.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaImageAndTextBannerComponent],
 })
 export class ElementsBannersModule {}
