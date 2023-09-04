5a4b9b0f0419cf251dc9f1bc7eb14dcfab0daeaa
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-04 18:15:55 +0300

    :lipstick: styled the contact form

--- libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
+++ libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { ContactPageComponent } from './main/contact-page/contact-page.component';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 import { ContactPageFormSectionComponent } from './components/contact-page-form-section/contact-page-form-section.component';
 
 import { ContactRoutingModule } from './contact-page.routing';
 
 @NgModule({
-  imports: [CommonModule, ContactRoutingModule, ReactiveFormsModule],
+  imports: [CommonModule, ContactRoutingModule, ReactiveFormsModule, AppHeaderModule],
   declarations: [ContactPageComponent, ContactPageFormSectionComponent],
   exports: [ContactPageComponent, ContactPageFormSectionComponent],
 })
