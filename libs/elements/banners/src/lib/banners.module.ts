b6e5a41e52e08ad67b06bff9c2958d4deaa4b17e
x-russian <philophilo037@gmail.com>
2023-09-04 18:34:24 +0300

    :fire:fix: Import text module to re-use soe of its component.

--- libs/elements/banners/src/lib/banners.module.ts
+++ libs/elements/banners/src/lib/banners.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ElewaImageAndTextBannerComponent } from './elewa-image-and-text-banner/elewa-image-and-text-banner.component';
 import { ImagesModule } from '@elewa-website/elements/layout/images';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
 
 @NgModule({
-  imports: [CommonModule, ImagesModule],
+  imports: [CommonModule, ImagesModule, TextsModule],
   declarations: [ElewaImageAndTextBannerComponent],
   exports: [ElewaImageAndTextBannerComponent]
 })
