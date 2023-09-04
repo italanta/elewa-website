d10cee7177bd04dde6795447983d340b220417de
levy <lnjiru43@gmail.com>
2023-09-04 06:54:15 +0300

    imported buttons module and text module to re use slider button and hero text respectively

--- libs/features/pages/booklets-page/src/lib/booklets-page.module.ts
+++ libs/features/pages/booklets-page/src/lib/booklets-page.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+
 import { BookletsPageComponent } from './main/booklets-page/booklets-page.component';
 import { BookletsRoutingModule } from './booklets.routing';
 import { BookletsHeroSectionComponent } from './components/booklets-hero-section/booklets-hero-section.component';
 
 @NgModule({
-  imports: [CommonModule, BookletsRoutingModule],
+  imports: [CommonModule, BookletsRoutingModule, TextsModule, ButtonsModule],
   declarations: [BookletsPageComponent, BookletsHeroSectionComponent],
 })
 export class BookletsPageModule {}
