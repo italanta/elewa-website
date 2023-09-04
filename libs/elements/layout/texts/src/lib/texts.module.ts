1bf8bd50549af69d287c974cedb027ed4051ff32
Oduol Peris <oduolpepe618@gmail.com>
2023-09-04 15:43:36 +0300

    ✨feat:Generate about-hero-section component

--- libs/elements/layout/texts/src/lib/texts.module.ts
+++ libs/elements/layout/texts/src/lib/texts.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ElewaTextSectionComponent } from './components/elewa-text-section/elewa-text-section.component';
 import { ElewaTextContentItemComponent } from './components/elewa-text-content-item/elewa-text-content-item.component';
+import { AboutHeroSectionComponent } from './components/about-hero-section/about-hero-section.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaTextContentItemComponent, ElewaTextSectionComponent],
-  exports: [ElewaTextSectionComponent]
+  declarations: [
+    ElewaTextContentItemComponent,
+    ElewaTextSectionComponent,
+    AboutHeroSectionComponent,
+  ],
+  exports: [ElewaTextSectionComponent],
 })
 export class TextsModule {}
