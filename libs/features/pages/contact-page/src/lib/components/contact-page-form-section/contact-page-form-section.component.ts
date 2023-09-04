605472b9961db908e8e8f071ebe5a9d8206b6d24
paul <paulnyoiken@gmail.com>
2023-09-04 18:41:29 +0300

    ✨feat: added a new component in ContactPageModule named contact-page-form-section

---
+++ libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-contact-page-form-section',
+  templateUrl: './contact-page-form-section.component.html',
+  styleUrls: ['./contact-page-form-section.component.scss'],
+})
+export class ContactPageFormSectionComponent {}
--- libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
+++ libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ContactPageComponent } from './main/contact-page/contact-page.component';
 
 import { ContactRoutingModule } from './contact-page.routing';
+import { ContactPageFormSectionComponent } from './components/contact-page-form-section/contact-page-form-section.component';
 
 @NgModule({
   imports: [CommonModule, ContactRoutingModule],
-  declarations: [ContactPageComponent],
-  exports: [ContactPageComponent]
+  declarations: [ContactPageComponent, ContactPageFormSectionComponent],
+  exports: [ContactPageComponent],
 })
 export class ContactPageModule {}
