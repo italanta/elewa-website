ae27d30298d419cdba4edb2035b83835d08b9ef2
levy <lnjiru43@gmail.com>
2023-09-04 16:27:56 +0300

    :sparkles: generated elewa-image-and-details-banner component

--- libs/elements/banners/src/lib/banners.module.ts
+++ libs/elements/banners/src/lib/banners.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ElewaImageAndDetailsBannerComponent } from './components/elewa-image-and-details-banner/elewa-image-and-details-banner.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [ElewaImageAndDetailsBannerComponent],
 })
 export class BannersModule {}
---
+++ libs/elements/banners/src/lib/components/elewa-image-and-details-banner/elewa-image-and-details-banner.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-image-and-details-banner',
+  templateUrl: './elewa-image-and-details-banner.component.html',
+  styleUrls: ['./elewa-image-and-details-banner.component.scss'],
+})
+export class ElewaImageAndDetailsBannerComponent {}
