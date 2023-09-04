56f07b00771f64fd488d3db6720dbedd04ea0bd6
paul <paulnyoiken@gmail.com>
2023-09-04 21:22:11 +0300

    ✨feat: created and styled contact form

--- libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
+++ libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
@@ -1,8 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
 
 @Component({
   selector: 'elewa-website-contact-page-form-section',
   templateUrl: './contact-page-form-section.component.html',
   styleUrls: ['./contact-page-form-section.component.scss'],
 })
-export class ContactPageFormSectionComponent {}
+export class ContactPageFormSectionComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      company: [''],
+      email: ['', [Validators.required, Validators.email]],
+      reason: ['general', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+  onSubmit() {
+    /**Form submission functionality */
+  }
+
+  sendButtonData: SliderButtonData = {
+    text: "Send message"
+  }
+}
--- libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
+++ libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ContactPageComponent } from './main/contact-page/contact-page.component';
 
 import { ContactRoutingModule } from './contact-page.routing';
 import { ContactPageFormSectionComponent } from './components/contact-page-form-section/contact-page-form-section.component';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 @NgModule({
-  imports: [CommonModule, ContactRoutingModule],
+  imports: [CommonModule, ContactRoutingModule, ButtonsModule, ReactiveFormsModule],
   declarations: [ContactPageComponent, ContactPageFormSectionComponent],
-  exports: [ContactPageComponent],
+  exports: [ContactPageComponent, ContactPageFormSectionComponent],
 })
 export class ContactPageModule {}
