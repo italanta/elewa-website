22412907e1af4557cf5978d08c7bad1da764903f
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-04 06:55:50 +0300

    :lipstick: added the elewa website app header to the booklets page

--- libs/features/pages/booklets-page/src/lib/booklets-page.module.ts
+++ libs/features/pages/booklets-page/src/lib/booklets-page.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { BookletsPageComponent } from './main/booklets-page/booklets-page.component';
+import { BookletsHeroSectionComponent } from './components/booklets-hero-section/booklets-hero-section.component';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 import { BookletsRoutingModule } from './booklets.routing';
 
 @NgModule({
-  imports: [CommonModule, BookletsRoutingModule],
-  declarations: [BookletsPageComponent],
+  imports: [CommonModule, BookletsRoutingModule, AppHeaderModule],
+  declarations: [BookletsPageComponent, BookletsHeroSectionComponent],
 })
 export class BookletsPageModule {}
