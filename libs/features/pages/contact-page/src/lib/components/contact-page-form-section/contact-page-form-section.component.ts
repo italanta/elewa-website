68017eed561e3c88652360278d1e0af2a7130db5
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-04 15:28:28 +0300

    :lipstick: added contact-page form data

--- libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
+++ libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
@@ -1,8 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'elewa-website-contact-page-form-section',
   templateUrl: './contact-page-form-section.component.html',
   styleUrls: ['./contact-page-form-section.component.scss'],
 })
-export class ContactPageFormSectionComponent {}
+export class ContactPageFormSectionComponent {
+  contactForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', Validators.required],
+      company: [''],
+      email: ['', [Validators.required, Validators.email]],
+      reason: ['general', Validators.required],
+      message: ['', Validators.required],
+    });
+  }
+
+  onSubmit() {
+    // Add logic to handle form submission here
+    // You can access form values using this.contactForm.value
+    // Don't forget to unsubscribe from any observables if needed.
+  }
+}
+
--- libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
+++ libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ContactPageComponent } from './main/contact-page/contact-page.component';
+import { ContactPageFormSectionComponent } from './components/contact-page-form-section/contact-page-form-section.component';
 
 import { ContactRoutingModule } from './contact-page.routing';
 
 @NgModule({
-  imports: [CommonModule, ContactRoutingModule],
-  declarations: [ContactPageComponent],
-  exports: [ContactPageComponent]
+  imports: [CommonModule, ContactRoutingModule, ReactiveFormsModule],
+  declarations: [ContactPageComponent, ContactPageFormSectionComponent],
+  exports: [ContactPageComponent, ContactPageFormSectionComponent],
 })
 export class ContactPageModule {}
