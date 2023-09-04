40c6dbf16f6deb6cfe68a72d3b65ff7a84823d7a
Preston <prestonosoro56@gmail.com>
2023-09-04 21:14:51 +0300

    feat: import ContentDevHeroSectionComponent

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -7,10 +7,20 @@ import { ContentDevelopmentPageRoutingModule } from './content-development.routi
 import { TextsModule } from '@elewa-website/elements/layout/texts';
 import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+import { ContentDevHeroSectionComponent } from './content-dev-hero-section/content-dev-hero-section.component';
 
 @NgModule({
-  imports: [CommonModule, ContentDevelopmentPageRoutingModule,TextsModule,AppHeaderModule,ButtonsModule], /**import modules*/
-  declarations: [ContentDevelopmentPageComponent],
-  exports: [ContentDevelopmentPageComponent]
+  imports: [
+    CommonModule,
+    ContentDevelopmentPageRoutingModule,
+    TextsModule,
+    AppHeaderModule,
+    ButtonsModule,
+  ] /**import modules*/,
+  declarations: [
+    ContentDevelopmentPageComponent,
+    ContentDevHeroSectionComponent,
+  ],
+  exports: [ContentDevelopmentPageComponent],
 })
 export class ContentDevelopmentModule {}
