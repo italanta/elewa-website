0c410201493b6bdfb4a3c662cd938754f1222ebd
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-04 14:38:48 +0300

    :sparkles: generated the contact page form section component

---
+++ libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ContactPageFormSectionComponent } from './contact-page-form-section.component';
+
+describe('ContactPageFormSectionComponent', () => {
+  let component: ContactPageFormSectionComponent;
+  let fixture: ComponentFixture<ContactPageFormSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ContactPageFormSectionComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ContactPageFormSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
