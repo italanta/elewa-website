3cf60f912b0890ac6b1b45a93150f2a8ee69dc84
Oduol Peris <oduolpepe618@gmail.com>
2023-09-04 20:03:07 +0300

    ✨feat:generate consultancy-hero-section component

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ConsultancyRoutingModule } from './consultancy.routing';
 
 import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
+import { ConsultancyHeroSectionComponent } from './components/consultancy-hero-section/consultancy-hero-section.component';
 
 @NgModule({
   imports: [CommonModule, ConsultancyRoutingModule],
-  declarations: [ConsultancyPageComponent],
+  declarations: [ConsultancyPageComponent, ConsultancyHeroSectionComponent],
 })
 export class ConsultancyPageModule {}
