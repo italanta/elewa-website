af766661bbfe864a35840b7837f5e391b67d7631
levy <lnjiru43@gmail.com>
2023-09-04 10:28:35 +0300

    imported ImagesModule and TextModule

--- libs/elements/banners/src/lib/elements-banners.module.ts
+++ libs/elements/banners/src/lib/elements-banners.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { ImagesModule } from '@elewa-website/elements/layout/images';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+
 import { ElewaImageAndTextBannerComponent } from './components/elewa-image-and-text-banner/elewa-image-and-text-banner.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ImagesModule, TextsModule],
   declarations: [ElewaImageAndTextBannerComponent],
 })
 export class ElementsBannersModule {}
