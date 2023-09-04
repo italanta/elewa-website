6b527b2cf8bd7443998c7c8f96f5480d7a163075
levy <lnjiru43@gmail.com>
2023-09-04 07:31:45 +0300

    re used slider button and added sample data

--- libs/features/pages/booklets-page/src/lib/components/booklets-hero-section/booklets-hero-section.component.ts
+++ libs/features/pages/booklets-page/src/lib/components/booklets-hero-section/booklets-hero-section.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { HeroText } from '@elewa-website/models/schema/ui/texts';
+import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
 
 @Component({
   selector: 'elewa-website-booklets-hero-section',
@@ -12,4 +13,10 @@ export class BookletsHeroSectionComponent {
     title:"Booklets",
     description:"Your organisation has the potential to turn every task into an opportunity to learn. From growing crops, to nursing someone to good health, to developing the next-generation technology, let’s work together to make the magic happen."
   }
+
+  buttonData: SliderButtonData = {
+    text: "Unlock your Potential"
+  }
+
+
 }
