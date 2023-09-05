40e1194d016e31f13f48c210a676d090825495eb
x-russian <philophilo037@gmail.com>
2023-09-05 14:38:58 +0300

    :sparkles: Import booklets-page-text-image ...component.

--- libs/features/pages/booklets-page/src/lib/booklets-page.module.ts
+++ libs/features/pages/booklets-page/src/lib/booklets-page.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookletsPageComponent } from './main/booklets-page/booklets-page.component';
 import { BookletsRoutingModule } from './booklets.routing';
+import { BookletsPageTextImageBannerSectionComponent } from './main/booklets-page-text-image-banner-section/booklets-page-text-image-banner-section.component';
 
 @NgModule({
-  imports: [CommonModule, BookletsRoutingModule],
+  imports: [CommonModule, BookletsRoutingModule, BookletsPageTextImageBannerSectionComponent],
   declarations: [BookletsPageComponent],
 })
 export class BookletsPageModule {}
