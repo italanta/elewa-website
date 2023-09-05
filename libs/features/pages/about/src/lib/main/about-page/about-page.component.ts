c67424fe6e253e0c7d2642c59927a921ff1e342f
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-05 12:56:38 +0300

    :recycle: refactor: move list to about page

--- libs/features/pages/about/src/lib/main/about-page/about-page.component.ts
+++ libs/features/pages/about/src/lib/main/about-page/about-page.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+import { __highlightedSDGList } from '@elewa-website/data/sections';
+
 @Component({
   selector: 'elewa-website-about-page',
   templateUrl: './about-page.component.html',
   styleUrls: ['./about-page.component.scss'],
 })
-export class AboutPageComponent {}
+export class AboutPageComponent {
+  highlightedSDGList = __highlightedSDGList;
+}
--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-import { __highlightedSDGList } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-home',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {
-  highlightedSDGList = __highlightedSDGList
-}
+export class HomePageComponent {}
