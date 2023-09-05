7debcc76f478cc63006ba1f60f6188e83178233b
Preston <prestonosoro56@gmail.com>
2023-09-05 12:38:28 +0300

    feat: structure template| style | reuse textData && sliderButton

---
+++ libs/features/pages/consultancy-page/src/lib/consultancy-hero-section/consultancy-hero-section.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-consultancy-hero-section',
+  templateUrl: './consultancy-hero-section.component.html',
+  styleUrls: ['./consultancy-hero-section.component.scss'],
+})
+export class ConsultancyHeroSectionComponent {
+  heroTextData = {
+    title: 'Learning design',
+    description: 'Your organisation has the potential to turn every task into an opportunity to learn. From growing crops, to nursing someone to good health, to developing the next-generation technology. Let’s work together to make the magic happen.',
+  };
+  sliderButtonData = {
+    text: 'Unlock your potential',
+  };
+}
