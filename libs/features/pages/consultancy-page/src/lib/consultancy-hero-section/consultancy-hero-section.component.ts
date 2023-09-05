c0e7dc31c022fc5fb4c409922eb0c02c66a64d45
Preston <prestonosoro56@gmail.com>
2023-09-05 17:30:28 +0300

    feat: generate consultancy hero component

---
+++ libs/features/pages/consultancy-page/src/lib/consultancy-hero-section/consultancy-hero-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-consultancy-hero-section',
+  templateUrl: './consultancy-hero-section.component.html',
+  styleUrls: ['./consultancy-hero-section.component.scss'],
+})
+export class ConsultancyHeroSectionComponent {}
