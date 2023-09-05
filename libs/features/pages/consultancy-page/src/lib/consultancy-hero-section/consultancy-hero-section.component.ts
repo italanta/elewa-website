157d4c9ec687617a9ce31188ce3cd286e19bb5c8
Preston <prestonosoro56@gmail.com>
2023-09-05 17:39:03 +0300

    feat: textData | slidetButtonData

--- libs/features/pages/consultancy-page/src/lib/consultancy-hero-section/consultancy-hero-section.component.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-hero-section/consultancy-hero-section.component.ts
@@ -1,3 +1,4 @@
+
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,4 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './consultancy-hero-section.component.html',
   styleUrls: ['./consultancy-hero-section.component.scss'],
 })
-export class ConsultancyHeroSectionComponent {}
+export class ConsultancyHeroSectionComponent {
+  heroTextData = {
+    title: 'Learning design',
+    description: 'Your organisation has the potential to turn every task into an opportunity to learn. From growing crops, to nursing someone to good health, to developing the next-generation technology. Let’s work together to make the magic happen.',
+  };
+  sliderButtonData = {
+    text: 'Unlock your potential',
+  };
+}
