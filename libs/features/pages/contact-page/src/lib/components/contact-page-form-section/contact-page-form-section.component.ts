13b1a8aeba781d1b9f2ab7a480f0ca9c7efd34e3
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-09-05 09:39:48 +0300

    :art: :sparkles:created from&stylings

--- libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
+++ libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
@@ -1,8 +1,25 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'elewa-website-contact-page-form-section',
   templateUrl: './contact-page-form-section.component.html',
   styleUrls: ['./contact-page-form-section.component.scss'],
 })
-export class ContactPageFormSectionComponent {}
+export class ContactPageFormSectionComponent {
+  buttonData = {
+    text: 'Send message',
+
+    /** base colors on button */
+    color: '#ffffff',
+    bgColor: '#292A50',
+    iconColor: '#000000',
+    borderColor: '#000000',
+    iconBackgroundColor: '#ffffff',
+
+    /** hover effect styles on the button  */
+    hoverColor: '#000000',
+    hoverBgColor: '#ffffff',
+    hoverIconColor: '#000000',
+    hoverBorderColor: '#00000',
+    hoverIconBackgroundColor: '#000000',
+  };
+}
