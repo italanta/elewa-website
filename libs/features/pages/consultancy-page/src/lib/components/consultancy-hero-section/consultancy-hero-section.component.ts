2e1906f0a287b685e063f2722049f74d20e1f1f1
Yahya <yahyahussein670@gmail.com>
2023-09-05 11:29:15 +0300

    ✨ feat: Add a sample data

--- libs/features/pages/consultancy-page/src/lib/components/consultancy-hero-section/consultancy-hero-section.component.ts
+++ libs/features/pages/consultancy-page/src/lib/components/consultancy-hero-section/consultancy-hero-section.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HeroText } from '@elewa-website/models/schema/ui/texts';
+import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
+
 
 @Component({
   selector: 'elewa-website-consultancy-hero-section',
@@ -6,4 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./consultancy-hero-section.component.scss'],
 })
 export class ConsultancyHeroSectionComponent {
+  heroTexts: HeroText = {
+    title:"Learning Design",
+    description:"Your organisation has the potential to turn every task into an opportunity to learn. From growing crops, to nursing someone to good health, to developing the next-generation technology. Let’s work together to make the magic happen."
+  }
+
+  buttonData: SliderButtonData = {
+    text: "Unlock your potential"
+  }
 }
