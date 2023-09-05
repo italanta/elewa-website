cedb559a14db2584954f7253ddc710f8e447b930
Preston <prestonosoro56@gmail.com>
2023-09-05 12:34:16 +0300

    feat: import modules

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -4,9 +4,19 @@ import { CommonModule } from '@angular/common';
 import { ConsultancyRoutingModule } from './consultancy.routing';
 
 import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
+import { ConsultancyHeroSectionComponent } from './consultancy-hero-section/consultancy-hero-section.component';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 
 @NgModule({
-  imports: [CommonModule, ConsultancyRoutingModule],
-  declarations: [ConsultancyPageComponent],
+  imports: [
+    CommonModule,
+    ConsultancyRoutingModule,
+    TextsModule,
+    AppHeaderModule,
+    ButtonsModule,
+  ] /**  import modules */,
+  declarations: [ConsultancyPageComponent, ConsultancyHeroSectionComponent],
 })
 export class ConsultancyPageModule {}
