9ccd36a660a872e10dadbf18a3f059e4d975b875
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-05 13:24:19 +0300

    :recycle: refactor: pass buttonData from the about page main component

--- libs/elements/cards/src/lib/components/elewa-sdg-list/elewa-sdg-list.component.ts
+++ libs/elements/cards/src/lib/components/elewa-sdg-list/elewa-sdg-list.component.ts
@@ -1,20 +1,13 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { InfoCard } from '@elewa-website/models/schema/ui/cards';
-import { __highlightedSDGList } from '@elewa-website/data/sections';
 import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
-import { __mockSliderButtonData } from '@elewa-website/data/ui/content-text';
 
 @Component({
   selector: 'elewa-website-elewa-sdg-list',
   templateUrl: './elewa-sdg-list.component.html',
   styleUrls: ['./elewa-sdg-list.component.scss'],
 })
-export class ElewaSdgListComponent implements OnInit {
-  @Input() sdgList!: InfoCard[];
-  @Input() buttonData!: SliderButtonData;
-
-  ngOnInit(): void {
-    this.sdgList = __highlightedSDGList;
-    this.buttonData = __mockSliderButtonData;
-  }
+export class ElewaSdgListComponent {
+  @Input({ required: true }) sdgList!: InfoCard[];
+  @Input({ required: true }) buttonData!: SliderButtonData;
 }
