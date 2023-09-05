fa68d82a940aae7c7c04f6c6f582549a9ed0a0e5
Preston <prestonosoro56@gmail.com>
2023-09-05 16:55:38 +0300

    feat: import modules

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -4,10 +4,23 @@ import { CommonModule } from '@angular/common';
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
+import { ContentDevHeroSectionComponent } from './content-dev-hero-section/content-dev-hero-section.component';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 @NgModule({
-  imports: [CommonModule, ContentDevelopmentPageRoutingModule],
-  declarations: [ContentDevelopmentPageComponent],
-  exports: [ContentDevelopmentPageComponent]
+  imports: [
+    CommonModule,
+    ContentDevelopmentPageRoutingModule,
+    TextsModule,
+    AppHeaderModule,
+    ButtonsModule,
+  ] /**import modules*/,  
+  declarations: [
+    ContentDevelopmentPageComponent,
+    ContentDevHeroSectionComponent,
+  ],
+  exports: [ContentDevelopmentPageComponent],
 })
 export class ContentDevelopmentModule {}
