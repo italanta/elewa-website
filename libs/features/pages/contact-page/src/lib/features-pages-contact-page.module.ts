313eef722a28a5e07a9df01b7e4f4efb66bd6363
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-09-05 10:06:03 +0300

    :sparkles: imported reactiveformModule

--- libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
+++ libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
@@ -7,8 +7,9 @@ import { ContactRoutingModule } from './contact-page.routing';
 import { ContactPageFormSectionComponent } from './components/contact-page-form-section/contact-page-form-section.component';
  
 import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
-  imports: [CommonModule, ContactRoutingModule,ButtonsModule],
+  imports: [CommonModule, ContactRoutingModule,ButtonsModule,ReactiveFormsModule],
   declarations: [ContactPageComponent, ContactPageFormSectionComponent],
   exports: [ContactPageComponent],
 })
