ae084430085fe9f5959111c361df78d0a89c3dad
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-05 13:53:01 +0300

    :sparkles: generated the sdg section component

---
+++ libs/features/pages/about/src/lib/components/sdg-section/sdg-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'elewa-website-sdg-section',
+  templateUrl: './sdg-section.component.html',
+  styleUrls: ['./sdg-section.component.scss'],
+})
+export class SdgSectionComponent {}
