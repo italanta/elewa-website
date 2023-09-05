1e8f3834257ea26e1ee20899302ae563e919a033
x-russian <philophilo037@gmail.com>
2023-09-05 21:54:07 +0300

    :sparkles:feat: Import for ImageAndTexttype + an input object.

--- libs/features/pages/consultancy-page/src/lib/main/elewa-consultancy-about-one/elewa-consultancy-about-one.component.ts
+++ libs/features/pages/consultancy-page/src/lib/main/elewa-consultancy-about-one/elewa-consultancy-about-one.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
+
 
 @Component({
   selector: 'elewa-website-elewa-consultancy-about-one',
   templateUrl: './elewa-consultancy-about-one.component.html',
   styleUrls: ['./elewa-consultancy-about-one.component.scss'],
 })
-export class ElewaConsultancyAboutOneComponent {}
+export class ElewaConsultancyAboutOneComponent {
+
+  /* an object(InputData) to input data using type ImageAndText from re-usable component */
+  InputData !: ImageAndText;
+}
