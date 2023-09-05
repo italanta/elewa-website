605b6e29c7f65331dfe4ebfb8ec9dea17373d5fc
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-05 09:24:57 +0300

    :sparkles: feat: create and export the main page component

--- libs/elements/layout/pages/main-page/src/lib/main-page.module.ts
+++ libs/elements/layout/pages/main-page/src/lib/main-page.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainPageComponent } from './main-page/main-page.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [MainPageComponent],
+  exports: [MainPageComponent],
 })
 export class MainPageModule {}
---
+++ libs/elements/layout/pages/main-page/src/lib/main-page/main-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./main-page.component.scss'],
+})
+export class MainPageComponent {}
