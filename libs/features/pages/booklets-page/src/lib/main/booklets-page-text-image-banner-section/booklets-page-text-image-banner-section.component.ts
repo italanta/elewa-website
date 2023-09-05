175f773193693d278a82539347b0dd1d9024a59c
x-russian <philophilo037@gmail.com>
2023-09-05 12:31:44 +0300

    :art: Import banner-module to new standalone component.

--- libs/features/pages/booklets-page/src/lib/main/booklets-page-text-image-banner-section/booklets-page-text-image-banner-section.component.ts
+++ libs/features/pages/booklets-page/src/lib/main/booklets-page-text-image-banner-section/booklets-page-text-image-banner-section.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BannersModule } from '@elewa-website/elements/banners';
 
 @Component({
   selector: 'elewa-website-booklets-page-text-image-banner-section',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BannersModule],
   templateUrl: './booklets-page-text-image-banner-section.component.html',
   styleUrls: ['./booklets-page-text-image-banner-section.component.scss'],
 })
