5cc11b931e73d3fa809ccfd91986b5c18d3fdaad
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-06 12:16:42 +0300

    :sparkles: feat: create projects section component

---
+++ libs/features/pages/home/src/lib/components/projects-section/projects-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-projects-section',
+  templateUrl: './projects-section.component.html',
+  styleUrls: ['./projects-section.component.scss'],
+})
+export class ProjectsSectionComponent {}
--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -6,12 +6,17 @@ import { MainPageModule } from '@elewa-website/elements/layout/pages/main-page';
 
 import { HomePageComponent } from './main/home/home-page.component';
 import { NewsSectionComponent } from './components/news-section/news-section.component';
+import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
 
 import { HomeRoutingModule } from './home.routing';
 
 @NgModule({
   imports: [CommonModule, HomeRoutingModule, CardsModule, MainPageModule],
-  declarations: [HomePageComponent, NewsSectionComponent],
+  declarations: [
+    HomePageComponent,
+    NewsSectionComponent,
+    ProjectsSectionComponent,
+  ],
   exports: [HomePageComponent],
 })
 export class FeaturesPagesHomeModule {}
