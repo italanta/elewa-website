b1269c966768a6a16b5629450a0e79ab8333023a
x-russian <philophilo037@gmail.com>
2023-09-06 12:56:22 +0300

    :sparkles: Import ImageAndText type + declare input-data obj.

--- libs/features/pages/content-development/src/lib/main/elewa-content-dev-about-one/elewa-content-dev-about-one.component.ts
+++ libs/features/pages/content-development/src/lib/main/elewa-content-dev-about-one/elewa-content-dev-about-one.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
 
 @Component({
   selector: 'elewa-website-elewa-content-dev-about-one',
   templateUrl: './elewa-content-dev-about-one.component.html',
   styleUrls: ['./elewa-content-dev-about-one.component.scss'],
 })
-export class ElewaContentDevAboutOneComponent {}
+export class ElewaContentDevAboutOneComponent {
+
+    /* an object(InputData) to input data using type ImageAndText from re-usable component */
+    InputData !: ImageAndText;
+    
+}
