d24d331e9bc51ebd3148e8ee374553229bdda0f1
levy <lnjiru43@gmail.com>
2023-09-06 13:32:46 +0300

    added component to accept input of type ImageAndDetails

--- libs/features/pages/content-development/src/lib/components/elewa-content-dev-about-two/elewa-content-dev-about-two.component.ts
+++ libs/features/pages/content-development/src/lib/components/elewa-content-dev-about-two/elewa-content-dev-about-two.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ImageAndDetails } from '@elewa-website/models/schema/ui/banners';
+import { InfoCard } from '@elewa-website/models/schema/ui/cards';
+import { ImageConfig } from '@elewa-website/models/schema/ui/images';
 
 @Component({
   selector: 'elewa-website-elewa-content-dev-about-two',
   templateUrl: './elewa-content-dev-about-two.component.html',
   styleUrls: ['./elewa-content-dev-about-two.component.scss'],
 })
-export class ElewaContentDevAboutTwoComponent {}
+export class ElewaContentDevAboutTwoComponent {
+  @Input() imageAndDetails!: ImageAndDetails;
+
+
+}
