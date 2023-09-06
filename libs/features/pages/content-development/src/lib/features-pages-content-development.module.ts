b363b48469bbaade643cb1382960b9d4360d65de
x-russian <philophilo037@gmail.com>
2023-09-06 12:07:11 +0300

    :package:  Elewa-content-dev-about-one component.

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
+import { ElewaContentDevAboutOneComponent } from './main/elewa-content-dev-about-one/elewa-content-dev-about-one.component';
 
 @NgModule({
   imports: [CommonModule, ContentDevelopmentPageRoutingModule],
-  declarations: [ContentDevelopmentPageComponent],
-  exports: [ContentDevelopmentPageComponent]
+  declarations: [
+    ContentDevelopmentPageComponent,
+    ElewaContentDevAboutOneComponent,
+  ],
+  exports: [ContentDevelopmentPageComponent],
 })
 export class ContentDevelopmentModule {}
---
+++ libs/features/pages/content-development/src/lib/main/elewa-content-dev-about-one/elewa-content-dev-about-one.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-content-dev-about-one',
+  templateUrl: './elewa-content-dev-about-one.component.html',
+  styleUrls: ['./elewa-content-dev-about-one.component.scss'],
+})
+export class ElewaContentDevAboutOneComponent {}
