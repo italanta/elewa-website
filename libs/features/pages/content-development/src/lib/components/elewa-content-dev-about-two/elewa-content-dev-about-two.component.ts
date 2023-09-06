dd7550c12f3c81418c83f94d8e484eadbbe9b6f2
levy <lnjiru43@gmail.com>
2023-09-06 12:15:05 +0300

    :sparkles: generated elewa-content-dev-about-two component

---
+++ libs/features/pages/content-development/src/lib/components/elewa-content-dev-about-two/elewa-content-dev-about-two.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-content-dev-about-two',
+  templateUrl: './elewa-content-dev-about-two.component.html',
+  styleUrls: ['./elewa-content-dev-about-two.component.scss'],
+})
+export class ElewaContentDevAboutTwoComponent {}
--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -4,10 +4,14 @@ import { CommonModule } from '@angular/common';
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
+import { ElewaContentDevAboutTwoComponent } from './components/elewa-content-dev-about-two/elewa-content-dev-about-two.component';
 
 @NgModule({
   imports: [CommonModule, ContentDevelopmentPageRoutingModule],
-  declarations: [ContentDevelopmentPageComponent],
-  exports: [ContentDevelopmentPageComponent]
+  declarations: [
+    ContentDevelopmentPageComponent,
+    ElewaContentDevAboutTwoComponent,
+  ],
+  exports: [ContentDevelopmentPageComponent],
 })
 export class ContentDevelopmentModule {}
