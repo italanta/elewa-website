c2397fe3a22378b3da44f5f05346a120ade9e501
Preston <prestonosoro56@gmail.com>
2023-09-06 04:38:14 +0300

    feat: import modules

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -5,9 +5,11 @@ import { ConsultancyRoutingModule } from './consultancy.routing';
 
 import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
 import { ElewaConsultancyAboutOneComponent } from './elewa-consultancy-about-one/elewa-consultancy-about-one.component';
+import { BannersModule } from '@elewa-website/elements/banners';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
 
 @NgModule({
-  imports: [CommonModule, ConsultancyRoutingModule],
+  imports: [CommonModule, ConsultancyRoutingModule,BannersModule,TextsModule], /**import modules */
   declarations: [ConsultancyPageComponent, ElewaConsultancyAboutOneComponent],
 })
 export class ConsultancyPageModule {}
