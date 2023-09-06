1468b2e2c1a86baab1876e7c7ab32a4e44a9f5cd
x-russian <philophilo037@gmail.com>
2023-09-06 07:58:27 +0300

    :sparkles: Imports banner module.

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -5,9 +5,10 @@ import { ConsultancyRoutingModule } from './consultancy.routing';
 
 import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
 import { ElewaConsultancyAboutTwoComponent } from './main/elewa-consultancy-about-two/elewa-consultancy-about-two.component';
+import { BannersModule } from '@elewa-website/elements/banners';
 
 @NgModule({
-  imports: [CommonModule, ConsultancyRoutingModule],
+  imports: [CommonModule, ConsultancyRoutingModule, BannersModule],
   declarations: [ConsultancyPageComponent, ElewaConsultancyAboutTwoComponent],
 })
 export class ConsultancyPageModule {}
