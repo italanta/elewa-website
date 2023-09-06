71eb56c75361298e2a3c6fa8176ce097bc78b123
levy <lnjiru43@gmail.com>
2023-09-06 09:13:47 +0300

    :art: removed unneccesary method

--- libs/elements/banners/src/lib/components/elewa-image-and-details-banner/elewa-image-and-details-banner.component.ts
+++ libs/elements/banners/src/lib/components/elewa-image-and-details-banner/elewa-image-and-details-banner.component.ts
@@ -9,23 +9,5 @@ import { ImageAndDetails } from '@elewa-website/models/schema/ui/banners';
 })
 export class ElewaImageAndDetailsBannerComponent {
   @Input() imageAnddetails!: ImageAndDetails;
-
-    /**
-   * Get the CSS style for the image position based on the provided string.
-   *
-   * @param {string} imagePosition - The position of the image ('left' or 'right').
-   * @returns {{ [key: string]: string }} - An object representing CSS styles.
-   */
-
-  getPositionStyle(imagePosition: string) :string  {
-    switch (imagePosition) {
-      case 'left':
-        return "info-card-image-left";
-      case 'right':
-        return "info-card-image-right";
-      default:
-        return ""; // default value
-    }
-  }
-  
+ 
 }
