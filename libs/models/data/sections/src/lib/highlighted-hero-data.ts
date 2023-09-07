5c8074b83ff0249c11521841b8c9cb28344d3abd
Preston <prestonosoro56@gmail.com>
2023-09-08 00:29:38 +0300

    feat: create consultancy hero section data

---
+++ libs/models/data/sections/src/lib/highlighted-hero-data.ts
@@ -0,0 +1,9 @@
+import { ContentText } from '@elewa-website/models/schema/ui/texts';
+export const __highlightedContent: ContentText[] = [
+  {
+    title: ' Learning design',
+    descriptions: [
+      'Your organisation has the potential to turn every task into an opportunity to learn. From growing crops, to nursing someone to good health, to developing the next-generation technology. Let’s work together to make the magic happen.',
+    ],
+  },
+];
