6c888792010f26cadc649554ea3ae1fc4a112c8e
levy <lnjiru43@gmail.com>
2023-09-07 20:24:19 +0300

    menu item html and css and input of type MenuItem

--- libs/elements/layout/header/src/lib/components/menu-item/menu-item.component.ts
+++ libs/elements/layout/header/src/lib/components/menu-item/menu-item.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { MenuItem } from '@elewa-website/models/schema/ui/header';
 
 @Component({
   selector: 'elewa-website-menu-item',
   templateUrl: './menu-item.component.html',
   styleUrls: ['./menu-item.component.scss'],
 })
-export class MenuItemComponent {}
+export class MenuItemComponent {
+  @Input() menuItem!: MenuItem;
+}
