69c852ca5843b16f9d00e2f2b86a16141c51c035
x-russian <philophilo037@gmail.com>
2023-09-07 09:42:21 +0300

    :sparkles: Import infoCard type + init input obj.

--- libs/features/pages/consultancy-page/src/lib/main/elewa-website-learning-services/elewa-website-learning-services.component.ts
+++ libs/features/pages/consultancy-page/src/lib/main/elewa-website-learning-services/elewa-website-learning-services.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { InfoCard } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-elewa-website-learning-services',
   templateUrl: './elewa-website-learning-services.component.html',
   styleUrls: ['./elewa-website-learning-services.component.scss'],
 })
-export class ElewaWebsiteLearningServicesComponent {}
+export class ElewaWebsiteLearningServicesComponent {
+
+  /* an input object with the type InfoCard. **/
+  InputCardData !: InfoCard;
+}
