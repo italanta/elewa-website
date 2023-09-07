9c8d83f3d07074e81bf8227c66c3f20871c83419
Oduol Peris <oduolpepe618@gmail.com>
2023-09-07 20:33:22 +0300

    🎨feat:Add About text

--- libs/features/pages/about/src/lib/components/about-hero-section/about-hero-section.component.ts
+++ libs/features/pages/about/src/lib/components/about-hero-section/about-hero-section.component.ts
@@ -9,9 +9,9 @@ import { __elewaImageData } from '@elewa-website/data/schema/ui/images';
 })
 export class AboutHeroSectionComponent {
   heroAboutData = {
-    title: 'Conversational Learning',
+    title: 'Lorem ipsum dolorem sit eos',
     subtitle: '',
-    description: 'Your organisation has the potential to turn every task into an opportunity to learn. From growing crops, to nursing someone to good health, to developing the next-generation technology, let’s work together to make the magic happen.'
+    description: 'Neque porro quisquam dolorem ipsum quia dolor sit Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam dolorem ipsum quia dolor sit Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam dolorem ipsum quia dolor.'
   };
   image= __elewaImageData
 
