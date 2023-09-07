602c6805846a9b4106878dcc51e34aee60b2dd37
Preston <prestonosoro56@gmail.com>
2023-09-07 23:00:12 +0300

    fix: fix order of imports

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 import { ConsultancyRoutingModule } from './consultancy.routing';
 
 import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
 import { ConsultancyHeroSectionComponent } from './consultancy-hero-section/consultancy-hero-section.component';
-import { TextsModule } from '@elewa-website/elements/layout/texts';
-import { AppHeaderModule } from '@elewa-website/elements/layout/header';
-import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 
 @NgModule({
   imports: [
