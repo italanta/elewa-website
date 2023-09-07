f56168602eb674aae2f72eb1837e46e3875e0a6b
+ <+>
2023-09-07 11:15:51 +0300

    :art: imported app header module and buttons module

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -5,9 +5,11 @@ import { ConsultancyRoutingModule } from './consultancy.routing';
 
 import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
 import { ConsultancyHeroSectionComponent } from './components/consultancy-hero-section/consultancy-hero-section.component';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 
 @NgModule({
-  imports: [CommonModule, ConsultancyRoutingModule],
+  imports: [CommonModule, ConsultancyRoutingModule, ButtonsModule, AppHeaderModule],
   declarations: [ConsultancyPageComponent, ConsultancyHeroSectionComponent],
   exports: [ConsultancyHeroSectionComponent]
 })
