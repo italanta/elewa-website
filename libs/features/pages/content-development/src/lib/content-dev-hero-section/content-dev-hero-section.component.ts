fd3b3c58eaa0b64a7dea660bc5921162a87b0ca6
Preston <prestonosoro56@gmail.com>
2023-09-07 22:44:49 +0300

    feat: assign the imported data to heroTextData

--- libs/features/pages/content-development/src/lib/content-dev-hero-section/content-dev-hero-section.component.ts
+++ libs/features/pages/content-development/src/lib/content-dev-hero-section/content-dev-hero-section.component.ts
@@ -1,17 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ContentText } from '@elewa-website/models/schema/ui/texts';
+import { __highlightedContent as heroTextData } from '@elewa-website/models/data/sections';
 
 @Component({
   selector: 'elewa-website-content-dev-hero-section',
   templateUrl: './content-dev-hero-section.component.html',
   styleUrls: ['./content-dev-hero-section.component.scss'],
 })
-export class ContentDevHeroSectionComponent {
-  heroTextData = {
-    title: 'Content Development',
-    description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor dolorem ipsum quia dolor sit Eos qui ratione voluptatem sequi nesciunt.',
-  };
-  sliderButtonData = {
-    text: 'Unlock your potential',
-  };
+export class ContentDevHeroSectionComponent implements OnInit {
+  @Input() heroTextData!: ContentText[];
+
+  ngOnInit() {
+    // Assign the imported data to heroTextData directly
+    this.heroTextData = heroTextData;
+  }
 }
