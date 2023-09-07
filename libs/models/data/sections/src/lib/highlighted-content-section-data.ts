8efd9c1842c3db99d7299d3445708b9fe5a5b47e
Preston <prestonosoro56@gmail.com>
2023-09-07 22:46:40 +0300

    feat: create content dev hero section data

---
+++ libs/models/data/sections/src/lib/highlighted-content-section-data.ts
@@ -0,0 +1,9 @@
+import { ContentText } from '@elewa-website/models/schema/ui/texts';
+export const __highlightedContent: ContentText[] = [
+  {
+    title: 'Content Development',
+    descriptions: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor dolorem ipsum quia dolor sit Eos qui ratione voluptatem sequi nesciunt.',
+    ],
+  },
+];
