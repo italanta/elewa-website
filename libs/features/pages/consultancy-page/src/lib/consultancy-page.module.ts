a17d9ae3fd7eaeac1c8f5b586cfe766cd4167150
Gikonyo-Otieno <gikonyootieno@gmail.com>
2023-09-07 10:09:19 +0300

    ✨ imported CardsModule and InfoCard

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -5,9 +5,10 @@ import { ConsultancyRoutingModule } from './consultancy.routing';
 
 import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
 import { ElewaWebsiteLearningServicesComponent } from './components/elewa-website-learning-services/elewa-website-learning-services.component';
+import { CardsModule } from '@elewa-website/elements/cards';
 
 @NgModule({
-  imports: [CommonModule, ConsultancyRoutingModule],
+  imports: [CommonModule, ConsultancyRoutingModule,CardsModule],
   declarations: [ConsultancyPageComponent, ElewaWebsiteLearningServicesComponent],
 })
 export class ConsultancyPageModule {}
