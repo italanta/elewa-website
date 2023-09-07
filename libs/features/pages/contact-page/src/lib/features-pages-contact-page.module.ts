53fe61aa3e019d69da8865f2ba6106615945da7e
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-09-07 16:02:32 +0300

    :pencil2: fixed order of imports

--- libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
+++ libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ReactiveFormsModule } from '@angular/forms';
+
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 
 import { ContactPageComponent } from './main/contact-page/contact-page.component';
 
 import { ContactRoutingModule } from './contact-page.routing';
 import { ContactPageFormSectionComponent } from './components/contact-page-form-section/contact-page-form-section.component';
  
-import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
-import { AppHeaderModule } from '@elewa-website/elements/layout/header';
+
 
 @NgModule({
   imports: [CommonModule, ContactRoutingModule,ButtonsModule,ReactiveFormsModule,AppHeaderModule],
