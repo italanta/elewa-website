a46fc915c463c46670c83ed260bd785b1e9e10f9
+ <+>
2023-09-07 11:17:09 +0300

    :art: updated buttons data on ts file

--- libs/features/pages/consultancy-page/src/lib/components/consultancy-hero-section/consultancy-hero-section.component.ts
+++ libs/features/pages/consultancy-page/src/lib/components/consultancy-hero-section/consultancy-hero-section.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-import { HeroText } from '@elewa-website/models/schema/ui/texts';
 
 @Component({
   selector: 'elewa-website-consultancy-hero-section',
@@ -8,8 +7,12 @@ import { HeroText } from '@elewa-website/models/schema/ui/texts';
   styleUrls: ['./consultancy-hero-section.component.scss'],
 })
 export class ConsultancyHeroSectionComponent {
-  consultancyHeroSection : HeroText = {
+  consultancyHeroSection = {
     title: 'Learning design',
     description: "Your organization has the potential to turn every task into an opportunity to learn. From growing crops, to nursing someone to good health to developing the next-generation technology. Let's work together to make the magic happen."
+  };
+
+  buttonData  = {
+    text: 'Unlock your potential'
   }
 }
