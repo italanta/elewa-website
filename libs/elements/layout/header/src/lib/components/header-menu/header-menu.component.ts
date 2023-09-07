11841d2973077264c29975600ed3613f49b41fb3
levy <lnjiru43@gmail.com>
2023-09-07 14:20:48 +0300

    :sparkles: generated header-menu component

---
+++ libs/elements/layout/header/src/lib/components/header-menu/header-menu.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-header-menu',
+  templateUrl: './header-menu.component.html',
+  styleUrls: ['./header-menu.component.scss'],
+})
+export class HeaderMenuComponent {}
