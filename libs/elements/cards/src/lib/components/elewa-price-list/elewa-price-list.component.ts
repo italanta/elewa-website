20bae5194dbb4b3cb794f74d4200c5d8a21e8017
mutagah <kihahumutagah@gmail.com>
2023-09-07 14:56:02 +0300

    feat: Render and style and price item card

--- libs/elements/cards/src/lib/components/elewa-price-list/elewa-price-list.component.ts
+++ libs/elements/cards/src/lib/components/elewa-price-list/elewa-price-list.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
-import { ElewaWebsitePriceItemCardComponent } from '../elewa-price-item-card/elewa-website-price-item-card.component';
+import { __highlightedPricingList } from '@elewa-website/data/sections';
 @Component({
   selector: 'elewa-website-elewa-price-list',
   templateUrl: './elewa-price-list.component.html',
   styleUrls: ['./elewa-price-list.component.scss']
 })
 export class ElewaPriceListComponent {
-  
+  highlightedPricingList = __highlightedPricingList
 }
