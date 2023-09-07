1f68132f0891c9ccb80cbb1d1c19340a997f3d10
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-09-07 15:56:03 +0300

    :art:added onginit hook

--- libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
+++ libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
@@ -1,31 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+
 import { FormControl,FormGroup } from '@angular/forms';
+import { __buttonData } from '@elewa-website/models/data/sections';
 @Component({
   selector: 'elewa-website-contact-page-form-section',
   templateUrl: './contact-page-form-section.component.html',
   styleUrls: ['./contact-page-form-section.component.scss'],
 })
-export class ContactPageFormSectionComponent {
-  buttonData = {
-    text: 'Send message',
-    /** base colors on button */
-    color: '#ffffff',
-    bgColor: '#292A50',
-    iconColor: '#000000',
-    borderColor: '#000000',
-    iconBackgroundColor: '#ffffff',
-
-    /** hover effect styles on the button  */
-    hoverColor: '#000000',
-    hoverBgColor: '#ffffff',
-    hoverIconColor: '#000000',
-    hoverBorderColor: '#00000',
-    hoverIconBackgroundColor: '#000000',
-  };
+export class ContactPageFormSectionComponent  implements OnInit{
+buttonData = __buttonData;
   formData!: FormGroup;
-  
+
    handleSubmit(){
-    console.log(this.formData.value)
+  this.formData.value
    }
    buildContactForm(){
     this.formData =new FormGroup({
@@ -36,4 +23,8 @@ export class ContactPageFormSectionComponent {
       message: new FormControl(''),
     })
    }
+ 
+ngOnInit(){
+  this.buildContactForm();
+}
 }
