5af429c60d4501029b1512faa946c653edfdd148
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-07 09:58:36 +0300

    :sparkles: feat: create sdg card component

---
+++ libs/elements/cards/src/lib/components/elewa-sdg-card/elewa-sdg-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-sdg-card',
+  templateUrl: './elewa-sdg-card.component.html',
+  styleUrls: ['./elewa-sdg-card.component.scss'],
+})
+export class ElewaSdgCardComponent {}
