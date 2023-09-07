9e2bdec28fe98e402ea6e1e6fe6a8568a1d78749
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-07 10:04:54 +0300

    :sparkles: feat: create sdg card component and use that to render the sdglist

--- libs/elements/cards/src/lib/components/elewa-sdg-card/elewa-sdg-card.component.ts
+++ libs/elements/cards/src/lib/components/elewa-sdg-card/elewa-sdg-card.component.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { SDGCard } from '@elewa-website/models/schema/ui/cards';
 
 @Component({
   selector: 'elewa-website-elewa-sdg-card',
   templateUrl: './elewa-sdg-card.component.html',
   styleUrls: ['./elewa-sdg-card.component.scss'],
 })
-export class ElewaSdgCardComponent {}
+export class ElewaSdgCardComponent {
+  /** input cardData of type @interface SDGCard */
+  @Input() cardsData!: SDGCard;
+}
