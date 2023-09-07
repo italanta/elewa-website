bb3c4fff71a81108ff5a1aa5ded218017f6c656f
mutagah <kihahumutagah@gmail.com>
2023-09-07 19:27:14 +0300

    :lipstick:feat: Making elewa-price-list-component-reusable

--- libs/elements/cards/src/lib/components/elewa-price-list/elewa-price-list.component.ts
+++ libs/elements/cards/src/lib/components/elewa-price-list/elewa-price-list.component.ts
@@ -1,12 +1,11 @@
-import { Component, Input , OnInit} from '@angular/core';
-import { PriceItem } from '@elewa-website/models/schema/ui/cards';
+import { Component, Input} from '@angular/core';
 
+import { PriceItem } from '@elewa-website/models/schema/ui/cards';
 @Component({
   selector: 'elewa-website-elewa-price-list',
   templateUrl: './elewa-price-list.component.html',
   styleUrls: ['./elewa-price-list.component.scss']
 })
 export class ElewaPriceListComponent {
-  
   @Input() highlightedPricingList!: PriceItem[]
 }
