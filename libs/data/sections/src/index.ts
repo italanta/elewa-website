edb95415272c06daa4f1f553bcbd4af998896843
Yahya <yahyahussein670@gmail.com>
2023-09-07 17:58:11 +0300

    ♻️: refractor: Moved the heroTexts and buttonData objects to a separate file for better organization and reusability.

--- libs/data/sections/src/index.ts
+++ libs/data/sections/src/index.ts
@@ -1,4 +1,7 @@
 export * from './lib/highlighted-project.data';
 export * from './lib/highlighted-news.data';
+
 export * from './lib/highlighted-sdg-list.data';
 export * from './lib/team-members-list.data'
+export * from './lib/team-members-list.data'
+export * from './lib/consultancy-hero.data'
--- libs/features/pages/consultancy-page/src/lib/components/consultancy-hero-section/consultancy-hero-section.component.ts
+++ libs/features/pages/consultancy-page/src/lib/components/consultancy-hero-section/consultancy-hero-section.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { HeroText } from '@elewa-website/models/schema/ui/texts';
-import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
+import { heroTexts, buttonData } from '@elewa-website/data/sections';
 
 
 @Component({
@@ -9,12 +8,6 @@ import { SliderButtonData } from '@elewa-website/models/schema/ui/buttons';
   styleUrls: ['./consultancy-hero-section.component.scss'],
 })
 export class ConsultancyHeroSectionComponent {
-  heroTexts: HeroText = {
-    title:"Learning Design",
-    description:"Your organisation has the potential to turn every task into an opportunity to learn. From growing crops, to nursing someone to good health, to developing the next-generation technology. Let’s work together to make the magic happen."
-  }
-
-  buttonData: SliderButtonData = {
-    text: "Unlock your potential"
-  }
+  heroTexts = heroTexts;
+  buttonData = buttonData;
 }
