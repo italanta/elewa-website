f22a4728e02923fd0507634975d331aeebaa7c6b
Gikonyo-Otieno <gikonyootieno@gmail.com>
2023-09-07 09:58:30 +0300

    ✨ generated elewa-website-learning-services

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ConsultancyRoutingModule } from './consultancy.routing';
 
 import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
+import { ElewaWebsiteLearningServicesComponent } from './components/elewa-website-learning-services/elewa-website-learning-services.component';
 
 @NgModule({
   imports: [CommonModule, ConsultancyRoutingModule],
-  declarations: [ConsultancyPageComponent],
+  declarations: [ConsultancyPageComponent, ElewaWebsiteLearningServicesComponent],
 })
 export class ConsultancyPageModule {}
