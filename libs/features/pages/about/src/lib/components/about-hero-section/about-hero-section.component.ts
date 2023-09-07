2b6e1312146e813a5282c64629b0c9904a431191
Oduol Peris <oduolpepe618@gmail.com>
2023-09-07 11:45:20 +0300

    ✨feat:pass text and image data

--- libs/features/pages/about/src/lib/components/about-hero-section/about-hero-section.component.ts
+++ libs/features/pages/about/src/lib/components/about-hero-section/about-hero-section.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
+import { __elewaImageData } from '@elewa-website/data/schema/ui/images';
+
 
 @Component({
   selector: 'elewa-website-about-hero-section',
   templateUrl: './about-hero-section.component.html',
   styleUrls: ['./about-hero-section.component.scss'],
 })
-export class AboutHeroSectionComponent {}
+export class AboutHeroSectionComponent {
+  heroAboutData = {
+    title: 'Conversational Learning',
+    subtitle: '',
+    description: 'Your organisation has the potential to turn every task into an opportunity to learn. From growing crops, to nursing someone to good health, to developing the next-generation technology, let’s work together to make the magic happen.'
+  };
+  image= __elewaImageData
+
+}
