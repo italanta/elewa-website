7b129a638c7f53b42b574eb556aca6f17bf6ee19
levy <lnjiru43@gmail.com>
2023-09-07 20:54:19 +0300

    added menu itme data and imported to header menu component

---
+++ libs/data/sections/src/lib/menu-item.data.ts
@@ -0,0 +1,22 @@
+import { MenuItem } from '@elewa-website/models/schema/ui/header';
+
+
+export const __consultancyItem: MenuItem = {
+    title: "consultancy",
+    description: "Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam"
+}
+
+export const __contentDevelopmentItem: MenuItem = {
+    title: "content development",
+    description: "Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam"
+}
+
+export const __bookletsItem: MenuItem = {
+    title: "conversational Learning",
+    description: "Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam"
+}
+
+export const __conversationalLearningItem: MenuItem = {
+    title: "booklets",
+    description: "Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam"
+}
--- libs/elements/layout/header/src/lib/components/header-menu/header-menu.component.ts
+++ libs/elements/layout/header/src/lib/components/header-menu/header-menu.component.ts
@@ -1,8 +1,18 @@
 import { Component } from '@angular/core';
 
+import { MenuItem } from '@elewa-website/models/schema/ui/header';
+import { __consultancyItem, __contentDevelopmentItem, __bookletsItem, __conversationalLearningItem } from '@elewa-website/data/sections';
+
 @Component({
   selector: 'elewa-website-header-menu',
   templateUrl: './header-menu.component.html',
   styleUrls: ['./header-menu.component.scss'],
 })
-export class HeaderMenuComponent {}
+export class HeaderMenuComponent {
+
+  consultancyItem = __consultancyItem
+  contentDevelopmentItem = __contentDevelopmentItem
+  bookletsItem = __bookletsItem
+  conversationalLearningItem = __conversationalLearningItem
+  
+}
