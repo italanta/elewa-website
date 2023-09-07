1da03d69985790d38b35e03a662e61ea168ea458
Preston <prestonosoro56@gmail.com>
2023-09-07 16:01:42 +0300

    fix: fix order of imports

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { TextsModule } from '@elewa-website/elements/layout/texts';
+import { AppHeaderModule } from '@elewa-website/elements/layout/header';
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
 import { ContentDevHeroSectionComponent } from './content-dev-hero-section/content-dev-hero-section.component';
-import { TextsModule } from '@elewa-website/elements/layout/texts';
-import { AppHeaderModule } from '@elewa-website/elements/layout/header';
-import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+
 
 @NgModule({
   imports: [
