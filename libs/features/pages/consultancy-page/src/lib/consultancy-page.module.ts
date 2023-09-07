9afb1aa803903b8f1d3c5b7478bfde8cdf17e6ef
+ <+>
2023-09-07 10:37:14 +0300

    :art: exported consultancy hero section component

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -4,9 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ConsultancyRoutingModule } from './consultancy.routing';
 
 import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
+import { ConsultancyHeroSectionComponent } from './components/consultancy-hero-section/consultancy-hero-section.component';
 
 @NgModule({
   imports: [CommonModule, ConsultancyRoutingModule],
-  declarations: [ConsultancyPageComponent],
+  declarations: [ConsultancyPageComponent, ConsultancyHeroSectionComponent],
+  exports: [ConsultancyHeroSectionComponent]
 })
 export class ConsultancyPageModule {}
