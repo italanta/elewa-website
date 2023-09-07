7d946d90abd5424e60fbcd886909a44095c4dfa1
kiplangat01 <meshnobie@gmail.com>
2023-09-07 15:31:54 +0300

    deleted extra file

--- libs/data/sections/src/lib/consoltancy-page-about.data.ts
+++ libs/data/sections/src/lib/consoltancy-page-about.data.ts
@@ -9,6 +9,7 @@ export const _consultancyPageAbout: ImageAndText = {
     title: 'our approach to content development',
     descriptions: ['Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil beatae, porro aliquam soluta blanditiis ', 'quia animi voluptates doloremque magnam quis quibusdam, assumenda, cupiditate recusandae aut et! Esse facere hic iusto!'],
   },
+  
   image: {
     imageSrc: 'https://media.istockphoto.com/id/1322139094/photo/multiethnic-colleagues-sitting-at-desk-looking-at-laptop-computer-in-office.jpg?s=612x612&w=0&k=20&c=xrGP_nwtcdaZF8heKJ_FiEqnuqWxv1A7i71YZLXxSaE=',
     title: 'lorem',
