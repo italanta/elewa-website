982d989fe79146cdb75b44c92c5fa5e751fbd297
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-07 08:43:48 +0300

    :sparkles: generated the learning services component

---
+++ libs/features/pages/consultancy-page/src/lib/components/elewa-website-learning-services/elewa-website-learning-services.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-elewa-website-learning-services',
+  templateUrl: './elewa-website-learning-services.component.html',
+  styleUrls: ['./elewa-website-learning-services.component.scss'],
+})
+export class ElewaWebsiteLearningServicesComponent {}
