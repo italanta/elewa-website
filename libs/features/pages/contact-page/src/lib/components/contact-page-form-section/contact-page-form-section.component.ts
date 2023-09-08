9da0fc6513d46a877b04a170962f9e2985933266
Nicholas <nickndolo95@gmail.com>
2023-09-08 10:47:05 +0300

    create formGroup model

--- libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
+++ libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'elewa-website-contact-page-form-section',
   templateUrl: './contact-page-form-section.component.html',
   styleUrls: ['./contact-page-form-section.component.scss'],
 })
-export class ContactPageFormSectionComponent {}
+export class ContactPageFormSectionComponent {
+
+  //generate form controls
+  contactForm = this.fb.group({
+    name: [''],
+    companyName: [''],
+    selectOption: [''],
+    message: [''],
+  })
+
+  //inject the FormBuilder service
+  constructor( private fb: FormBuilder) {
+
+  }
+}
