a5965b53cdfc6440236942fdc0adad18233fbcec
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-09-08 12:09:34 +0300

    imported data(imageAndDetails)

--- libs/features/pages/content-development/src/lib/component/elewa-content-dev-about-two/elewa-content-dev-about-two.component.ts
+++ libs/features/pages/content-development/src/lib/component/elewa-content-dev-about-two/elewa-content-dev-about-two.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { __imageAnddetails } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-elewa-content-dev-about-two',
   templateUrl: './elewa-content-dev-about-two.component.html',
   styleUrls: ['./elewa-content-dev-about-two.component.scss'],
 })
-export class ElewaContentDevAboutTwoComponent {}
+export class ElewaContentDevAboutTwoComponent {
+  imageAnddetails = __imageAnddetails
+}
