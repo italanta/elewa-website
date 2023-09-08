6bdb61f71d39e12c17c4fb178e42b79936add2ff
jbdura <bidurajohnson@gmail.com>
2023-09-08 17:54:55 +0300

    Added display of information

--- libs/features/pages/consultancy-page/src/lib/components/elewa-consultancy-about-two/elewa-consultancy-about-two.component.ts
+++ libs/features/pages/consultancy-page/src/lib/components/elewa-consultancy-about-two/elewa-consultancy-about-two.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-// import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text'; // Import the ImageAndText type
-import { __imageAndTextData } from '@elewa-website/data/sections';
+import { __consultancyImageAndTextData } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-elewa-consultancy-about-two',
@@ -8,5 +7,7 @@ import { __imageAndTextData } from '@elewa-website/data/sections';
   styleUrls: ['./elewa-consultancy-about-two.component.scss'],
 })
 export class ElewaConsultancyAboutTwoComponent {
-  imageAndTextData = __imageAndTextData
+
+  imageAndTextData = __consultancyImageAndTextData
+
 }
