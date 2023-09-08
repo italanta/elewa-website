016bd84bcdbda8b6d8b0c8ac5c4cc7deb7625a46
kevinkkimutai <kiruikevinkim@gmail.com>
2023-09-08 10:43:52 +0300

    ✨ feat: generated contact-page

---
+++ libs/features/pages/contact-page/src/index.ts
@@ -0,0 +1,1 @@
+export * from './lib/features-pages-contact-page.module';
---
+++ libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactPageFormSectionComponent } from './components/contact-page-form-section/contact-page-form-section.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [ContactPageFormSectionComponent],
+})
+export class FeaturesPagesContactPageModule {}
