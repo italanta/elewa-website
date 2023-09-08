181d9bd9696c055dd3f5de00ac8b873c39ae98c8
dancunrandy <dancunrandy@gmail.com>
2023-09-08 10:31:31 +0300

    imported consultancy page

--- libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
+++ libs/features/pages/consultancy-page/src/lib/consultancy-page.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { CardsModule } from '@elewa-website/elements/cards';
+
 import { ConsultancyRoutingModule } from './consultancy.routing';
 
 import { ConsultancyPageComponent } from './main/consultancy-page/consultancy-page.component';
 import { ElewaWebsiteLearningServicesComponent } from './Component/elewa-website-learning-services/elewa-website-learning-services.component';
 
 @NgModule({
-  imports: [CommonModule, ConsultancyRoutingModule],
+  imports: [CommonModule, ConsultancyRoutingModule, CardsModule],
   declarations: [
     ConsultancyPageComponent,
     ElewaWebsiteLearningServicesComponent,
