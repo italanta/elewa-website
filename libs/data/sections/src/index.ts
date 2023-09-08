69968ac27bc85b6ad9bc606ea5a0e1f8e24141c4
dancunrandy <dancunrandy@gmail.com>
2023-09-08 10:32:39 +0300

    exported data

--- libs/data/sections/src/index.ts
+++ libs/data/sections/src/index.ts
@@ -2,3 +2,4 @@ export * from './lib/highlighted-project.data';
 export * from './lib/highlighted-news.data';
 export * from './lib/highlighted-sdg-list.data';
 export * from './lib/team-members-list.data'
+export * from './lib/leaning-service.data'
