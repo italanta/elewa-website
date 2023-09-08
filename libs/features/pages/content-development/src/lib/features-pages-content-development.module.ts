60d95c8c1d94980a1837a8ffd8a21d9f5c7e0173
jbdura <bidurajohnson@gmail.com>
2023-09-08 12:21:21 +0300

    Created the component

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -3,11 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 
+import { BannersModule } from '@elewa-website/elements/banners';
+
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
+import { ElewaContentDevAboutOneComponent } from './components/elewa-content-dev-about-one/elewa-content-dev-about-one.component';
 
 @NgModule({
-  imports: [CommonModule, ContentDevelopmentPageRoutingModule],
-  declarations: [ContentDevelopmentPageComponent],
-  exports: [ContentDevelopmentPageComponent]
+  imports: [
+    CommonModule, 
+    ContentDevelopmentPageRoutingModule,
+    BannersModule,
+  ],
+  declarations: [
+    ContentDevelopmentPageComponent,
+    ElewaContentDevAboutOneComponent,
+  ],
+  exports: [ ContentDevelopmentPageComponent, ElewaContentDevAboutOneComponent ],
 })
 export class ContentDevelopmentModule {}
