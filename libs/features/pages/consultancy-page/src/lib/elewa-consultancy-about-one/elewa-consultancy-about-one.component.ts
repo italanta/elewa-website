75843a1ac7fd6f9ee7a3648327f9c03c69ee9f59
Preston <prestonosoro56@gmail.com>
2023-09-08 10:34:24 +0300

    feat:import image data

--- libs/features/pages/consultancy-page/src/lib/elewa-consultancy-about-one/elewa-consultancy-about-one.component.ts
+++ libs/features/pages/consultancy-page/src/lib/elewa-consultancy-about-one/elewa-consultancy-about-one.component.ts
@@ -1,32 +1,27 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
-import { ContentText } from '@elewa-website/models/schema/ui/texts';
+import {
+  __highlightedImage as imageData,
+  getLayoutClass,
+} from '@elewa-website/models/data/sections';
 
 @Component({
   selector: 'elewa-website-elewa-consultancy-about-one',
   templateUrl: './elewa-consultancy-about-one.component.html',
   styleUrls: ['./elewa-consultancy-about-one.component.scss'],
 })
-export class ElewaConsultancyAboutOneComponent {
-  @Input() ImageAndText!: ImageAndText;
+export class ElewaConsultancyAboutOneComponent implements OnInit {
+  @Input() imageData!: ImageAndText[];
 
-  // Use the ContentText type for the content input
-  @Input() content: ContentText = {
-    title: 'People at the center',
-    descriptions: [
-      'People are the secret to successful education programs. Through in-depth research, we work with your organization at different levels to understand (“elewa”) how people live and learn.',
-      'Based on decades of research, this understanding allows us to co-create strong learning networks within your organization. Networks that can be re-used every time a training need arises; At a fraction of the cost of current solutions.',
-    ], // An array of strings
-  };
+  ngOnInit() {
+    // Assign the imported data to imageData directly
+    this.imageData = imageData;
+    // Debugging: Log the loaded data
+    // console.log('Loaded imageData:', this.imageData);
+  }
 
-  getLayoutClass(): string {
-    // Check if ImageAndText is defined before accessing its properties
-    if (this.ImageAndText && this.ImageAndText.imagePosition) {
-      return this.ImageAndText.imagePosition === 'left'
-        ? 'left-image'
-        : 'right-image';
-    }
-    // Default layout class if ImageAndText is not defined or lacks 'imagePosition'
-    return 'default-layout';
+  // Reference the imported getLayoutClass function
+  getLayoutClass(item: ImageAndText): string {
+    return getLayoutClass(item);
   }
 }
