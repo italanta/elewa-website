e60ee64c602ff6e4bfd78c9b16e28d7ae2f7e69b
Markmutuku <mark.mutuku@student.moringaschool.com>
2023-09-08 12:17:02 +0300

    added the necessary imports for the bannersModule, commonModule,contentDevelopmentPageRoutingModule

--- libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
+++ libs/features/pages/content-development/src/lib/features-pages-content-development.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { BannersModule } from '@elewa-website/elements/banners';
 import { ContentDevelopmentPageComponent } from './main/content-development-page/content-development-page.component';
 
 import { ContentDevelopmentPageRoutingModule } from './content-development.routing';
 import { ElewaContentDevAboutTwoComponent } from './component/elewa-content-dev-about-two/elewa-content-dev-about-two.component';
 
 @NgModule({
-  imports: [CommonModule, ContentDevelopmentPageRoutingModule],
+  imports: [CommonModule, ContentDevelopmentPageRoutingModule, BannersModule],
   declarations: [
     ContentDevelopmentPageComponent,
     ElewaContentDevAboutTwoComponent,
