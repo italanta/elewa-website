f5f982ef718b5ecef971f6a3ce59a6f52c82fb32
jbdura <bidurajohnson@gmail.com>
2023-09-08 17:03:00 +0300

    working on displaying data

--- libs/features/pages/consultancy-page/src/lib/components/elewa-consultancy-about-two/elewa-consultancy-about-two.component.ts
+++ libs/features/pages/consultancy-page/src/lib/components/elewa-consultancy-about-two/elewa-consultancy-about-two.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+// import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text'; // Import the ImageAndText type
+import { __imageAndTextData } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-elewa-consultancy-about-two',
   templateUrl: './elewa-consultancy-about-two.component.html',
   styleUrls: ['./elewa-consultancy-about-two.component.scss'],
 })
-export class ElewaConsultancyAboutTwoComponent {}
+export class ElewaConsultancyAboutTwoComponent {
+  imageAndTextData = __imageAndTextData
+}
