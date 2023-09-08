6fad85e8b7dda035bc801b2214ff1545bcb55c90
kevinkkimutai <kiruikevinkim@gmail.com>
2023-09-08 13:39:13 +0300

    ✨ feat: generate contact-page  component

--- libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
+++ libs/features/pages/contact-page/src/lib/components/contact-page-form-section/contact-page-form-section.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'elewa-website-contact-page-form-section',
   templateUrl: './contact-page-form-section.component.html',
   styleUrls: ['./contact-page-form-section.component.scss'],
 })
-export class ContactPageFormSectionComponent {}
+export class ContactPageFormSectionComponent {
+  name = new FormControl('');
+  companyname = new FormControl('');
+  email = new FormControl('');
+  option = new FormControl('');
+  message = new FormControl('');
+}
--- libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
+++ libs/features/pages/contact-page/src/lib/features-pages-contact-page.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ContactPageFormSectionComponent } from './components/contact-page-form-section/contact-page-form-section.component';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   declarations: [ContactPageFormSectionComponent],
+  exports: [ContactPageFormSectionComponent],
 })
 export class FeaturesPagesContactPageModule {}
