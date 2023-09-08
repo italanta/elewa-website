d79c8a11d9a3e9cee29f646af3ec3cfc09a953f1
jbdura <bidurajohnson@gmail.com>
2023-09-08 17:02:39 +0300

    exported fake data

--- libs/data/sections/src/index.ts
+++ libs/data/sections/src/index.ts
@@ -3,6 +3,7 @@ export * from './lib/highlighted-news.data';
 export * from './lib/pricing-list.data';
 export * from './lib/content-development-hero-section.data'
 export * from './lib/highlighted-sdg-list.data';
-export * from './lib/team-members-list.data'
-export * from './lib/team-members-list.data'
-export * from './lib/consultancy-hero.data'
+export * from './lib/team-members-list.data';
+export * from './lib/team-members-list.data';
+export * from './lib/consultancy-hero.data';
+export * from './lib/consultancy-page-section-two.data';
