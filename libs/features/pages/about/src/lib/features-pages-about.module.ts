668e494d9bfd26ff5b58482ef16d1be9fa1e8731
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-08 10:04:41 +0300

    :lipstick: returned the discover section component imports to the about page

--- libs/features/pages/about/src/lib/features-pages-about.module.ts
+++ libs/features/pages/about/src/lib/features-pages-about.module.ts
@@ -10,6 +10,7 @@ import { MainPageModule } from '@elewa-website/elements/layout/pages/main-page';
 import { AboutPageComponent } from './main/about-page/about-page.component';
 import { TeamMembersSectionComponent } from './components/team-members-section/team-members-section.component';
 import { SdgSectionComponent } from './components/sdg-section/sdg-section.component';
+import { DiscoverSectionComponent } from './components/discover-section/discover-section.component';
 
 import { AboutRoutingModule } from './about.routing';
 
@@ -22,7 +23,7 @@ import { AboutRoutingModule } from './about.routing';
     MainPageModule,
     AboutRoutingModule,
   ],
-  declarations: [AboutPageComponent, TeamMembersSectionComponent, SdgSectionComponent],
+  declarations: [AboutPageComponent, TeamMembersSectionComponent, SdgSectionComponent, DiscoverSectionComponent],
   exports: [SdgSectionComponent]
 })
 export class FeaturesPagesAboutModule {}
