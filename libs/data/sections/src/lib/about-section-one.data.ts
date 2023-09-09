ce6ae01e60a52fa8625f80eaac462274a50c87ea
Tracie35 <kariukitracie@gmail.com>
2023-09-09 16:14:54 +0300

    :sparkles:import-content-text-and-image

---
+++ libs/data/sections/src/lib/about-section-one.data.ts
@@ -0,0 +1,23 @@
+import { ContentText } from '@elewa-website/models/schema/ui/texts';
+import { ImageConfig, ImageVisualisation } from '@elewa-website/models/schema/ui/images';
+export const _section1Content: ContentText = 
+
+  {
+    title: 'People at the center',
+    descriptions: [
+      'People are the secret to successful education programs. Through in-depth research, we work with your organization at different levels to understand ("elewa") how people live and learn.',
+      'Based on decades of research, this understanding allows us to co-create strong learning networks within your organization. Networks that can be reused every time a training need arises; At a fraction of the cost of current solutions.',
+    ],
+  }
+  export const _section1ImageData: ImageConfig = {
+    title: 'Image 1',
+    imageSrc:
+      'https://images.pexels.com/photos/1876790/pexels-photo-1876790.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    visualisation: ImageVisualisation.Pill,
+    maxWidth: '100%',
+  };
+  
+  export const _imageAndTextData = {
+    contentData: _section1Content,
+    imageData: _section1ImageData,
+  };  
