fe48b92e7699b089447dcf0a0807a50dc0a64975
Preston <prestonosoro56@gmail.com>
2023-09-11 14:53:05 +0300

    feat: import section mockData

--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,8 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+
+import { __highlightedImage as imageData, getLayoutClass } from '@elewa-website/data/sections';
+import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
+import { ImageConfig, ImageVisualisation } from '@elewa-website/models/schema/ui/images';
 
 @Component({
   selector: 'elewa-website-home',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent {}
+export class HomePageComponent implements OnInit {
+  @Input() imageData!: ImageAndText[];
+
+  ngOnInit() {
+    // Assign the imported data to imageData directly
+    this.imageData = imageData;
+    }
+
+  mapImageAndTextToImageConfig(item: ImageAndText): ImageConfig {
+    return {
+      title: item.content.title,
+      imageSrc: item.image.imageSrc,
+      visualisation: item.image.visualisation || ImageVisualisation.Window, // Set a default value
+    };
+  }
+  
+  // Reference the imported getLayoutClass function
+  getLayoutClass(item: ImageAndText): string {
+    return getLayoutClass(item);
+  }
+}
