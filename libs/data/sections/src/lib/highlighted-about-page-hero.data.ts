eee0c5d333e3c8b38b8ba5566838af53262606b4
paul <paulnyoiken@gmail.com>
2023-09-11 15:43:51 +0300

    ✨feat: added new mock data in SectionsModule named highlighted-about-page-hero.data.ts

---
+++ libs/data/sections/src/lib/highlighted-about-page-hero.data.ts
@@ -0,0 +1,9 @@
+import { HeroText } from "@elewa-website/models/schema/ui/texts";
+
+export const __highlightedAboutItems: HeroText[] = [
+  {
+    title: 'Lorem ipsum dolorem sit eos',
+    subtitle: '',
+    description: 'Neque porro quisquam dolorem ipsum quia dolor sit Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam dolorem ipsum quia dolor sit Eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam dolorem ipsum quia dolor.',
+  },
+];
