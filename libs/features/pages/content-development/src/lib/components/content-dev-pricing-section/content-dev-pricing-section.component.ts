49fff43ffdcdeb6eb7ee4dab79a05286ada34a63
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-11 16:00:38 +0300

    :sparkles: feat: create pricing section component

---
+++ libs/features/pages/content-development/src/lib/components/content-dev-pricing-section/content-dev-pricing-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-content-dev-pricing-section',
+  templateUrl: './content-dev-pricing-section.component.html',
+  styleUrls: ['./content-dev-pricing-section.component.scss'],
+})
+export class ContentDevPricingSectionComponent {}
