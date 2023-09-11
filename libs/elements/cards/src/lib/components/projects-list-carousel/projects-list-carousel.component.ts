87c73b68958903c05fd403b11c5ba55c0c14a907
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-11 16:24:42 +0300

    :sparkles: feat: create projects list carousel

---
+++ libs/elements/cards/src/lib/components/projects-list-carousel/projects-list-carousel.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-projects-list-carousel',
+  templateUrl: './projects-list-carousel.component.html',
+  styleUrls: ['./projects-list-carousel.component.scss'],
+})
+export class ProjectsListCarouselComponent {}
