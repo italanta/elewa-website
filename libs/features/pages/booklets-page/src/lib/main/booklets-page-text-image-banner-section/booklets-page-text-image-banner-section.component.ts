286eec1b7fbbd0e318a7535d0352b88eb2b95119
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-11 10:35:38 +0300

    :sparkles: feat: convert standalone component to a regular component

--- libs/features/pages/booklets-page/src/lib/main/booklets-page-text-image-banner-section/booklets-page-text-image-banner-section.component.ts
+++ libs/features/pages/booklets-page/src/lib/main/booklets-page-text-image-banner-section/booklets-page-text-image-banner-section.component.ts
@@ -1,27 +1,22 @@
-import { Component} from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { BannersModule } from '@elewa-website/elements/banners';
+import { Component } from '@angular/core';
+
 import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
 import { _imageAndTextData } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-booklets-page-text-image-banner-section',
-  standalone: true,
-  imports: [CommonModule, BannersModule],
   templateUrl: './booklets-page-text-image-banner-section.component.html',
   styleUrls: ['./booklets-page-text-image-banner-section.component.scss'],
 })
 export class BookletsPageTextImageBannerSectionComponent {
-
   /*Where to set an input Object/Data */
-  InputData : ImageAndText = {
-
+  InputData: ImageAndText = {
     // content Data
     content: _imageAndTextData.contentData,
 
     // our image
-    image: _imageAndTextData.imageData, 
+    image: _imageAndTextData.imageData,
     // positioning
-    imagePosition: 'left', 
+    imagePosition: 'left',
   };
 }
