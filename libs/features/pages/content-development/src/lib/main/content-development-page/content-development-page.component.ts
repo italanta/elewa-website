2d9b209b874a91a7872af53e75323101df3410d1
Tracie35 <kariukitracie@gmail.com>
2023-09-11 14:11:41 +0300

    :sparkles:moved code to the correct component

--- libs/features/pages/content-development/src/lib/main/content-development-page/content-development-page.component.ts
+++ libs/features/pages/content-development/src/lib/main/content-development-page/content-development-page.component.ts
@@ -1,23 +1,9 @@
 import { Component } from '@angular/core';
-import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
-import { _imageAndTextData } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-content-development-page',
   templateUrl: './content-development-page.component.html',
   styleUrls: ['./content-development-page.component.scss'],
 })
-export class ContentDevelopmentPageComponent {
-  /* an object(InputData) to input data using type ImageAndText from re-usable component */
-  InputData : ImageAndText = {
+export class ContentDevelopmentPageComponent {}
 
-    // content Data
-    content: _imageAndTextData.contentData,
-
-    // our image
-    image: _imageAndTextData.imageData, 
-
-    // positioning
-    imagePosition: 'left', 
-  };
-}
