b1766980644e531f70c65c59f72132bbf625666b
+ <+>
2023-09-11 15:23:24 +0300

    :sparkles: updated the ts file

--- libs/features/pages/consultancy-page/src/lib/components/elewa-consultancy-about-two/elewa-consultancy-about-two.component.ts
+++ libs/features/pages/consultancy-page/src/lib/components/elewa-consultancy-about-two/elewa-consultancy-about-two.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
+
+import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
+import { __highlightedImageData as highlightedImageData, getLayoutClass } from '@elewa-website/data/sections';
+
 
 @Component({
   selector: 'elewa-website-elewa-consultancy-about-two',
   templateUrl: './elewa-consultancy-about-two.component.html',
   styleUrls: ['./elewa-consultancy-about-two.component.scss'],
 })
-export class ElewaConsultancyAboutTwoComponent {}
+export class ElewaConsultancyAboutTwoComponent implements OnInit {
+  @Input() imageData !: ImageAndText[];
+  
+  ngOnInit(){
+    this.imageData = highlightedImageData;
+  }
+
+  getLayoutClass(item: ImageAndText): string {
+    return getLayoutClass(item);
+  }
+}
