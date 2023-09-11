62dbba336124f11808da542c1ea914db695e95ec
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-11 09:54:13 +0300

    :recycle: refactor: update consultancy about page component path

--- libs/features/pages/consultancy-page/src/lib/elewa-consultancy-about-one/elewa-consultancy-about-one.component.ts
+++ libs/features/pages/consultancy-page/src/lib/elewa-consultancy-about-one/elewa-consultancy-about-one.component.ts
@@ -1,27 +1,15 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
-import {
-  __highlightedImage as imageData,
-  getLayoutClass,
-} from '@elewa-website/models/data/sections';
 
 @Component({
-  selector: 'elewa-website-elewa-consultancy-about-one',
+  selector: 'elewa-website-consultancy-about-one',
   templateUrl: './elewa-consultancy-about-one.component.html',
   styleUrls: ['./elewa-consultancy-about-one.component.scss'],
 })
-export class ElewaConsultancyAboutOneComponent implements OnInit {
+export class ConsultancyAboutOneComponent {
   @Input() imageData!: ImageAndText[];
 
-  ngOnInit() {
-    // Assign the imported data to imageData directly
-    this.imageData = imageData;
-    // Debugging: Log the loaded data
-    // console.log('Loaded imageData:', this.imageData);
-  }
-
-  // Reference the imported getLayoutClass function
   getLayoutClass(item: ImageAndText): string {
-    return getLayoutClass(item);
+    return item.imagePosition === 'left' ? 'left-image' : 'right-image';
   }
 }
--- libs/features/pages/consultancy-page/src/lib/main/consultancy-page/consultancy-page.component.ts
+++ libs/features/pages/consultancy-page/src/lib/main/consultancy-page/consultancy-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { __highlightedImage } from '@elewa-website/models/data/sections';
 
 @Component({
   selector: 'elewa-website-consultancy-page',
   templateUrl: './consultancy-page.component.html',
   styleUrls: ['./consultancy-page.component.scss'],
 })
-export class ConsultancyPageComponent {}
+export class ConsultancyPageComponent {
+  content = __highlightedImage
+}
