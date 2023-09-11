bb48154cc5f28126b1e6488757a4287a707d83c8
x-russian <philophilo037@gmail.com>
2023-09-11 08:42:53 +0300

    export learning-services data file.

--- libs/data/sections/src/index.ts
+++ libs/data/sections/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/highlighted-project.data';
 export * from './lib/highlighted-news.data';
 export * from './lib/team-members-list.data'
+export * from './lib/learning-services.data';
