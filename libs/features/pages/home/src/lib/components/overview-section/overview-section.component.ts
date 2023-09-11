800082a17e61a9ab1d197553c40db0e31c13fd9b
jbdura <bidurajohnson@gmail.com>
2023-09-11 19:09:40 +0300

    Edited ts file and imported data

--- libs/features/pages/home/src/lib/components/overview-section/overview-section.component.ts
+++ libs/features/pages/home/src/lib/components/overview-section/overview-section.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { __overviewImageAndTextData } from '@elewa-website/data/sections';
+
 
 @Component({
   selector: 'elewa-website-overview-section',
   templateUrl: './overview-section.component.html',
   styleUrls: ['./overview-section.component.scss'],
 })
-export class OverviewSectionComponent {}
+export class OverviewSectionComponent {
+  // imported data
+  imageAndTextData = __overviewImageAndTextData;
+
+}
