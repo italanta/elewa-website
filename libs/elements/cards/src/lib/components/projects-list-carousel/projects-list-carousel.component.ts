9f88b006980c018e494034c9ebd571a8b530f95e
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-11 16:31:14 +0300

    :sparkles: feat: setup the reusable projects list to use the carousel

--- libs/elements/cards/src/lib/components/projects-list-carousel/projects-list-carousel.component.ts
+++ libs/elements/cards/src/lib/components/projects-list-carousel/projects-list-carousel.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+import { ProjectItem } from '@elewa-website/models/sections/projects';
 
 @Component({
   selector: 'elewa-website-projects-list-carousel',
   templateUrl: './projects-list-carousel.component.html',
   styleUrls: ['./projects-list-carousel.component.scss'],
 })
-export class ProjectsListCarouselComponent {}
+export class ProjectsListCarouselComponent {
+  @Input({ required: true }) projectsList!: ProjectItem[];
+}
