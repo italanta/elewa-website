0185a46b350c8a4cfebaa22afaa10bdd0d55e6b7
x-russian <philophilo037@gmail.com>
2023-09-11 09:50:04 +0300

    :package: mock-data file.

--- libs/data/sections/src/index.ts
+++ libs/data/sections/src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/highlighted-project.data';
 export * from './lib/highlighted-news.data';
 export * from './lib/team-members-list.data'
+export * from './lib/text-image-banner-section.data';
---
+++ libs/data/sections/src/lib/text-image-banner-section.data.ts
@@ -0,0 +1,25 @@
+import { ContentText } from '@elewa-website/models/schema/ui/texts';
+import { ImageConfig, ImageVisualisation } from '@elewa-website/models/schema/ui/images';
+
+export const _section: ContentText = 
+
+  {
+    title: 'Development of educational manuals and booklets',
+    descriptions: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    ],
+  }
+
+
+export const _imageData: ImageConfig = {
+  title: 'Image 1',
+  imageSrc:
+    'https://images.pexels.com/photos/1876790/pexels-photo-1876790.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  visualisation: ImageVisualisation.Pill,
+  maxWidth: '100%',
+};
+
+export const _imageAndTextData = {
+  contentData: _section,
+  imageData: _imageData,
+};
