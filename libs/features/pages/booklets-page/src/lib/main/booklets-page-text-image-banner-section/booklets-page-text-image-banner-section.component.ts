279a78d47e68610ce57f6d38ec987a402a50fca7
x-russian <philophilo037@gmail.com>
2023-09-11 09:51:03 +0300

    :sparkles: import and use input data.

--- libs/features/pages/booklets-page/src/lib/main/booklets-page-text-image-banner-section/booklets-page-text-image-banner-section.component.ts
+++ libs/features/pages/booklets-page/src/lib/main/booklets-page-text-image-banner-section/booklets-page-text-image-banner-section.component.ts
@@ -2,7 +2,7 @@ import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannersModule } from '@elewa-website/elements/banners';
 import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
-
+import { _imageAndTextData } from '@elewa-website/data/sections';
 
 @Component({
   selector: 'elewa-website-booklets-page-text-image-banner-section',
@@ -14,5 +14,14 @@ import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
 export class BookletsPageTextImageBannerSectionComponent {
 
   /*Where to set an input Object/Data */
-  InputData !: ImageAndText;
+  InputData : ImageAndText = {
+
+    // content Data
+    content: _imageAndTextData.contentData,
+
+    // our image
+    image: _imageAndTextData.imageData, 
+    // positioning
+    imagePosition: 'left', 
+  };
 }
