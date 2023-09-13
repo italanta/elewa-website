90d7edf36aad900f94d576b818453dfffba28324
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-13 11:34:46 +0300

    :recycle: refactor: remove unnecessary sections from about page

--- libs/features/pages/about/src/lib/components/about-hero-section/about-hero-section.component.ts
+++ libs/features/pages/about/src/lib/components/about-hero-section/about-hero-section.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
- import { __elewaImageData } from '@elewa-website/data/schema/ui/images';
+import { __elewaImageData } from '@elewa-website/data/schema/ui/images';
 import { __highlightedAboutItems } from '@elewa-website/data/sections';
 
 
