5adfaef5f5065be99f5d8834c37d91554511eeef
KaranjaK <karanjakelvin77@gmail.com>
2023-09-13 10:42:10 +0300

    📦 Creates heade-menu component

--- libs/elements/layout/header/src/lib/app-header.module.ts
+++ libs/elements/layout/header/src/lib/app-header.module.ts
@@ -5,10 +5,11 @@ import { TranslocoModule } from '@ngneat/transloco';
 
 import { HeaderComponent } from './components/header/header.component';
 import { ToggleMenuDirective } from './directives/toggle-menu.directive';
+import { HeaderMenuComponent } from './components/header-menu/header-menu.component';
 
 @NgModule({
   imports: [CommonModule, TranslocoModule],
-  declarations: [HeaderComponent, ToggleMenuDirective],
+  declarations: [HeaderComponent, ToggleMenuDirective, HeaderMenuComponent],
   exports: [HeaderComponent],
 })
 export class AppHeaderModule {}
