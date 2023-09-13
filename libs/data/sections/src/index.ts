e4e983eb9fefa791ef5766833da0605f53a55ba6
jbdura <bidurajohnson@gmail.com>
2023-09-13 13:13:46 +0300

    created data

--- libs/data/sections/src/index.ts
+++ libs/data/sections/src/index.ts
@@ -15,3 +15,4 @@ export * from './lib/highlighted-sdg-list.data';
 export * from './lib/consultancy-hero.data';
 export * from './lib/content-dev-about.data';
 export * from './lib/highlighted-about.data';
+export * from './lib/consultancy-about-two.data'
