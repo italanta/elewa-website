7d4135c29f3e60448c49076e67707db03060767c
Preston <prestonosoro56@gmail.com>
2023-09-13 06:39:06 +0300

    feat: import hero section mock data

--- libs/features/pages/about/src/lib/about-hero-section/about-hero-section.component.ts
+++ libs/features/pages/about/src/lib/about-hero-section/about-hero-section.component.ts
@@ -1,8 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import {
+  __highlightedImage as imageData,
+  getLayoutClass,
+} from '@elewa-website/data/sections';
+import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
+import {
+  ImageConfig,
+  ImageVisualisation,
+} from '@elewa-website/models/schema/ui/images';
 
 @Component({
   selector: 'elewa-website-about-hero-section',
   templateUrl: './about-hero-section.component.html',
   styleUrls: ['./about-hero-section.component.scss'],
 })
-export class AboutHeroSectionComponent {}
+export class AboutHeroSectionComponent implements OnInit {
+  @Input() imageData!: ImageAndText[];
+
+  ngOnInit() {
+    // Assign the imported data to imageData directly
+    this.imageData = imageData;
+  }
+
+  mapImageAndTextToImageConfig(item: ImageAndText): ImageConfig {
+    return {
+      title: item.content.title,
+      imageSrc: item.image.imageSrc,
+      visualisation: item.image.visualisation || ImageVisualisation.Stacked, // Set a default value
+    };
+  }
+
+  // Reference the imported getLayoutClass function
+  getLayoutClass(item: ImageAndText): string {
+    return getLayoutClass(item);
+  }
+}
