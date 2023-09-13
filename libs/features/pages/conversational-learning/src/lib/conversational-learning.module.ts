b0b673ec15cfccfd795822d01109a35ea9d3f4f9
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-13 10:07:54 +0300

    :sparkles: feat: create conv-learning pricing section

--- libs/features/pages/conversational-learning/src/lib/conversational-learning.module.ts
+++ libs/features/pages/conversational-learning/src/lib/conversational-learning.module.ts
@@ -5,11 +5,13 @@ import { TextsModule } from '@elewa-website/elements/layout/texts';
 import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 import { MainPageModule } from '@elewa-website/elements/layout/pages/main-page';
 import { BannersModule } from '@elewa-website/elements/banners';
+import { CardsModule } from '@elewa-website/elements/cards';
 
 import { ConversationalLearningPageComponent } from './main/conversational-learning-page/conversational-learning-page.component';
 import { ConvHeroSectionComponent } from './components/conv-hero-section/conv-hero-section.component';
 import { ConvAboutSectionOneComponent } from './components/conv-about-section-one/conv-about-section-one.component';
 import { ConvAboutSectionTwoComponent } from './components/conv-about-section-two/conv-about-section-two.component';
+import { ConvPricingSectionComponent } from './components/conv-pricing-section/conv-pricing-section.component';
 
 import { ConversationalLearningRoutingModule } from './conversational-learning-routing.module';
 
@@ -20,6 +22,7 @@ import { ConversationalLearningRoutingModule } from './conversational-learning-r
     TextsModule,
     ButtonsModule,
     BannersModule,
+    CardsModule,
     MainPageModule,
   ],
   declarations: [
@@ -27,6 +30,7 @@ import { ConversationalLearningRoutingModule } from './conversational-learning-r
     ConvHeroSectionComponent,
     ConvAboutSectionOneComponent,
     ConvAboutSectionTwoComponent,
+    ConvPricingSectionComponent,
   ],
 })
 export class ConversationalLearningModule {}
