9224cbe828438c7dcc2f8e44894faabdb9c8fc3c
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-13 16:11:24 +0300

    :recycle: refactor: delete unnecessary component

--- libs/elements/layout/texts/src/lib/components/about-hero-section/about-hero-section.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'elewa-website-about-hero-section',
-  templateUrl: './about-hero-section.component.html',
-  styleUrls: ['./about-hero-section.component.scss'],
-})
-export class AboutHeroSectionComponent {}
