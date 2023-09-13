bed1c782c68e00aaa83b0c43eedb8fdda4364ed8
Preston <prestonosoro56@gmail.com>
2023-09-13 13:38:10 +0300

    feat: clear previous code and implement the changes in the newly created component to handle the updates

--- libs/features/pages/home/src/lib/main/home/home-page.component.ts
+++ libs/features/pages/home/src/lib/main/home/home-page.component.ts
@@ -1,32 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-
-import { __highlightedImage as imageData, getLayoutClass } from '@elewa-website/data/sections';
-import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
-import { ImageConfig, ImageVisualisation } from '@elewa-website/models/schema/ui/images';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'elewa-website-home',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
-export class HomePageComponent implements OnInit {
-  @Input() imageData!: ImageAndText[];
-
-  ngOnInit() {
-    // Assign the imported data to imageData directly
-    this.imageData = imageData;
-    }
-
-  mapImageAndTextToImageConfig(item: ImageAndText): ImageConfig {
-    return {
-      title: item.content.title,
-      imageSrc: item.image.imageSrc,
-      visualisation: item.image.visualisation || ImageVisualisation.Window, // Set a default value
-    };
-  }
-  
-  // Reference the imported getLayoutClass function
-  getLayoutClass(item: ImageAndText): string {
-    return getLayoutClass(item);
-  }
-}
+export class HomePageComponent {}
