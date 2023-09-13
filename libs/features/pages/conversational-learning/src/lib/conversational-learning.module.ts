d97b0bdde49d19cc8e69818b3cb366974218401b
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-13 09:56:35 +0300

    :sparkles: feat: create conv-learning about section one component

--- libs/features/pages/conversational-learning/src/lib/conversational-learning.module.ts
+++ libs/features/pages/conversational-learning/src/lib/conversational-learning.module.ts
@@ -1,25 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AppHeaderModule } from '@elewa-website/elements/layout/header';
 import { TextsModule } from '@elewa-website/elements/layout/texts';
 import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
 import { MainPageModule } from '@elewa-website/elements/layout/pages/main-page';
-
-import { ConversationalLearningRoutingModule } from './conversational-learning-routing.module';
+import { BannersModule } from '@elewa-website/elements/banners';
 
 import { ConversationalLearningPageComponent } from './main/conversational-learning-page/conversational-learning-page.component';
 import { ConvHeroSectionComponent } from './components/conv-hero-section/conv-hero-section.component';
+import { ConvAboutSectionOneComponent } from './components/conv-about-section-one/conv-about-section-one.component';
+
+import { ConversationalLearningRoutingModule } from './conversational-learning-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
     ConversationalLearningRoutingModule,
-    AppHeaderModule,
     TextsModule,
     ButtonsModule,
+    BannersModule,
     MainPageModule,
   ],
-  declarations: [ConversationalLearningPageComponent, ConvHeroSectionComponent],
+  declarations: [
+    ConversationalLearningPageComponent,
+    ConvHeroSectionComponent,
+    ConvAboutSectionOneComponent,
+  ],
 })
 export class ConversationalLearningModule {}
