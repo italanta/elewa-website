f0f281db673d3968a34c063d0eca29e55971d14d
darlene9497 <darlene.nandabi@student.moringaschool.com>
2023-09-13 10:36:33 +0300

    :sparkles: generated the home-hero-section component

---
+++ libs/features/pages/home/src/lib/components/home-hero-section/home-hero-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-home-hero-section',
+  templateUrl: './home-hero-section.component.html',
+  styleUrls: ['./home-hero-section.component.scss'],
+})
+export class HomeHeroSectionComponent {}
