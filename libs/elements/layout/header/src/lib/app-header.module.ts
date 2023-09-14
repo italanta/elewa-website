e015de2b81dbf333b38d4a61b9c33ea1abf8d708
Dennnis Mburu Mwaura <dennismburu2063@gmail.com>
2023-09-14 12:09:16 +0300

    :fire: remove directive file and update header module

--- libs/elements/layout/header/src/lib/app-header.module.ts
+++ libs/elements/layout/header/src/lib/app-header.module.ts
@@ -4,11 +4,10 @@ import { CommonModule } from '@angular/common';
 import { TranslocoModule } from '@ngneat/transloco';
 
 import { HeaderComponent } from './components/header/header.component';
-import { ToggleMenuDirective } from './directives/toggle-menu.directive';
 
 @NgModule({
   imports: [CommonModule, TranslocoModule],
-  declarations: [HeaderComponent, ToggleMenuDirective],
+  declarations: [HeaderComponent],
   exports: [HeaderComponent],
 })
 export class AppHeaderModule {}
--- libs/elements/layout/header/src/lib/directives/toggle-menu.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ToggleMenuDirective } from './toggle-menu.directive';
-
-describe('ToggleMenuDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ToggleMenuDirective();
-    expect(directive).toBeTruthy();
-  });
-});
