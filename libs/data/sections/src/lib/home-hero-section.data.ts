630427b1fbb53d083c44456ba0e2abbe5eaa5bf1
jbdura <bidurajohnson@gmail.com>
2023-09-14 18:43:07 +0300

    Created data file

--- libs/data/sections/src/lib/home-hero-section.data.ts
+++ libs/data/sections/src/lib/home-hero-section.data.ts
@@ -19,10 +19,9 @@ export const __homeHerotexts: HeroText[] = [
 // Add the imageConfig property
 export const __homeImageConfig: ImageConfig[] = [
   {
-    imageSrc: '',   // Add image URL
-    title: '',      // Add image title
+    imageSrc: 'https://images.pexels.com/photos/1181325/pexels-photo-1181325.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',   // Add image URL
+    title: 'image',      // Add image title
     visualisation: ImageVisualisation.Stacked, // You can set this to Stacked or any other value as needed
     maxWidth: '803px',
-    maxHeight: '796px',
   }
 ];
