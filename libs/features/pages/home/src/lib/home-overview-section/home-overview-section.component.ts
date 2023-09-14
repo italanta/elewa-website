a3e8676ffe8f2cb90b13eddbef709258669cfd35
Preston <prestonosoro56@gmail.com>
2023-09-14 10:39:12 +0300

    feat: remove redundant methods

--- libs/features/pages/home/src/lib/home-overview-section/home-overview-section.component.ts
+++ libs/features/pages/home/src/lib/home-overview-section/home-overview-section.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { __highlightedImage as imageData } from '@elewa-website/data/sections';
 import { ImageAndText } from '@elewa-website/models/schema/ui/image-and-text';
-import { ImageVisualisation } from '@elewa-website/models/schema/ui/images';
 
 @Component({
   selector: 'elewa-website-home-overview-section',
@@ -14,20 +13,5 @@ export class HomeOverviewSectionComponent implements OnInit {
   ngOnInit() {
     // Assign the imported data to imageData directly
     this.imageData = imageData;
-  }
-
-  // Create a method to return ImageVisualisation enum
-  getImageVisualisation(): typeof ImageVisualisation {
-    return ImageVisualisation;
-  }
-  // Define the getLayoutClass function
-  getLayoutClass(item: ImageAndText): string {
-    // Implement your logic to determine the layout class based on the item
-    // For example, you can access item.imagePosition and return a class name accordingly.
-    if (item && item.imagePosition) {
-      return item.imagePosition === 'left' ? 'left-image' : 'right-image';
-    }
-    // Return a default layout class if needed
-    return 'default-layout';
   }
 }
