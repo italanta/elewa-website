5a543f73f7e0766c475807eae8c088f56bcc3d89
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-15 15:34:02 +0300

    :sparkles: feat: create window svg image component

--- libs/elements/layout/images/src/lib/images.module.ts
+++ libs/elements/layout/images/src/lib/images.module.ts
@@ -3,10 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { ElewaImageContainerComponent } from './components/elewa-image-container/elewa-image-container.component';
 import { StackedSvgComponentComponent } from './components/stacked-svg-component/stacked-svg-component.component';
+import { WindowSvgComponentComponent } from './components/window-svg-component/window-svg-component.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaImageContainerComponent, StackedSvgComponentComponent],
+  declarations: [
+    ElewaImageContainerComponent,
+    StackedSvgComponentComponent,
+    WindowSvgComponentComponent,
+  ],
   exports: [ElewaImageContainerComponent],
 })
 export class ImagesModule {}
