f9a0d7b215f4a5d74f10ec9fd1255f35355d53a3
Alexotieno1717 <alexotieno900@gmail.com>
2023-09-15 11:15:08 +0300

    :sparkles: feat: created the education section component

---
+++ libs/features/pages/home/src/lib/education-section/education-section.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-education-section',
+  templateUrl: './education-section.component.html',
+  styleUrls: ['./education-section.component.scss'],
+})
+export class EducationSectionComponent {
+}
