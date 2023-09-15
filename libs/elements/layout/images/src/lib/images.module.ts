31c1512f1de3c3de29c3b1f6a88d168667490dd1
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-15 12:21:53 +0300

    :sparkles: feat: add stacked svg component

--- libs/elements/layout/images/src/lib/images.module.ts
+++ libs/elements/layout/images/src/lib/images.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElewaImageContainerComponent } from './components/elewa-image-container/elewa-image-container.component';
+import { StackedSvgComponentComponent } from './components/stacked-svg-component/stacked-svg-component.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ElewaImageContainerComponent],
-  exports: [ElewaImageContainerComponent]
+  declarations: [ElewaImageContainerComponent, StackedSvgComponentComponent],
+  exports: [ElewaImageContainerComponent],
 })
 export class ImagesModule {}
