5a3e053168bcbc168e00c3732c4e2686ba6f9b19
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-16 19:27:37 +0300

    :recycle: fix: solve merge conflicts

--- libs/features/pages/home/src/lib/features-pages-home.module.ts
+++ libs/features/pages/home/src/lib/features-pages-home.module.ts
@@ -5,15 +5,17 @@ import { CardsModule } from '@elewa-website/elements/cards';
 import { CarouselModule } from '@elewa-website/elements/layout/carousel';
 import { MainPageModule } from '@elewa-website/elements/layout/pages/main-page';
 import { BannersModule } from '@elewa-website/elements/banners';
-
+import { ButtonsModule } from '@elewa-website/elements/layout/buttons';
+import { ImagesModule } from '@elewa-website/elements/layout/images';
+import { TextsModule } from '@elewa-website/elements/layout/texts';
 
 import { HomePageComponent } from './main/home/home-page.component';
 import { NewsSectionComponent } from './components/news-section/news-section.component';
 import { ProjectsSectionComponent } from './components/projects-section/projects-section.component';
+import { OverviewSectionComponent } from './components/overview-section/overview-section.component';
 import { HomeHeroSectionComponent } from './components/home-hero-section/home-hero-section.component';
 
 import { HomeRoutingModule } from './home.routing';
-import { OverviewSectionComponent } from './components/overview-section/overview-section.component';
 
 @NgModule({
   imports: [
@@ -23,6 +25,9 @@ import { OverviewSectionComponent } from './components/overview-section/overview
     CarouselModule,
     MainPageModule,
     BannersModule,
+    ButtonsModule,
+    ImagesModule,
+    TextsModule
   ],
   declarations: [
     HomePageComponent,
