eabb9130da600a2df772d2982a36f0874966b326
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-16 10:43:10 +0300

    :sparkles: feat: create pill shaped svg component

--- libs/elements/layout/images/src/lib/images.module.ts
+++ libs/elements/layout/images/src/lib/images.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { ElewaImageContainerComponent } from './components/elewa-image-container/elewa-image-container.component';
 import { StackedSvgComponentComponent } from './components/stacked-svg-component/stacked-svg-component.component';
 import { WindowSvgComponentComponent } from './components/window-svg-component/window-svg-component.component';
+import { PillSvgComponentComponent } from './components/pill-svg-component/pill-svg-component.component';
 
 @NgModule({
   imports: [CommonModule],
@@ -11,6 +12,7 @@ import { WindowSvgComponentComponent } from './components/window-svg-component/w
     ElewaImageContainerComponent,
     StackedSvgComponentComponent,
     WindowSvgComponentComponent,
+    PillSvgComponentComponent,
   ],
   exports: [ElewaImageContainerComponent],
 })
