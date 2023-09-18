7afc57e3d735388618f073ab726615f425d05584
Alexotieno1717 <alexotieno900@gmail.com>
2023-09-18 09:45:16 +0300

    :sparkles: :lipstick: worked on each card component styling and displaying their data

--- libs/elements/cards/src/lib/components/elewa-education-section-card/education-card-1/education-card-1.component.ts
+++ libs/elements/cards/src/lib/components/elewa-education-section-card/education-card-1/education-card-1.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EducationText } from '@elewa-website/models/schema/ui/texts';
 
 @Component({
   selector: 'elewa-website-education-card-1',
   templateUrl: './education-card-1.component.html',
   styleUrls: ['./education-card-1.component.scss'],
 })
-export class EducationCard1Component {}
+export class EducationCard1Component {
+  @Input() educationItem!: EducationText;
+}
--- libs/elements/cards/src/lib/components/elewa-education-section-card/education-card-2/education-card-2.component.ts
+++ libs/elements/cards/src/lib/components/elewa-education-section-card/education-card-2/education-card-2.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { EducationText } from '@elewa-website/models/schema/ui/texts';
 
 @Component({
   selector: 'elewa-website-education-card-2',
   templateUrl: './education-card-2.component.html',
   styleUrls: ['./education-card-2.component.scss'],
 })
-export class EducationCard2Component {}
+export class EducationCard2Component {
+  @Input() educationItem2!: EducationText;
+}
