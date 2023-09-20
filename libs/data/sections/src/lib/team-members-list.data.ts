fb74b1ade97ed65082ffe215a9a75d7ebd73db18
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-20 10:44:26 +0300

    :sparkles: feat: update team members list

--- libs/data/sections/src/lib/team-members-list.data.ts
+++ libs/data/sections/src/lib/team-members-list.data.ts
@@ -2,37 +2,63 @@ import { TeamMember } from '@elewa-website/models/schema/ui/cards';
 
 export const __highlightedTeamMembers: TeamMember[] = [
   {
-    fullName: 'John Doe',
+    fullName: 'Jente Rosseel',
     imgSrc:
-      'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
-  },
-
-  {
-    fullName: 'Mark zukerburg',
-    imgSrc:
-      'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
-  },
-
-  {
-    fullName: 'Elon Musk',
-    imgSrc:
-      'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656820/elewa-group-website/elewa-team-members/jente-elewa_wbqyyi.jpg',
   },
   {
-    fullName: 'Elon Musk',
+    fullName: 'Kennedy Adhola',
     imgSrc:
-      'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656822/elewa-group-website/elewa-team-members/ken-elewa_ydwzop.jpg',
   },
   {
-    fullName: 'Elon Musk',
+    fullName: 'Peter Reinartz',
     imgSrc:
-      'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656822/elewa-group-website/elewa-team-members/peter-elewa_zuzovx.jpg',
   },
   {
-    fullName: 'Elon Musk',
+    fullName: 'Ian Odhiambo',
     imgSrc:
-      'https://images.unsplash.com/photo-1693401050168-4958b146f736?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1374&q=80',
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656821/elewa-group-website/elewa-team-members/ian-elewa_bejey9.jpg',
   },
-
-  //more data
+  {
+    fullName: 'Shawn Chiama',
+    imgSrc:
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,q_90,w_600/v1679657743/elewa-group-website/elewa-team-members/shawn-elewa_tt3iit.jpg',
+  },
+  {
+    fullName: 'Reagan Charana',
+    imgSrc:
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656821/elewa-group-website/elewa-team-members/reagan-elewa_bn8xkl.jpg',
+  },
+  {
+    fullName: 'Stephen Mwaura',
+    imgSrc:
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679985792/elewa-group-website/elewa-team-members/lemmy_oosngt.jpg',
+  },
+  {
+    fullName: 'Jane Njoroge',
+    imgSrc:
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656821/elewa-group-website/elewa-team-members/jane-elewa_yyybka.jpg',
+  },
+  {
+    fullName: 'Paul Chesa',
+    imgSrc:
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,q_90,w_600/v1679666201/elewa-group-website/elewa-team-members/chesa-elewa_soeb8f.jpg',
+  },
+  {
+    fullName: `Jeff King'ori`,
+    imgSrc:
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679665800/elewa-group-website/elewa-team-members/jeff-elewa_hdupp7.jpg',
+  },
+  {
+    fullName: 'Lynn Anastasia',
+    imgSrc:
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/c_scale,h_1000/v1682586211/IMG_9755_vfprhm.jpg',
+  },
+  {
+    fullName: 'Frankline Odero',
+    imgSrc:
+      'https://res.cloudinary.com/dyl3rncv3/image/upload/v1679656820/elewa-group-website/elewa-team-members/frank-elewa_kihn9e.jpg',
+  },
 ];
