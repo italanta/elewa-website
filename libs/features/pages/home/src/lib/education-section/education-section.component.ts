518045566c231bed2fade67f0f2db0fcfa8b5357
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-20 16:56:48 +0300

    :sparkles: feat: add routerlinks to education section blocks

--- libs/features/pages/home/src/lib/education-section/education-section.component.ts
+++ libs/features/pages/home/src/lib/education-section/education-section.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 import {
   __highlightedEducationSection1,
   __highlightedEducationSection2,
@@ -12,8 +14,26 @@ import {
   styleUrls: ['./education-section.component.scss'],
 })
 export class EducationSectionComponent {
+  constructor(private _route: Router) {}
+
   educationSection = __highlightedEducationSection1;
   educationSection2 = __highlightedEducationSection2;
   educationSection3 = __highlightedEducationSection3;
   educationSection4 = __highlightedEducationSection4;
+
+  openConsultancy() {
+    this._route.navigateByUrl('/consultancy');
+  }
+
+  openContentDev() {
+    this._route.navigateByUrl('/content-development');
+  }
+
+  openConvLearning() {
+    this._route.navigateByUrl('/conversational-learning');
+  }
+
+  openBooklets() {
+    this._route.navigateByUrl('/booklets');
+  }
 }
