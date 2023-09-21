f316f98df08bc0ccdb120b170122ca5030c8e075
lemmyMwaura <lemmymwauracodes@gmail.com>
2023-09-21 08:48:16 +0300

    :sparkles: feat: add contacts to contact page

---
+++ libs/features/pages/contact-page/src/lib/components/contact-numbers-section/contact-numbers-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-website-contact-numbers-section',
+  templateUrl: './contact-numbers-section.component.html',
+  styleUrls: ['./contact-numbers-section.component.scss'],
+})
+export class ContactNumbersSectionComponent {}
